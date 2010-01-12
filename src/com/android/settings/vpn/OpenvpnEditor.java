d0d9ce3d72b5aded631519f6363a56943fabc117
James Bottomley <James.Bottomley@suse.de>
2010-01-12 11:38:10 -0600

    Settings: Add openvpn to profile and VPN settings

---
+++ src/com/android/settings/vpn/OpenvpnEditor.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright (C) 2010 James Bottomley <James.Bottomley@suse.de>
+ *
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vpn;
+    
+import com.android.settings.R;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.vpn.OpenvpnProfile;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.security.CertTool;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * The class for editing {@link OpenvpnProfile}.
+ */
+class OpenvpnEditor extends VpnProfileEditor {
+
+    private static final String KEY_PROFILE = "openvpn_profile";
+    private static final int REQUEST_ADVANCED = 1;
+
+    private static final String TAG = OpenvpnEditor.class.getSimpleName();
+
+    private int MENU_ID_ADVANCED;
+
+    private CheckBoxPreference mUserAuth;
+    private ListPreference mCert;
+    private ListPreference mCACert;
+
+    public OpenvpnEditor(OpenvpnProfile p) {
+        super(p);
+    }
+
+    @Override
+    protected void loadExtraPreferencesTo(PreferenceGroup subpanel) {
+        final Context c = subpanel.getContext();
+        final OpenvpnProfile profile = (OpenvpnProfile) getProfile();
+	mUserAuth = new CheckBoxPreference(c);
+	mUserAuth.setTitle(R.string.vpn_openvpn_userauth);
+	mUserAuth.setSummary(R.string.vpn_openvpn_userauth_summary);
+	mUserAuth.setChecked(profile.getUserAuth());
+	mUserAuth.setOnPreferenceChangeListener(
+            new Preference.OnPreferenceChangeListener() {
+	        public boolean onPreferenceChange(
+                    Preference pref, Object newValue) {
+		    boolean enabled = (Boolean) newValue;
+		    profile.setUserAuth(enabled);
+		    mUserAuth.setChecked(enabled);
+		    return true;
+		}
+	    });
+	subpanel.addPreference(mUserAuth);
+	mCACert = createList(c, R.string.vpn_user_certificate_title,
+            profile.getCAName(),
+	    CertTool.getInstance().getAllCaCertificateKeys(),
+            new Preference.OnPreferenceChangeListener() {
+		public boolean onPreferenceChange(Preference pref, Object newValue) {
+		    String f = (String) newValue;
+		    profile.setCAName(f);
+		    setSummary(mCACert, R.string.vpn_ca_certificate, 
+			       profile.getCAName());
+
+		    return true;
+		}           
+	    });
+	setSummary(mCACert, R.string.vpn_ca_certificate, profile.getCAName());
+	subpanel.addPreference(mCACert);
+ 	mCert = createList(c, R.string.vpn_ca_certificate_title,
+            profile.getCertName(),
+            CertTool.getInstance().getAllUserCertificateKeys(),
+            new Preference.OnPreferenceChangeListener() {
+		public boolean onPreferenceChange(Preference pref, Object newValue) {
+		    String f = (String) newValue;
+		    profile.setCertName(f);
+		    setSummary(mCert, R.string.vpn_ca_certificate,
+			       profile.getCertName());
+
+		    return true;
+		}           
+	    });
+	setSummary(mCert, R.string.vpn_ca_certificate, profile.getCertName());
+	subpanel.addPreference(mCert);
+    }
+
+    @Override
+    public String validate() {
+        String result = super.validate();
+	if (result != null)
+	    return result;
+
+	result = validate(mCert, R.string.vpn_a_user_certificate);
+	if (result != null)
+	    return result;
+
+	return validate(mCert, R.string.vpn_a_ca_certificate);
+    }
+
+    @Override
+    protected void onCreateOptionsMenu(Menu menu, int last_item) {
+	MENU_ID_ADVANCED = last_item + 1;
+
+	menu.add(0, MENU_ID_ADVANCED, 0, R.string.wifi_menu_advanced)
+            .setIcon(android.R.drawable.ic_menu_manage);
+    }
+
+    @Override
+    protected boolean onOptionsItemSelected(PreferenceActivity p, MenuItem item) {
+	if (item.getItemId() == MENU_ID_ADVANCED) {
+	    Intent intent = new Intent(p, AdvancedSettings.class);
+	    intent.putExtra(KEY_PROFILE, (Parcelable) getProfile());
+	    p.startActivityForResult(intent, REQUEST_ADVANCED);
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+            final Intent data) {
+	if (requestCode != REQUEST_ADVANCED)
+	    return;
+
+	OpenvpnProfile p = (OpenvpnProfile) getProfile();
+	OpenvpnProfile newP = data.getParcelableExtra(KEY_PROFILE);
+	if (newP == null) {
+	    Log.e(TAG, "no profile from advanced settings");
+	    return;
+	}
+	// manually copy across all advanced settings
+	p.setPort(newP.getPort());
+	p.setProto(newP.getProto());
+ 	p.setUseCompLzo(newP.getUseCompLzo());
+	p.setSupplyAddr(newP.getSupplyAddr());
+	p.setLocalAddr(newP.getLocalAddr());
+	p.setRemoteAddr(newP.getRemoteAddr());
+    }
+
+    private ListPreference createList(Context c, int titleResId,
+            String selection, String[] keys,
+            Preference.OnPreferenceChangeListener listener) {
+        ListPreference pref = new ListPreference(c);
+        pref.setTitle(titleResId);
+        pref.setDialogTitle(titleResId);
+        pref.setPersistent(true);
+        pref.setEntries(keys);
+        pref.setEntryValues(keys);
+        pref.setValue(selection);
+        pref.setOnPreferenceChangeListener(listener);
+        return pref;
+    }
+
+    public static class AdvancedSettings extends PreferenceActivity {
+	private static final String KEY_PORT = "set_port";
+	private static final String KEY_PROTO = "set_protocol";
+	private static final String KEY_COMP_LZO = "set_comp_lzo";
+	private static final String KEY_SET_ADDR = "set_addr";
+	private static final String KEY_LOCAL_ADDR = "set_local_addr";
+	private static final String KEY_REMOTE_ADDR = "set_remote_addr";
+
+	private EditTextPreference mPort;
+	private ListPreference mProto;
+	private CheckBoxPreference mCompLzo;
+	private CheckBoxPreference mSetAddr;
+	private EditTextPreference mLocalAddr;
+	private EditTextPreference mRemoteAddr;
+	private OpenvpnProfile profile;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    profile = getIntent().getParcelableExtra(KEY_PROFILE);
+
+	    addPreferencesFromResource(R.xml.openvpn_advanced_settings);
+
+	    mPort = (EditTextPreference) findPreference(KEY_PORT);
+	    mProto = (ListPreference) findPreference(KEY_PROTO);
+	    mCompLzo = (CheckBoxPreference) findPreference(KEY_COMP_LZO);
+	    mSetAddr = (CheckBoxPreference) findPreference(KEY_SET_ADDR);
+	    mLocalAddr = (EditTextPreference) findPreference(KEY_LOCAL_ADDR);
+	    mRemoteAddr = (EditTextPreference) findPreference(KEY_REMOTE_ADDR);
+
+	    mPort.setSummary(profile.getPort());
+	    mPort.setText(profile.getPort());
+	    mPort.setOnPreferenceChangeListener(
+	        new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			String name = (String) newValue;
+			name.trim();
+			profile.setPort(name);
+			mPort.setSummary(profile.getPort());
+
+			return true;
+		    }
+		});
+
+	    mProto.setSummary(profile.getProto());
+	    mProto.setValue(profile.getProto());
+	    mProto.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			String name = (String) newValue;
+			name.trim();
+			profile.setProto(name);
+			mProto.setSummary(profile.getProto());
+
+			return true;
+		}           
+	    });
+
+	    mCompLzo.setChecked(profile.getUseCompLzo());
+	    mCompLzo.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			Boolean b = (Boolean) newValue;
+			profile.setUseCompLzo(b);
+
+			return true;
+		}           
+	    });
+
+	    // This is inverted to cope with the way dependencies work 
+	    mSetAddr.setChecked(!profile.getSupplyAddr());
+	    mSetAddr.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			Boolean b = (Boolean) newValue;
+			profile.setSupplyAddr(!b);
+
+			return true;
+		}           
+	    });
+
+	    mLocalAddr.setSummary(profile.getLocalAddr());
+	    mLocalAddr.setText(profile.getLocalAddr());
+	    mLocalAddr.setOnPreferenceChangeListener(
+	        new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			String name = (String) newValue;
+			name.trim();
+			profile.setLocalAddr(name);
+			mLocalAddr.setSummary(profile.getLocalAddr());
+
+			return true;
+		    }
+		});
+
+	    mRemoteAddr.setSummary(profile.getRemoteAddr());
+	    mRemoteAddr.setText(profile.getRemoteAddr());
+	    mRemoteAddr.setOnPreferenceChangeListener(
+	        new Preference.OnPreferenceChangeListener() {
+		    public boolean onPreferenceChange(Preference pref, Object newValue) {
+			String name = (String) newValue;
+			name.trim();
+			profile.setRemoteAddr(name);
+			mRemoteAddr.setSummary(profile.getRemoteAddr());
+
+			return true;
+		    }
+		});
+	}
+
+	@Override
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+		Intent intent = new Intent(this, VpnEditor.class);
+		intent.putExtra(KEY_PROFILE, (Parcelable) profile);
+		setResult(RESULT_OK, intent);
+
+                finish();
+                return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
+
+
+    }
+}
