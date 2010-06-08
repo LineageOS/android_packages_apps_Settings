6248b51fdb5103457a15a0c55e5b721c1effa566
ABAAKOUK Mehdi <theli48@gmail.com>
2010-06-08 17:08:45 +0200

    Add ADWLauncher settings launcher
    Change-Id: I7cdc5ca160ea31301190c748dee1c9e34e5594e3

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -18,10 +18,14 @@ package com.android.settings;
 
 import android.net.sip.SipManager;
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ActivityInfo;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
 
 public class Settings extends PreferenceActivity {
 
@@ -30,29 +34,52 @@ public class Settings extends PreferenceActivity {
     private static final String KEY_SYNC_SETTINGS = "sync_settings";
     private static final String KEY_DOCK_SETTINGS = "dock_settings";
     
+    private static final String KEY_LAUNCHER = "launcher_settings";
+
+    private Preference mLauncherSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         addPreferencesFromResource(R.xml.settings);
-        
+
         int activePhoneType = TelephonyManager.getDefault().getPhoneType();
 
         PreferenceGroup parent = (PreferenceGroup) findPreference(KEY_PARENT);
         Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_SYNC_SETTINGS, 0);
+        Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_LAUNCHER, 0);
+        mLauncherSettings = parent.findPreference(KEY_LAUNCHER);
 
         Preference dockSettings = parent.findPreference(KEY_DOCK_SETTINGS);
         if (getResources().getBoolean(R.bool.has_dock_settings) == false && dockSettings != null) {
             parent.removePreference(dockSettings);
         }
     }
-    
+
     @Override
     protected void onResume() {
         super.onResume();
         findPreference(KEY_CALL_SETTINGS).setEnabled(
                 !AirplaneModeEnabler.isAirplaneModeOn(this)
                 || SipManager.isVoipSupported(this));
+
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.HOME");
+
+        PreferenceGroup parent = (PreferenceGroup) findPreference(KEY_PARENT);
+
+        ActivityInfo a = getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY).activityInfo;
+         if (a != null && a.name.equals("com.android.launcher.Launcher") && (a.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0 ){
+            if ( parent.findPreference(KEY_LAUNCHER) == null){
+                parent.addPreference(mLauncherSettings);
+            }
+        } else {
+            if ( parent.findPreference(KEY_LAUNCHER) != null){
+                parent.removePreference(mLauncherSettings);
+            }
+        }
     }
 
 }
