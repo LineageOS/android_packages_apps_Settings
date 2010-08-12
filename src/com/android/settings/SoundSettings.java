2a66bf62a29d921667e77e35074bc163b3ccd46f
Alexander Hofbauer <alex@derhofbauer.at>
2010-08-12 17:51:58 +0200

    Add froyo settings for binary amber/green LEDs
    Change-Id: I446b699a7f9a1fabdb505a532636a344f39250a7

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -55,6 +55,9 @@ public class SoundSettings extends PreferenceActivity implements
     private static final String KEY_EMERGENCY_TONE = "emergency_tone";
     private static final String KEY_SOUND_SETTINGS = "sound_settings";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
+    private static final String KEY_NOTIFICATION_BLINK = "notification_blink";
+    private static final String KEY_NOTIFICATION_ALWAYS_ON = "notification_always_on";
+    private static final String KEY_NOTIFICATION_CHARGING = "notification_charging";
     private static final String KEY_LOCK_SOUNDS = "lock_sounds";
 
     private static final String VALUE_VIBRATE_NEVER = "never";
@@ -76,6 +79,9 @@ public class SoundSettings extends PreferenceActivity implements
     private CheckBoxPreference mSoundEffects;
     private CheckBoxPreference mHapticFeedback;
     private CheckBoxPreference mNotificationPulse;
+    private CheckBoxPreference mNotificationBlink;
+    private CheckBoxPreference mNotificationAlwaysOn;
+    private CheckBoxPreference mNotificationCharging;
     private CheckBoxPreference mLockSounds;
 
     private AudioManager mAudioManager;
@@ -139,16 +145,47 @@ public class SoundSettings extends PreferenceActivity implements
         mSoundSettings = (PreferenceGroup) findPreference(KEY_SOUND_SETTINGS);
         mNotificationPulse = (CheckBoxPreference)
                 mSoundSettings.findPreference(KEY_NOTIFICATION_PULSE);
-        if (mNotificationPulse != null &&
-                getResources().getBoolean(R.bool.has_intrusive_led) == false) {
+        mNotificationBlink = (CheckBoxPreference)
+                mSoundSettings.findPreference(KEY_NOTIFICATION_BLINK);
+        mNotificationAlwaysOn = (CheckBoxPreference)
+                mSoundSettings.findPreference(KEY_NOTIFICATION_ALWAYS_ON);
+        mNotificationCharging = (CheckBoxPreference)
+                mSoundSettings.findPreference(KEY_NOTIFICATION_CHARGING);
+
+        boolean amberGreenLight = getResources().getBoolean(
+                com.android.internal.R.bool.config_amber_green_light);
+
+        if (amberGreenLight) {
             mSoundSettings.removePreference(mNotificationPulse);
+
+            mNotificationBlink.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.NOTIFICATION_LIGHT_BLINK, 1) == 1);
+            mNotificationBlink.setOnPreferenceChangeListener(this);
+
+            mNotificationAlwaysOn.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.NOTIFICATION_LIGHT_ALWAYS_ON, 1) == 1);
+            mNotificationAlwaysOn.setOnPreferenceChangeListener(this);
+
+            mNotificationCharging.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.NOTIFICATION_LIGHT_CHARGING, 1) == 1);
+            mNotificationCharging.setOnPreferenceChangeListener(this);
+
         } else {
-            try {
-                mNotificationPulse.setChecked(Settings.System.getInt(resolver,
-                        Settings.System.NOTIFICATION_LIGHT_PULSE) == 1);
-                mNotificationPulse.setOnPreferenceChangeListener(this);
-            } catch (SettingNotFoundException snfe) {
-                Log.e(TAG, Settings.System.NOTIFICATION_LIGHT_PULSE + " not found");
+            mSoundSettings.removePreference(mNotificationBlink);
+            mSoundSettings.removePreference(mNotificationAlwaysOn);
+            mSoundSettings.removePreference(mNotificationCharging);
+
+            if (mNotificationPulse != null &&
+                    getResources().getBoolean(R.bool.has_intrusive_led) == false) {
+                mSoundSettings.removePreference(mNotificationPulse);
+            } else {
+                try {
+                    mNotificationPulse.setChecked(Settings.System.getInt(resolver,
+                            Settings.System.NOTIFICATION_LIGHT_PULSE) == 1);
+                    mNotificationPulse.setOnPreferenceChangeListener(this);
+                } catch (SettingNotFoundException snfe) {
+                    Log.e(TAG, Settings.System.NOTIFICATION_LIGHT_PULSE + " not found");
+                }
             }
         }
 
@@ -312,8 +349,22 @@ public class SoundSettings extends PreferenceActivity implements
             boolean value = mNotificationPulse.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.NOTIFICATION_LIGHT_PULSE, value ? 1 : 0);
-        }
 
+        } else if (preference == mNotificationBlink) {
+            boolean value = mNotificationBlink.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_BLINK, value ? 1 : 0);
+
+        } else if (preference == mNotificationAlwaysOn) {
+            boolean value = mNotificationAlwaysOn.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_ALWAYS_ON, value ? 1 : 0);
+
+        } else if (preference == mNotificationCharging) {
+            boolean value = mNotificationCharging.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_CHARGING, value ? 1 : 0);
+        }
         return true;
     }
 
