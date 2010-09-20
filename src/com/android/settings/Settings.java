9cffd6973f6197a435bf35074bfe7348dddcae58
Bjorn Bringert <bringert@android.com>
2010-09-20 12:40:57 +0100

    Remove Search from the system Settings app DO NOT MERGE
    The search settings acitivity lives in the system search app,
    which is build-dependent. Also, since the Browser no longer
    looks at the SHOW_WEB_SUGGESTIONS system preference, none
    of the search settings affect the behavior of any other
    platform apps.
    
    The search settings will henceforth only be
    available from within the search app (e.g. QuickSearchBox).
    
    Bug: 3017365
    
    Change-Id: I08f3979cd8cdc0742efbc49be95aa59753c94c0d

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -30,7 +30,6 @@ public class Settings extends PreferenceActivity {
     private static final String KEY_PARENT = "parent";
     private static final String KEY_CALL_SETTINGS = "call_settings";
     private static final String KEY_SYNC_SETTINGS = "sync_settings";
-    private static final String KEY_SEARCH_SETTINGS = "search_settings";
     private static final String KEY_DOCK_SETTINGS = "dock_settings";
     private static final String KEY_LAUNCHER = "launcher_settings";
 
@@ -47,8 +46,6 @@ public class Settings extends PreferenceActivity {
 
         PreferenceGroup parent = (PreferenceGroup) findPreference(KEY_PARENT);
         Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_SYNC_SETTINGS, 0);
-        Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_SEARCH_SETTINGS, 0);
-		Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_LAUNCHER, 0);
         mLauncherSettings = parent.findPreference(KEY_LAUNCHER);
 
         Preference dockSettings = parent.findPreference(KEY_DOCK_SETTINGS);
