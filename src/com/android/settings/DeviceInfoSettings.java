f81891488b23e6d9d19a029a7c33c060c16d74a5
Chris Soyars <ctso@ctso.me>
2010-12-19 17:26:58 -0500

    Add Modver to Settings.
    Change-Id: I596064a80210517ff0dae30401f71b52adabd614
    
    Rename modversion
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -63,6 +63,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
+    private static final String KEY_MOD_VERSION = "mod_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -93,6 +94,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_MOD_VERSION, "ro.cm.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
