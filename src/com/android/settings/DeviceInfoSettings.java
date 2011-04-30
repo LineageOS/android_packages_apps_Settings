87a718f0678f5ca420def63df94dce78516e929f
Steve Kondik <shade@chemlab.org>
2011-04-30 05:18:18 -0400

    Fix NPE in DeviceInfoSettings
    Change-Id: I7eddded200a8bc77adc97e258d043b34c8f5ae09

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -116,9 +116,6 @@ public class DeviceInfoSettings extends PreferenceActivity {
         boolean mUpdateSettingAvailable =
                 getResources().getBoolean(R.bool.config_additional_system_update_setting_enable);
 
-        if(mUpdateSettingAvailable == false) {
-            getPreferenceScreen().removePreference(findPreference(KEY_UPDATE_SETTING));
-        }
     }
 
     @Override
