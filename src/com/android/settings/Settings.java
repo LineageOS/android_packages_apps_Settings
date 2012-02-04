a71f494c38e51ca000cbb51e44e1619d017f5ecb
Danesh Mondegarian <daneshm90@gmail.com>
2012-02-03 21:02:21 -0500

    Settings: Anonymous Statistics (CMStats)
    CMStats is now integrated into settings.
    
    Change-Id: Ice1b2c0c4882620eef774aca26b91a39d27f7b47

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -106,5 +106,6 @@ public class Settings extends SettingsActivity {
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LiveDisplayActivity extends SettingsActivity { /* empty */ }
     public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
+    public static class AnonymousStatsActivity extends Settings { /* empty */ }
 }
 
