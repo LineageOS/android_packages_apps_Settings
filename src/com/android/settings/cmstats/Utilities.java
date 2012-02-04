673084f46ae0f1ece707aaf53a972963e9a5ace3
Danesh Mondegarian <daneshm90@gmail.com>
2012-02-03 21:02:21 -0500

    Settings: Anonymous Statistics (CMStats)
    CMStats is now integrated into settings.
    
    Patchset 2 : Set service's exported value to false
    Patchset 3 : Fix mismatched title for activity label
    Patchset 4 : Renaming of strings
                 Cleanup
                 New icons (Thanks to Markus Guidry)
    Patchset 5 : Change stats url
    	     Add header to Utilities and reformat
    Patchset 6 : Take into account unusual but possible use-cases
    Patchset 7 : Fix alarmManager + preference mismatch
    Patchset 8 : Update alarm setting (data calculation..etc)
                 Avoid unecessary reset's
    Patchset 9 : Use unix timestamp instead (No converting back/forth)
    
    Change-Id: I25d8819e1d65413c516ed95c7038a1fe4a45d553

---
+++ src/com/android/settings/cmstats/Utilities.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cmstats;
+
+import java.math.BigInteger;
+import java.net.NetworkInterface;
+import java.security.MessageDigest;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.telephony.TelephonyManager;
+
+public class Utilities {
+    public static String getUniqueID(Context ctx) {
+        TelephonyManager tm = (TelephonyManager) ctx
+                .getSystemService(Context.TELEPHONY_SERVICE);
+
+        String device_id = digest(tm.getDeviceId());
+        if (device_id == null) {
+            String wifiInterface = SystemProperties.get("wifi.interface");
+            try {
+                String wifiMac = new String(NetworkInterface.getByName(
+                        wifiInterface).getHardwareAddress());
+                device_id = digest(wifiMac);
+            } catch (Exception e) {
+                device_id = null;
+            }
+        }
+
+        return device_id;
+    }
+
+    public static String getCarrier(Context ctx) {
+        TelephonyManager tm = (TelephonyManager) ctx
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String carrier = tm.getNetworkOperatorName();
+        if ("".equals(carrier)) {
+            carrier = "Unknown";
+        }
+        return carrier;
+    }
+
+    public static String getCarrierId(Context ctx) {
+        TelephonyManager tm = (TelephonyManager) ctx
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String carrierId = tm.getNetworkOperator();
+        if ("".equals(carrierId)) {
+            carrierId = "0";
+        }
+        return carrierId;
+    }
+
+    public static String getCountryCode(Context ctx) {
+        TelephonyManager tm = (TelephonyManager) ctx
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String countryCode = tm.getNetworkCountryIso();
+        if (countryCode.equals("")) {
+            countryCode = "Unknown";
+        }
+        return countryCode;
+    }
+
+    public static String getDevice() {
+        return SystemProperties.get("ro.cm.device");
+    }
+
+    public static String getModVersion() {
+        return SystemProperties.get("ro.cm.version");
+    }
+
+    public static String digest(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return new BigInteger(1, md.digest(input.getBytes())).toString(16)
+                    .toUpperCase();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
