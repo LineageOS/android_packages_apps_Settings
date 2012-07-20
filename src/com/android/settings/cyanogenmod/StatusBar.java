3c745724a0bdcb1ebb73e9106be3361d27613ddf
DvTonder <david.vantonder@gmail.com>
2012-07-20 18:18:55 -0400

    Settings: Status bar cleanup
    Make the Status bar settings screen conform with the Android Design
    Guidelines.
    
    Before: https://dl.dropbox.com/u/50993944/Screenshot_2012-07-20-18-16-18.png
    
    Change-Id: I8619ff09db0ef7d95f3a48a381abbc16a78f5bb4
    After: https://dl.dropbox.com/u/50993944/Screenshot_2012-07-20-18-25-27.png

--- src/com/android/settings/cyanogenmod/StatusBar.java
+++ src/com/android/settings/cyanogenmod/StatusBar.java
@@ -34,35 +34,21 @@ import com.android.settings.Utils;
 public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
-
     private static final String STATUS_BAR_BATTERY = "status_bar_battery";
-
     private static final String STATUS_BAR_CLOCK = "status_bar_show_clock";
-
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
-
     private static final String STATUS_BAR_SIGNAL = "status_bar_signal";
-
     private static final String COMBINED_BAR_AUTO_HIDE = "combined_bar_auto_hide";
-
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
-
     private static final String STATUS_BAR_CATEGORY_GENERAL = "status_bar_general";
 
     private ListPreference mStatusBarAmPm;
-
     private ListPreference mStatusBarBattery;
-
     private ListPreference mStatusBarCmSignal;
-
     private CheckBoxPreference mStatusBarClock;
-
     private CheckBoxPreference mStatusBarBrightnessControl;
-
     private CheckBoxPreference mCombinedBarAutoHide;
-
     private CheckBoxPreference mStatusBarNotifCount;
-
     private PreferenceCategory mPrefCategoryGeneral;
 
     @Override
@@ -106,16 +92,19 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         int statusBarAmPm = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.STATUS_BAR_AM_PM, 2);
         mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
+        mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
         mStatusBarAmPm.setOnPreferenceChangeListener(this);
 
         int statusBarBattery = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.STATUS_BAR_BATTERY, 0);
         mStatusBarBattery.setValue(String.valueOf(statusBarBattery));
+        mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
         mStatusBarBattery.setOnPreferenceChangeListener(this);
 
         int signalStyle = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.STATUS_BAR_SIGNAL_TEXT, 0);
         mStatusBarCmSignal.setValue(String.valueOf(signalStyle));
+        mStatusBarCmSignal.setSummary(mStatusBarCmSignal.getEntry());
         mStatusBarCmSignal.setOnPreferenceChangeListener(this);
 
         mCombinedBarAutoHide.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
@@ -138,18 +127,24 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mStatusBarAmPm) {
             int statusBarAmPm = Integer.valueOf((String) newValue);
+            int index = mStatusBarAmPm.findIndexOfValue((String) newValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
             return true;
         } else if (preference == mStatusBarBattery) {
             int statusBarBattery = Integer.valueOf((String) newValue);
+            int index = mStatusBarBattery.findIndexOfValue((String) newValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_BATTERY, statusBarBattery);
+            mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
             return true;
         } else if (preference == mStatusBarCmSignal) {
             int signalStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarCmSignal.findIndexOfValue((String) newValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_SIGNAL_TEXT, signalStyle);
+            mStatusBarCmSignal.setSummary(mStatusBarCmSignal.getEntries()[index]);
             return true;
         }
         return false;
