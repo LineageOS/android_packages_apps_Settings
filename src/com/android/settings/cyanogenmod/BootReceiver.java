9b45fb246f155828ec23184401b709e7d80b4a34
Timo Wendt <timo@tjwendt.de>
2012-08-05 19:34:26 +0200

    Buttons: Allow disabling hard navigation keys
    This adds support for enabling the navbar on devices with
    hardware keys.
    This requires that the device supports the KeyDisabler
    HAF methods, and is available in the Buttons menu
    
    Change-Id: I01bd83b312745b4a8aa3bde0cf5f02ea99a888cc

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -27,6 +27,7 @@ import android.util.Log;
 import com.android.settings.DisplaySettings;
 import com.android.settings.R;
 import com.android.settings.Utils;
+import com.android.settings.ButtonSettings;
 import com.android.settings.location.LocationSettings;
 
 import java.util.Arrays;
@@ -40,5 +41,6 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context ctx, Intent intent) {
         /* Restore the hardware tunable values */
         DisplaySettings.restore(ctx);
+        ButtonSettings.restoreKeyDisabler(ctx);
     }
 }
