4c1f6225535538c6bbf3195ddc79991c02183691
blunden <blunden2@gmail.com>
2012-11-29 21:13:00 +0100

    Use holo alert drawable
    These instances of the GB icon got missed in the big feature merge.
    
    Change-Id: I4725be79af6e51e1aa5d575a32232bbac0a11275

--- src/com/android/settings/cmstats/AnonymousStats.java
+++ src/com/android/settings/cmstats/AnonymousStats.java
@@ -88,7 +88,7 @@ public class AnonymousStats extends SettingsPreferenceFragment
                 mOkDialog = new AlertDialog.Builder(getActivity()).setMessage(
                         getActivity().getResources().getString(R.string.anonymous_statistics_warning))
                         .setTitle(R.string.anonymous_statistics_warning_title)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setIconAttribute(android.R.attr.alertDialogIcon)
                         .setPositiveButton(android.R.string.yes, this)
                         .setNeutralButton(getString(R.string.anonymous_learn_more), this)
                         .setNegativeButton(android.R.string.no, this)
