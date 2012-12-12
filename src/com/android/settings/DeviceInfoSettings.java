d165cdee08bec3801a0903922d1f424406ea8d85
blunden <blunden2@gmail.com>
2012-12-12 07:05:07 +0100

    Remove duplicate line enabling Mod Version twice
    Change-Id: I0a144df643ec01e275500287efebdd8351887c2b

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -94,7 +94,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.cm.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
-        findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
         if (!SELinux.isSELinuxEnabled()) {
