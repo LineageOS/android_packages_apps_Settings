09e4ab6da8dcc588a1c1cdeb6cbfcc06c3befc60
Danesh Mondegarian <daneshm90@gmail.com>
2012-12-18 00:43:51 -0500

    KeyguardAppWidgetPickActivity : Show all widgets
    Allow any widget to be installed on lockscreen
    
    Change-Id: I7343ed424fa6c8879df57435869ea14ba6078d24

--- src/com/android/settings/AppWidgetLoader.java
+++ src/com/android/settings/AppWidgetLoader.java
@@ -177,6 +177,6 @@ public class AppWidgetLoader<Item extends AppWidgetLoader.LabelledItem> {
      */
     void putInstalledAppWidgets(List<Item> items, int categoryFilter) {
         List<AppWidgetProviderInfo> installed = mAppWidgetManager.getInstalledProviders();
-        putAppWidgetItems(installed, null, items, categoryFilter, false);
+        putAppWidgetItems(installed, null, items, categoryFilter, true);
     }
 }
