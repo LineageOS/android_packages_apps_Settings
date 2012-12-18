54426113067ee2b440eba2dd911da6e24044d18a
Nathan Grebowiec <njgreb@gmail.com>
2012-12-17 20:47:13 -0600

    Modify Changelog to fragment
    Patchset 1) Initial commit
    (gweedo767)
    
    Patchset 2) Clean up XML and imports
    (gweedo767)
    
    patchset 3) removed setHasOptionsMenu
    (gweedo767)
    
    patchset 4) removed hardcoded string and one unneeded string
    (gweedo767)
    
    patchset 5) fix spacing in Manifest
    (gweedo767)
    
    Change-Id: Id39c3d44cf364b98bd8450db378d3dcfe7301fbb

--- src/com/android/settings/cyanogenmod/ChangeLog.java
+++ src/com/android/settings/cyanogenmod/ChangeLog.java
@@ -24,6 +24,12 @@ import android.util.Config;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.view.ViewGroup;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -34,14 +40,13 @@ import java.io.InputStreamReader;
 import com.android.internal.app.AlertActivity;
 import com.android.internal.app.AlertController;
 
-public class ChangeLog extends AlertActivity {
+public class ChangeLog extends Fragment {
 
     private static final String CHANGELOG_PATH = "/system/etc/CHANGELOG-CM.txt";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
         InputStreamReader inputReader = null;
         StringBuilder data = null;
         try {
@@ -53,8 +58,7 @@ public class ChangeLog extends AlertActivity {
                 data.append(tmp, 0, numRead);
             }
         } catch (IOException e) {
-            showErrorAndFinish();
-            return;
+            data.append(this.getString(R.string.changelog_error));
         } finally {
             try {
                 if (inputReader != null) {
@@ -64,34 +68,12 @@ public class ChangeLog extends AlertActivity {
             }
         }
 
-        if (TextUtils.isEmpty(data)) {
-            showErrorAndFinish();
-            return;
-        }
+        TextView textView = new TextView(getActivity());
+        textView.setText(data.toString());
 
-        WebView webView = new WebView(this);
+        final ScrollView scrollView = new ScrollView(getActivity());
+        scrollView.addView(textView);
 
-        // Begin the loading.  This will be done in a separate thread in WebView.
-        webView.loadDataWithBaseURL(null, data.toString(), "text/plain", "utf-8", null);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // Change from 'Loading...' to the real title
-                mAlert.setTitle(getString(R.string.changelog_dialog));
-            }
-        });
-
-        final AlertController.AlertParams p = mAlertParams;
-        p.mTitle = getString(R.string.changelog_loading);
-        p.mView = webView;
-        p.mForceInverseBackground = true;
-        setupAlert();
+        return scrollView;
     }
-
-    private void showErrorAndFinish() {
-        Toast.makeText(this, R.string.changelog_error, Toast.LENGTH_LONG)
-                .show();
-        finish();
-    }
-
 }
