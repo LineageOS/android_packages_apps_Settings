0e2b3b1ebb5b8f872482da8ba04db0098a2efacf
DvTonder <david.vantonder@gmail.com>
2013-02-18 09:25:26 -0500

    Settings: Add advanced reboot options (1 of 2)
    This is a squash of the commits below.
    
    commit 63011dd90a9be9488f8755ab4849c566b00ad858
    Author: jackmu95 <jacob.mueller.elz@gmail.com>
    Date:   Wed Feb 20 21:53:29 2013 +0100
    
        Settings: Move 'Advanced reboot' to other position
    
        This commit moves the 'Advanced reboot' checkbox one item down
        because it looks a bit strange as first element in the list IMHO.
    
        Before: http://goo.gl/jhw8h
        After:  http://goo.gl/X3H68
    
        Change-Id: I26d4a5caba3e720b952ec405e200c1528f7a7321
    
        Conflicts:
            res/xml/development_prefs.xml
    
    commit 3a882790a53e999aabafa0128bb59d724fb8b90b
    Author: DvTonder <david.vantonder@gmail.com>
    Date:   Mon Feb 18 09:25:26 2013 -0500
    
        Settings: Add Advanced reboot (1 of 2)
    
        This commit adds a setting in Development settings for including options
        in the power menu for rebooting into recovery or bootloader, defauled to
        off.
    
        When enabled, the Advanced reboot options will only be available once the
        device is unlocked.
    
        Change-Id: Ib77f0be178d15131b99adbe0abb6ba18b2b7c0b6
    
        Conflicts:
            res/values/cm_strings.xml
            res/values/strings.xml
            res/xml/development_prefs.xml
            src/com/android/settings/DevelopmentSettings.java
    
    Change-Id: I60fcb43b87bdfda1a85643adc5302b1216cdd643

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -164,6 +164,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
 
+    private static final String ADVANCED_REBOOT_KEY = "advanced_reboot";
+
     private static final int RESULT_DEBUG_APP = 1000;
 
     private static final String PERSISTENT_DATA_BLOCK_PROP = "ro.frp.pst";
@@ -240,6 +242,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private ListPreference mRootAccess;
     private Object mSelectedRootValue;
 
+    private CheckBoxPreference mAdvancedReboot;
+
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
 
     private final ArrayList<CheckBoxPreference> mResetCbPrefs
@@ -309,6 +313,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mDebugViewAttributes = findAndInitCheckboxPref(DEBUG_VIEW_ATTRIBUTES);
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
+        mAdvancedReboot = findAndInitCheckboxPref(ADVANCED_REBOOT_KEY);
 
 
         if (!android.os.Process.myUserHandle().equals(UserHandle.OWNER)) {
@@ -316,6 +321,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             disableForUser(mClearAdbKeys);
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
+            disableForUser(mAdvancedReboot);
         }
 
         mDebugAppPref = findPreference(DEBUG_APP_KEY);
@@ -582,6 +588,23 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateUseNuplayerOptions();
         updateUSBAudioOptions();
         updateRootAccessOptions();
+        updateAdvancedRebootOptions();
+    }
+
+    private void resetAdvancedRebootOptions() {
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 0);
+    }
+
+    private void writeAdvancedRebootOptions() {
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT,
+                mAdvancedReboot.isChecked() ? 1 : 0);
+    }
+
+    private void updateAdvancedRebootOptions() {
+        mAdvancedReboot.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 0) != 0);
     }
 
     private void resetDangerousOptions() {
@@ -596,6 +619,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         resetDebuggerOptions();
         writeLogdSizeOption(null);
         resetRootAccessOptions();
+        resetAdvancedRebootOptions();
         writeAnimationScaleOption(0, mWindowAnimationScale, null);
         writeAnimationScaleOption(1, mTransitionAnimationScale, null);
         writeAnimationScaleOption(2, mAnimatorDurationScale, null);
@@ -1496,6 +1520,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             writeUseNuplayerOptions();
         } else if (preference == mUSBAudio) {
             writeUSBAudioOptions();
+        } else if (preference == mAdvancedReboot) {
+            writeAdvancedRebootOptions();
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
