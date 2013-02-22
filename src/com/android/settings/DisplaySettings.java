b4f4782c1f0a6a59f351c13451c020cdddfa6966
Konsta <konsta09@gmail.com>
2013-02-22 15:57:30 +0200

    fix hiding volume key wakeup option
    Volume rocker wake option can't be currently hidden without
    disabling all volume rocker preferences.
    
    This issue was introduced in:
    http://review.cyanogenmod.org/#/c/32108/
    
    Change-Id: Icdbb2a833bad1045fb75517d9fffcd8eb92f9494

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -144,7 +144,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mVolumeWake = (CheckBoxPreference) findPreference(KEY_VOLUME_WAKE);
         if (mVolumeWake != null) {
             if (!getResources().getBoolean(R.bool.config_show_volumeRockerWake)
-                    && !Utils.hasVolumeRocker(getActivity())) {
+                    || !Utils.hasVolumeRocker(getActivity())) {
                 getPreferenceScreen().removePreference(mVolumeWake);
                 getPreferenceScreen().removePreference((PreferenceCategory) findPreference(KEY_WAKEUP_CATEGORY));
             } else {
