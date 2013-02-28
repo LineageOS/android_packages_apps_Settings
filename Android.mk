9fa16369d3ddbac34cf504e98e9e0d09f1bd2e1b
Koushik Dutta <koushd@gmail.com>
2013-02-28 14:49:24 -0800

    Settings: Add Superuser entry
    Squashed commit of the following:
    
    Author: Koushik Dutta <koushd@gmail.com>
    Switch to new Superuser
    Change-Id: I9d91392ad6fbc9953d19059ab59b91d214c00e0c
    
    Author: Danny Baumann <dannybaumann@web.de>
    Hide Superuser entry if root access for apps is disabled.
    Change-Id: Ibb77ba5a0fccf22564a26b8b1e13f7762c65718e
    
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Development: Root for apps is unavailable if the su daemon isn't running
    
    The superuser options are tied to the Superuser app variant we use,
    which in turn is tied to the su daemon. No daemon, no menu options.
    Change-Id: I41d58e62986759336ab38e67f26a558c2eed5aa3
    
    Change-Id: I9d91392ad6fbc9953d19059ab59b91d214c00e0c

--- Android.mk
+++ Android.mk
@@ -22,10 +22,15 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-include frameworks/opt/setupwizard/navigationbar/common.mk
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS += --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
+
+LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
 
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 
+include frameworks/opt/setupwizard/navigationbar/common.mk
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
