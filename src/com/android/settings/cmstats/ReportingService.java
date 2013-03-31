c6f4a87a6b38f262ae0b295465da4b683582d134
Koushik Dutta <koushd@gmail.com>
2013-03-31 14:41:18 -0700

    Add Google Analytics support.
    Change-Id: I81f816f2a358a36247c05a97053e948f4ea62753
    
    Conflicts:
    	Android.mk

--- src/com/android/settings/cmstats/ReportingService.java
+++ src/com/android/settings/cmstats/ReportingService.java
@@ -39,6 +39,11 @@ import android.util.Log;
 import com.android.settings.R;
 import com.android.settings.Settings;
 
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
+
+import com.android.settings.R;
+
 public class ReportingService extends Service {
     protected static final String TAG = "CMStats";
 
@@ -80,6 +85,13 @@ public class ReportingService extends Service {
         Log.d(TAG, "SERVICE: Carrier=" + deviceCarrier);
         Log.d(TAG, "SERVICE: Carrier ID=" + deviceCarrierId);
 
+        // report to google analytics
+        GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+        Tracker tracker = ga.getTracker(getString(R.string.ga_trackingId));
+        tracker.sendEvent(deviceName, deviceVersion, deviceCountry, null);
+        tracker.close();
+
+        // report to the cmstats service
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("http://stats.cyanogenmod.org/submit");
         try {
