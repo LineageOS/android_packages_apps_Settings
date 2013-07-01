deff1bd108333b3c997751a5dfb2f7731d7f6bda
Lars Greiss <kufikugel@googlemail.com>
2013-07-02 01:46:10 +0200

    Settings: Introduce Privacy Guard Manager
    Due that on the one hand a lot user are not familiar
    with permissions and how to read them and on the
    other user want to have a faster easy access to
    setup privacy guard for several apps this small
    manager assist the user to setup.
    
    Privacy Guard Manager shows a list off all installed apps
    Per default it shows only user apps which have critical
    permissions privacy guard can protect.
    The user can optional show in the list as well all system apps
    or turn off the filter to show all apps undependent of the
    permission the app has. To activate privacy guard for an
    app a simple click on the entry activates it so that the user
    can go fast trough the list. Longpress opens app detail page.
    Initial help dialog tells the user how to use it.
    
    Due the filter user who are unfimiliar with permissions can
    fast see which apps are effected without deeper knowledge
    
    cheers
    
    SlimRoms
    
    PatchSet3:
    - go inline with AOSP style
    - move PrivacyGuard by default into manager
    - change behaviour off active icons
    
    PatchSet4:
    - change app icon size to 36dip
    
    PatchSet5:
    - string change
    
    PatchSet6:
    - add reset option to disable privacy guard for
      all shown apps in the list
    
    PatchSet7:
    - Cleaned up the intro dialog a bit.
    
    PatchSet8+9:
    - cleanup
    - fix alignment of top checkbox
    
    PatchSet10:
    - fix permission filter
    
    PatchSet11:
    - improve icon loading
    
    Change-Id: I6bc886393bf90f8daed92b66d5fee1ce9e1accd0

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -94,7 +94,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
     private static final String KEY_VIBRATE_PREF = "lockscreen_vibrate";
     private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
-    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
     private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
 
     DevicePolicyManager mDPM;
@@ -128,7 +127,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private CheckBoxPreference mHomeUnlock;
     private CheckBoxPreference mQuickUnlockScreen;
     private ListPreference mSmsSecurityCheck;
-    private CheckBoxPreference mPrivacyGuardDefault;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -409,14 +407,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 PreferenceGroup appCategory = (PreferenceGroup)
                         root.findPreference(KEY_APP_SECURITY_CATEGORY);
                 appCategory.removePreference(mSmsSecurityCheck);
-            }
-
-            mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
-            try {
-                mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
-                        Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
-            } catch (SettingNotFoundException e) {
-                mPrivacyGuardDefault.setChecked(false);
             }
          }
 
@@ -708,9 +698,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         } else if (KEY_TOGGLE_VERIFY_APPLICATIONS.equals(key)) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.PACKAGE_VERIFIER_ENABLE,
                     mToggleVerifyApps.isChecked() ? 1 : 0);
-        } else if (KEY_PRIVACY_GUARD_DEFAULT.equals(key)) {
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.PRIVACY_GUARD_DEFAULT,
-                    mPrivacyGuardDefault.isChecked() ? 1 : 0);
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
