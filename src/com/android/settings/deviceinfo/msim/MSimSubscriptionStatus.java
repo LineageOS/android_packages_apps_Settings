4a2e790b7b840c0c8e5c4ef3db60453779c80473
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-07-04 18:59:54 +0530

    Settings(MSIM): Extend CB "Operator info" feature to MSIM.
    1) Add SUB information while broadcasting CB intent for MSIM.
    2) Remove country specific checks for cellbroadcast Operator
       info for MSIM.
    3) Move Operator info to About phone->Status->SelectSubscription
       ->SUB1/SUB2->Operator info in MSIM
    
    Change-Id: Idb6a2e3aac2a30528740729ea9e5b941bc495083
    CRs-fixed: 498245

--- src/com/android/settings/deviceinfo/msim/MSimSubscriptionStatus.java
+++ src/com/android/settings/deviceinfo/msim/MSimSubscriptionStatus.java
@@ -19,6 +19,7 @@
 
 package com.android.settings.deviceinfo;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -26,8 +27,10 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Message;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.telephony.CellBroadcastMessage;
 import android.telephony.MSimTelephonyManager;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
@@ -37,8 +40,10 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.telephony.MSimConstants;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
+
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.SelectSubscription;
@@ -72,6 +77,7 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
     private static final String KEY_MEID_NUMBER = "meid_number";
     private static final String KEY_SIGNAL_STRENGTH = "signal_strength";
     private static final String KEY_ICC_ID = "icc_id";
+    private static final String KEY_LATEST_AREA_INFO = "latest_area_info";
 
     private static final String[] PHONE_RELATED_ENTRIES = {
         KEY_SERVICE_STATE,
@@ -85,8 +91,19 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
         KEY_ESN_NUMBER,
         KEY_MEID_NUMBER,
         KEY_SIGNAL_STRENGTH,
-        KEY_ICC_ID
+        KEY_ICC_ID,
+        KEY_LATEST_AREA_INFO
     };
+
+    static final String CB_AREA_INFO_RECEIVED_ACTION =
+            "android.cellbroadcastreceiver.CB_AREA_INFO_RECEIVED";
+
+    static final String GET_LATEST_CB_AREA_INFO_ACTION =
+            "android.cellbroadcastreceiver.GET_LATEST_CB_AREA_INFO";
+
+    // Require the sender to have this permission to prevent third-party spoofing.
+    static final String CB_AREA_INFO_SENDER_PERMISSION =
+            "android.permission.RECEIVE_EMERGENCY_BROADCAST";
 
     private MSimTelephonyManager mTelephonyManager;
     private Phone mPhone = null;
@@ -99,6 +116,24 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
     private PhoneStateListener mPhoneStateListener;
 
     private static String sUnknown;
+
+    private BroadcastReceiver mAreaInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (CB_AREA_INFO_RECEIVED_ACTION.equals(action)) {
+                Bundle extras = intent.getExtras();
+                if (extras == null) {
+                    return;
+                }
+                CellBroadcastMessage cbMessage = (CellBroadcastMessage) extras.get("message");
+                if (cbMessage != null && cbMessage.getServiceCategory() == 50) {
+                    String latestAreaInfo = cbMessage.getMessageBody();
+                    updateAreaInfo(latestAreaInfo);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -197,6 +232,14 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
                                      | PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
             updateSignalStrength();
             updateServiceState();
+            registerReceiver(mAreaInfoReceiver, new IntentFilter(CB_AREA_INFO_RECEIVED_ACTION),
+                    CB_AREA_INFO_SENDER_PERMISSION, null);
+            // Ask CellBroadcastReceiver to broadcast the latest area info received
+            Intent getLatestIntent = new Intent(GET_LATEST_CB_AREA_INFO_ACTION);
+            getLatestIntent.putExtra(MSimConstants.SUBSCRIPTION_KEY, mSub);
+            sendBroadcastAsUser(getLatestIntent, UserHandle.ALL,
+                    CB_AREA_INFO_SENDER_PERMISSION);
+
         }
     }
 
@@ -206,6 +249,7 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
 
         if (!Utils.isWifiOnly(getApplicationContext())) {
             mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+            unregisterReceiver(mAreaInfoReceiver);
         }
     }
 
@@ -286,6 +330,12 @@ public class MSimSubscriptionStatus extends PreferenceActivity {
                 setSummaryText(KEY_ROAMING_STATE, mRes.getString(R.string.radioInfo_roaming_not));
             }
             setSummaryText(KEY_OPERATOR_NAME, mServiceState.getOperatorAlphaLong());
+        }
+    }
+
+    private void updateAreaInfo(String areaInfo) {
+        if (areaInfo != null) {
+            setSummaryText(KEY_LATEST_AREA_INFO, areaInfo);
         }
     }
 
