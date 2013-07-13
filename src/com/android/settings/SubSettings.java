bf75f541ebca3016bab98a731ac2c9b700af4d19
DvTonder <david.vantonder@gmail.com>
2013-07-13 09:37:01 -0400

    Settings: Generic Blacklist support (1 of 5)
    Change-Id: I58aff50637b9bf71095ec791e16efd520e6c29a3

--- src/com/android/settings/SubSettings.java
+++ src/com/android/settings/SubSettings.java
@@ -16,6 +16,9 @@
 
 package com.android.settings;
 
+import android.app.FragmentManager;
+import android.view.MenuItem;
+
 /**
  * Stub class for showing sub-settings; we can't use the main Settings class
  * since for our app it is a special singleTask class.
@@ -26,5 +29,26 @@ public class SubSettings extends Settings {
     public boolean onNavigateUp() {
         finish();
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (popFragment()) {
+                    return true;
+                }
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean popFragment() {
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            fm.popBackStack();
+            return true;
+        }
+        return false;
     }
 }
