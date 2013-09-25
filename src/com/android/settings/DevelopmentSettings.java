a5fa3671116c930328982e6ff243e4935a934882
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-25 18:32:56 +0100

    DevSettings: Fix the root-access toggle in eng builds
    It was being unconditionally removed, put it back
    
    Change-Id: I8eecab7353c59b224d917cc1710124f57656cf47

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -386,7 +386,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private boolean removeRootOptionsIfRequired() {
         // user builds don't get root, and eng always gets root
-        if (!Build.IS_DEBUGGABLE || "eng".equals(Build.TYPE)) {
+        if (!(Build.IS_DEBUGGABLE || "eng".equals(Build.TYPE))) {
             if (mRootAccess != null) {
                 getPreferenceScreen().removePreference(mRootAccess);
                 return true;
