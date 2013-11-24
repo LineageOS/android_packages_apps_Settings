4abe7d444c8e18deafb6e3d9f4b3d8fd56d22532
Michael Limb <something15525@gmail.com>
2013-11-24 00:09:51 -0800

    Settings: add back SMS security limit
    Limit the number of outgoing messages per every 15 minutes by
    any application that sends text messages.
    
    PS2-4: Whitespace fixes
    
    Change-Id: Ibb61396ea9adb3f95cdbd13b91cbbc3477f6e361

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -89,6 +89,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     // CyanogenMod Additions
     private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
     private static final String KEY_BLACKLIST = "blacklist";
+    private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
 
     private PackageManager mPM;
     private DevicePolicyManager mDPM;
@@ -117,6 +118,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
     // CyanogenMod Additions
     private PreferenceScreen mBlacklist;
+    private ListPreference mSmsSecurityCheck;
 
     public SecuritySettings() {
         super(null /* Don't ask for restrictions pin on creation. */);
@@ -303,13 +305,19 @@ public class SecuritySettings extends RestrictedSettingsFragment
         // App security settings
         addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
         mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
-
+        mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
         // Determine options based on device telephony support
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
+            mSmsSecurityCheck.setOnPreferenceChangeListener(this);
+            int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
+            updateSmsSecuritySummary(smsSecurityCheck);
+        } else {
             // No telephony, remove dependent options
             PreferenceGroup appCategory = (PreferenceGroup)
                     root.findPreference(KEY_APP_SECURITY_CATEGORY);
             appCategory.removePreference(mBlacklist);
+            appCategory.removePreference(mSmsSecurityCheck);
         }
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
@@ -410,6 +418,11 @@ public class SecuritySettings extends RestrictedSettingsFragment
         if (mWarnInstallApps != null) {
             mWarnInstallApps.dismiss();
         }
+    }
+
+    private void updateSmsSecuritySummary(int selection) {
+        String message = getString(R.string.sms_security_check_limit_summary, selection);
+        mSmsSecurityCheck.setSummary(message);
     }
 
     private void setupLockAfterPreference() {
@@ -612,6 +625,11 @@ public class SecuritySettings extends RestrictedSettingsFragment
                 Log.e("SecuritySettings", "could not persist lockAfter timeout setting", e);
             }
             updateLockAfterPreferenceSummary();
+        } else if (preference == mSmsSecurityCheck) {
+            int smsSecurityCheck = Integer.valueOf((String) value);
+            Settings.Global.putInt(getContentResolver(),
+                    Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, smsSecurityCheck);
+            updateSmsSecuritySummary(smsSecurityCheck);
         }
         return true;
     }
