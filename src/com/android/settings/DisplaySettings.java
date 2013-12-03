236b41f6577370afdcbf3305f34fdb990b7d09f7
Michael Bestas <mikeioannina@gmail.com>
2013-12-03 21:12:55 +0200

    Allow changing screen-off animation (2/2)
    * Allow enabling/disabling screen-off animation
    * Allow selection between fade & crt effects if the device allows it
    
    Change-Id: I5ebfab22e68ea34e3babfaacd9c8103e25932d3d

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
+import static android.provider.Settings.System.SCREEN_ANIMATION_STYLE;
 
 import android.app.ActivityManagerNative;
 import android.app.Dialog;
@@ -71,12 +72,16 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+    private static final String KEY_SCREEN_ANIMATION_OFF = "screen_off_animation";
+    private static final String KEY_SCREEN_ANIMATION_STYLE = "screen_animation_style";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
     private CheckBoxPreference mAccelerometer;
     private FontDialogPreference mFontSizePref;
     private CheckBoxPreference mWakeWhenPluggedOrUnplugged;
+    private CheckBoxPreference mScreenOffAnimation;
+    private ListPreference mScreenAnimationStylePreference;
 
     private PreferenceScreen mNotificationPulse;
     private PreferenceScreen mBatteryPulse;
@@ -154,6 +159,31 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
         mWakeWhenPluggedOrUnplugged =
                 (CheckBoxPreference) findPreference(KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
+
+        boolean allowsScreenOffAnimation = res.getBoolean(
+                com.android.internal.R.bool.config_screenOffAnimation);
+        boolean requiresFadeAnimation = res.getBoolean(
+                com.android.internal.R.bool.config_animateScreenLights);
+        mScreenOffAnimation = (CheckBoxPreference) findPreference(KEY_SCREEN_ANIMATION_OFF);
+        mScreenAnimationStylePreference =
+                (ListPreference) findPreference(KEY_SCREEN_ANIMATION_STYLE);
+
+        if (allowsScreenOffAnimation) {
+            if (!requiresFadeAnimation) {
+                final int currentAnimation =
+                        Settings.System.getInt(resolver, SCREEN_ANIMATION_STYLE, 0);
+                mScreenAnimationStylePreference.setValue(String.valueOf(currentAnimation));
+                mScreenAnimationStylePreference.setOnPreferenceChangeListener(this);
+                updateScreenAnimationStylePreferenceDescription(currentAnimation);
+            } else {
+                getPreferenceScreen().removePreference(mScreenAnimationStylePreference);
+                mScreenAnimationStylePreference.setValue(String.valueOf(1));
+            }
+        } else {
+            mScreenOffAnimation.setChecked(false);
+            getPreferenceScreen().removePreference(mScreenOffAnimation);
+            getPreferenceScreen().removePreference(mScreenAnimationStylePreference);
+        }
 
         boolean hasNotificationLed = res.getBoolean(
                 com.android.internal.R.bool.config_intrusiveNotificationLed);
@@ -293,6 +323,24 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         }
         screenTimeoutPreference.setEnabled(revisedEntries.size() > 0);
+    }
+
+    private void updateScreenAnimationStylePreferenceDescription(int currentAnimation) {
+        ListPreference preference = mScreenAnimationStylePreference;
+        String summary;
+        if (currentAnimation < 0) {
+            // Unsupported value
+            summary = "";
+        } else {
+            final CharSequence[] entries = preference.getEntries();
+            final CharSequence[] values = preference.getEntryValues();
+            if (entries == null || entries.length == 0) {
+                summary = "";
+            } else {
+                summary = entries[currentAnimation].toString();
+            }
+        }
+        preference.setSummary(summary);
     }
 
     @Override
@@ -444,6 +492,15 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
         if (KEY_FONT_SIZE.equals(key)) {
             writeFontSizePreference(objValue);
+        }
+        if (KEY_SCREEN_ANIMATION_STYLE.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), SCREEN_ANIMATION_STYLE, value);
+                updateScreenAnimationStylePreferenceDescription(value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist screen animation style setting", e);
+            }
         }
 
         return true;
