65d09e3bbf80bcc928ce4e1e064172110dec2cce
jianzhou <jianzhou@codeaurora.org>
2013-12-09 15:46:16 +0800

    Settings: Add IZat location feature.
    Add IZat "Accelerated location" item in "Location mode" page in
    Settings. The feature can be turned on and off with a toggle switch.
    CRs-Fixed: 584373
    
    Conflicts:
    res/values-hi/strings.xml
    Change-Id: Iddd8863536b9f85b36a59284b4b1f16f11a1e40e

--- Android.mk
+++ Android.mk
@@ -12,6 +12,10 @@ LOCAL_SRC_FILES := \
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
+LOCAL_SRC_FILES += \
+        src/com/android/location/XT/IXTSrv.aidl \
+        src/com/android/location/XT/IXTSrvCb.aidl
+
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
