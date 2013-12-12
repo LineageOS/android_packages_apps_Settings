0a6616dce3cd92eed3471ded948f011f6c795433
Michael Bestas <mikeioannina@gmail.com>
2013-12-12 05:40:54 +0200

    Settings: Forward port lock pattern grid size (2/2)
    Author: d34d <clark@cyngn.com>
    Date:   Tue Dec 29 11:51:21 2015 -0800
    
        Fingerprint: Forward challenge extras
    
        The forward port of custom pattern sizes introduced a bug that would
        cause an NPE when trying to add a fingerprint for security. This patch
        forwards the extras associated with the challenge that would have
        normally been passed directly in to the ChooseLockPattern fragment.
    
        Change-Id: I3ad994b2604ff45b573c011c3588afe8b35bfdd3
        TICKET: CYNGNOS-1490
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Mar 21 15:17:16 2016 -0700
    
        Settings: forward decrypt required on boot flag
    
        This wasn't being pass forward and all of the defaults had it set to
        true.
    
        Ticket: CYNGNOS-2270
    
        Change-Id: I90a6c918bb5b4b52791bcff18a6ea3bcfb194547
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Dec 11 11:37:00 2015 -0600
    
        Settings: allow rotation while setting new pattern
    
        Change-Id: I0fc9a7df686b8af0c60edf1916dc6fe02185f704
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 6 12:20:33 2015 -0700
    
        Settings: handle decrypting larger pattern sizes
    
        Change-Id: Id24d46829063171fa87cabb23a7da378726d7548
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue Apr 28 14:02:21 2015 -0700
    
        CryptKeeper: pattern unlock displays incorrect pw when correct
    
        fakeUnlockAttempt() gets called when the user inputs any pattern length
        < 4 which queues up the 'incorrect error' message. The message needs to
        be cleared before trying to actually check the password so it never goes
        through in case the password was correct.
    
        Change-Id: If78db332d3d696ba443d0be911fb5db504cb14cd
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri May 1 17:07:47 2015 -0700
    
        Settings: fix non lock pattern CryptKeeper crash
    
        Change-Id: Ib2d6c9a468bfe778d5cb927759f11b2b03c25ee6
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue May 19 14:06:51 2015 -0700
    
        CryptKeeper: layout whole screen in bounds
    
        Add flags to make the screen layout properly on devices with the
        navigation bar visible
    
        Change-Id: I8607c8e49ac3625a7fb6a13b4501ea7ac818b9ba
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Wed May 20 10:31:13 2015 -0700
    
        CryptKeeper improvements
    
        - Status text was used enough to warrant it being a field variable
          instead of looking for the view every time
    
        - Display proper text after changing pattern sizes (to input a pattern,
          not a password)
    
        - Disable changing pattern sizes while validaing pattern
    
        REFS: LETTUCE-557, LETTUCE-352
        Change-Id: Ib4ecf04a58da8d1648d514d9650d69c33d9587a7
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Alberto97 <albertop2197@gmail.com>
    Date:   Mon Nov 21 20:47:25 2016 +0100
    
        Settings: Use GLIF Theme for missing Settings > Screen lock Activities
    
        Icons by Asher
    
        Change-Id: Icf1627b41ef604302a5819ad3b1bdfd6d8479202
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon May 10 22:00:01 2021 +0200
    
        Settings: Remove hardcoded extras from ChooseLockPatternSize
    
        Change-Id: I860d87842de0dbb9f87394a0c1fc5043fd16b07b
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue May 11 11:32:38 2021 +0200
    
        Settings: Make setup wizard go through ChooseLockPatternSize if needed
    
        Change-Id: Idfd3670d4b541b3618c4416b3b9dbf7d88ae3ed1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon May 10 22:29:49 2021 +0200
    
        Settings: Make FRP go through ChooseLockPatternSize if needed
    
        Change-Id: I70b0ddb20033dd5505c391763a0a58fd2f9c8004
    
    Change-Id: I7078d703c218cd096d9b77c003a94b52fbce6322

---
+++ src/com/android/settings/password/ChooseLockPatternSize.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2012-2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.password;
+
+import android.content.Intent;
+import android.content.res.Resources.Theme;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.Preference;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.widget.LockscreenCredential;
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SetupWizardUtils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.utils.SettingsDividerItemDecoration;
+
+import com.google.android.setupdesign.GlifPreferenceLayout;
+
+import org.lineageos.internal.logging.LineageMetricsLogger;
+
+public class ChooseLockPatternSize extends SettingsActivity {
+
+    @Override
+    public Intent getIntent() {
+        Intent modIntent = new Intent(super.getIntent());
+        modIntent.putExtra(EXTRA_SHOW_FRAGMENT, ChooseLockPatternSizeFragment.class.getName());
+        return modIntent;
+    }
+
+    @Override
+    protected void onApplyThemeResource(Theme theme, int resid, boolean first) {
+        resid = SetupWizardUtils.getTheme(this, getIntent());
+        super.onApplyThemeResource(theme, resid, first);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        if (ChooseLockPatternSizeFragment.class.getName().equals(fragmentName)) return true;
+        return false;
+    }
+
+    @Override
+    protected boolean isToolbarEnabled() {
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        findViewById(R.id.content_parent).setFitsSystemWindows(false);
+    }
+
+    public static class ChooseLockPatternSizeFragment extends SettingsPreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (!(getActivity() instanceof ChooseLockPatternSize)) {
+                throw new SecurityException("Fragment contained in wrong activity");
+            }
+            addPreferencesFromResource(R.xml.security_settings_pattern_size);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(Preference preference) {
+            final String key = preference.getKey();
+
+            byte patternSize;
+            if ("lock_pattern_size_4".equals(key)) {
+                patternSize = 4;
+            } else if ("lock_pattern_size_5".equals(key)) {
+                patternSize = 5;
+            } else if ("lock_pattern_size_6".equals(key)) {
+                patternSize = 6;
+            } else {
+                patternSize = 3;
+            }
+
+            Bundle extras = getActivity().getIntent().getExtras();
+            Intent intent = new Intent();
+            intent.setClassName(getActivity(), extras.getString("className"));
+            intent.putExtras(extras);
+            intent.putExtra("pattern_size", patternSize);
+            intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+            startActivity(intent);
+
+            finish();
+            return true;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            GlifPreferenceLayout layout = (GlifPreferenceLayout) view;
+            layout.setDividerItemDecoration(new SettingsDividerItemDecoration(getContext()));
+
+            layout.setIcon(getContext().getDrawable(R.drawable.ic_lock));
+
+            if (getActivity() != null) {
+                getActivity().setTitle(R.string.lock_settings_picker_pattern_size_message);
+            }
+
+            layout.setHeaderText(R.string.lock_settings_picker_pattern_size_message);
+
+            // Use the dividers in SetupWizardRecyclerLayout. Suppress the dividers in
+            // PreferenceFragment.
+            setDivider(null);
+        }
+
+        @Override
+        public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent,
+                Bundle savedInstanceState) {
+            GlifPreferenceLayout layout = (GlifPreferenceLayout) parent;
+            return layout.onCreateRecyclerView(inflater, parent, savedInstanceState);
+        }
+
+        @Override
+        public int getMetricsCategory() {
+            return LineageMetricsLogger.CHOOSE_LOCK_PATTERN_SIZE;
+        }
+    }
+}
