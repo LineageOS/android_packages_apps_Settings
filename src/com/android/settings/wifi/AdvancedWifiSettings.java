8a4154b23165e40ea52475d9de12a4127f05e9c2
luxiiaol <luxiaol@codeaurora.org>
2013-12-25 13:39:10 +0800

    Settings: Add the feature of wifi auto connect
    This FR is a wifi auto connect checkbox. If it is checked,
    allow automaticlly connect WLAN.
    
    CRs-Fixed: 507964
    
    Change-Id: I2c764552f4f12387c6211159ddeeadf14dfdcd60

--- src/com/android/settings/wifi/AdvancedWifiSettings.java
+++ src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -52,6 +52,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_SCAN_ALWAYS_AVAILABLE = "wifi_scan_always_available";
     private static final String KEY_INSTALL_CREDENTIALS = "install_credentials";
     private static final String KEY_SUSPEND_OPTIMIZATIONS = "suspend_optimizations";
+    private static final String KEY_AUTO_CONNECT_TYPE = "auto_connect_type";
 
     private static final String KEY_CURRENT_GATEWAY = "current_gateway";
     private static final String KEY_CURRENT_NETMASK = "current_netmask";
@@ -144,6 +145,21 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
             sleepPolicyPref.setValue(stringValue);
             updateSleepPolicySummary(sleepPolicyPref, stringValue);
         }
+
+        CheckBoxPreference AutoPref = (CheckBoxPreference) findPreference(KEY_AUTO_CONNECT_TYPE);
+        if (AutoPref != null) {
+            if (getResources().getBoolean(R.bool.config_auto_connect_wifi_enabled)) {
+                AutoPref.setChecked(Settings.System.getInt(getContentResolver(),
+                        getResources().getString(R.string.wifi_autoconn_type),
+                        getResources().getInteger(R.integer.wifi_autoconn_type_auto)) ==
+                        getResources().getInteger(R.integer.wifi_autoconn_type_auto));
+                AutoPref.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(AutoPref);
+            }
+        } else {
+            Log.d(TAG, "Fail to get auto connect pref");
+        }
     }
 
     private void updateSleepPolicySummary(Preference sleepPolicyPref, String value) {
@@ -224,6 +240,14 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
                         Toast.LENGTH_SHORT).show();
                 return false;
             }
+        }
+
+        if (KEY_AUTO_CONNECT_TYPE.equals(key)) {
+            boolean checked = ((Boolean) newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(),
+                    getResources().getString(R.string.wifi_autoconn_type),
+                    checked ? getResources().getInteger(R.integer.wifi_autoconn_type_auto)
+                            : getResources().getInteger(R.integer.wifi_autoconn_type_manual));
         }
 
         return true;
