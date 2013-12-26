463da07ae0ca0b6fe281c3a86f815457fe478a1b
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-12-26 15:45:19 +0200

    Clean up lock screen interface code
    - Remove unnecessary (and wrong) user check for the removal of maximize
      widgets
    
    - Remove unnecessary null checks for the widgets category
    
    - Reorder preference initialization to the same order as they appear on
      screen
    
    - Extract DPM checking into a function for future additions, remove
      unused DPM admin class, centralize LockPatternUtils, clean up comments
      and wrap to 100 lines.
    
    Change-Id: I94159fec7b1604e290829d2ddc2eb07bf5b015e7

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -17,7 +17,6 @@
 package com.android.settings.cyanogenmod;
 
 import android.app.ActivityManager;
-import android.app.admin.DeviceAdminReceiver;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.os.Bundle;
@@ -35,77 +34,62 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 public class LockscreenInterface extends SettingsPreferenceFragment {
-    private static final String TAG = "LockscreenInterface";
 
+    private static final String LOCKSCREEN_WIDGETS_CATEGORY = "lockscreen_widgets_category";
     private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
-    private static final String LOCKSCREEN_WIDGETS_CATEGORY = "lockscreen_widgets_category";
 
     private CheckBoxPreference mEnableKeyguardWidgets;
 
     private ChooseLockSettingsHelper mChooseLockSettingsHelper;
+    private LockPatternUtils mLockUtils;
     private DevicePolicyManager mDPM;
-    private boolean mIsPrimary;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
 
         mChooseLockSettingsHelper = new ChooseLockSettingsHelper(getActivity());
-        mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mLockUtils = mChooseLockSettingsHelper.utils();
+        mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 
-        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
-        PreferenceCategory widgetsCategory = (PreferenceCategory) findPreference(LOCKSCREEN_WIDGETS_CATEGORY);
+        // Find categories
+        PreferenceCategory widgetsCategory = (PreferenceCategory)
+                findPreference(LOCKSCREEN_WIDGETS_CATEGORY);
 
-        // Only add if device has LockClock installed
+        // Find preferences
+        mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
+
+        // Remove/disable custom widgets based on device RAM and policy
+        if (ActivityManager.isLowRamDeviceStatic()) {
+            // Widgets take a lot of RAM, so disable them on low-memory devices
+            widgetsCategory.removePreference(findPreference(KEY_ENABLE_WIDGETS));
+            mEnableKeyguardWidgets = null;
+        } else {
+            checkDisabledByPolicy(mEnableKeyguardWidgets,
+                    DevicePolicyManager.KEYGUARD_DISABLE_WIDGETS_ALL);
+        }
+
+        // Remove cLock settings item if not installed
         if (!isPackageInstalled("com.cyanogenmod.lockclock")) {
             widgetsCategory.removePreference(findPreference(KEY_LOCK_CLOCK));
         }
 
-        // Determine which user is logged in
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
-        if (mIsPrimary) {
-            // Its the primary user, show all the settings
-            if (!Utils.isPhone(getActivity())) {
-                if (widgetsCategory != null) {
-                    widgetsCategory.removePreference(
-                            findPreference(Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS));
-                }
-            }
-
-        } else {
-            // Secondary user is logged in, remove all primary user specific preferences
-        }
-
-        // This applies to all users
-        // Enable or disable keyguard widget checkbox based on DPM state
-        mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
-        if (mEnableKeyguardWidgets != null) {
-            if (ActivityManager.isLowRamDeviceStatic()) {
-                    /*|| mLockPatternUtils.isLockScreenDisabled()) {*/
-                // Widgets take a lot of RAM, so disable them on low-memory devices
-                if (widgetsCategory != null) {
-                    widgetsCategory.removePreference(findPreference(KEY_ENABLE_WIDGETS));
-                    mEnableKeyguardWidgets = null;
-                }
-            } else {
-                final boolean disabled = (0 != (mDPM.getKeyguardDisabledFeatures(null)
-                        & DevicePolicyManager.KEYGUARD_DISABLE_WIDGETS_ALL));
-                if (disabled) {
-                    mEnableKeyguardWidgets.setSummary(
-                            R.string.security_enable_widgets_disabled_summary);
-                }
-                mEnableKeyguardWidgets.setEnabled(!disabled);
-            }
+        // Remove maximize widgets on tablets
+        if (!Utils.isPhone(getActivity())) {
+            widgetsCategory.removePreference(
+                    findPreference(Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS));
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        final LockPatternUtils lockPatternUtils = mChooseLockSettingsHelper.utils();
+
+        // Update custom widgets
         if (mEnableKeyguardWidgets != null) {
-            mEnableKeyguardWidgets.setChecked(lockPatternUtils.getWidgetsEnabled());
+            mEnableKeyguardWidgets.setChecked(mLockUtils.getWidgetsEnabled());
         }
     }
 
@@ -113,15 +97,37 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         final String key = preference.getKey();
 
-        final LockPatternUtils lockPatternUtils = mChooseLockSettingsHelper.utils();
         if (KEY_ENABLE_WIDGETS.equals(key)) {
-            lockPatternUtils.setWidgetsEnabled(mEnableKeyguardWidgets.isChecked());
+            mLockUtils.setWidgetsEnabled(mEnableKeyguardWidgets.isChecked());
             return true;
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    public static class DeviceAdminLockscreenReceiver extends DeviceAdminReceiver {}
+    /**
+     * Checks if a specific policy is disabled by a device administrator, and disables the
+     * provided preference if so.
+     * @param preference Preference
+     * @param feature Feature
+     */
+    private void checkDisabledByPolicy(Preference preference, int feature) {
+        boolean disabled = featureIsDisabled(feature);
+
+        if (disabled) {
+            preference.setSummary(R.string.security_enable_widgets_disabled_summary);
+        }
+
+        preference.setEnabled(!disabled);
+    }
+
+    /**
+     * Checks if a specific policy is disabled by a device administrator.
+     * @param feature Feature
+     * @return Is disabled
+     */
+    private boolean featureIsDisabled(int feature) {
+        return (mDPM.getKeyguardDisabledFeatures(null) & feature) != 0;
+    }
 
 }
