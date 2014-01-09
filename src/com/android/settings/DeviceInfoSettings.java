ef6bb45a99820a81a1dde0b751a43392a9747646
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-09 10:33:23 +0000

    DeviceInfoSettings: Use ro.cm.display.version instead of ro.cm.version
    Follows the same logic as ro.build.display.id/ro.build.id:
    engineering, debug and unsigned builds will have no distinction,
    user and signed builds will show a simplified version
    
    Change-Id: I713ccb2445ea2e333e831129ef7733fd518282eb
    (cherry picked from commit f9ae9cfcac2cb6b767775e42b400a7d3ea6a5530)

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -100,7 +100,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_MOD_VERSION, "ro.cm.version");
+        setValueSummary(KEY_MOD_VERSION, "ro.cm.display.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
