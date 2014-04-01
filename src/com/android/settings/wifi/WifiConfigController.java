b2cb2c3d459e3f371ca9b54072fe0dfd7d9a9f64
kaiyiz <kaiyiz@codeaurora.org>
2014-04-01 17:25:07 +0800

    Settings: Fix setting options in CMCC-AUTO issue.
    There are the codes that display other setting options in
    CMCC-AUTO in the source code.
    
    Change the codes to hide some setting options.
    
    CRs-Fixed: 635551
    
    Change-Id: Iacf2ec4168d566a9cccbdbf2894547d469636302

--- src/com/android/settings/wifi/WifiConfigController.java
+++ src/com/android/settings/wifi/WifiConfigController.java
@@ -118,6 +118,8 @@ public class WifiConfigController implements TextWatcher,
 
     private static final String TAG = "WifiConfigController";
 
+    private static final String CARRIER_AUTO_SSID = "CMCC-AUTO";
+
     private Spinner mIpSettingsSpinner;
     private TextView mIpAddressView;
     private TextView mGatewayView;
@@ -261,6 +263,12 @@ public class WifiConfigController implements TextWatcher,
                 if (showAdvancedFields) {
                     ((CheckBox)mView.findViewById(R.id.wifi_advanced_togglebox)).setChecked(true);
                     mView.findViewById(R.id.wifi_advanced_fields).setVisibility(View.VISIBLE);
+                }
+            }
+
+            if (CARRIER_AUTO_SSID.equals(mAccessPoint.ssid) && mEdit) {
+                if (mAccessPoint.security == AccessPoint.SECURITY_EAP) {
+                    mView.findViewById(R.id.wifi_advanced_toggle).setVisibility(View.GONE);
                 }
             }
 
@@ -640,6 +648,16 @@ public class WifiConfigController implements TextWatcher,
             }
         } else {
             showEapFieldsByMethod(mEapMethodSpinner.getSelectedItemPosition());
+        }
+
+        if (mAccessPoint != null && CARRIER_AUTO_SSID.equals(mAccessPoint.ssid) && mEdit) {
+            if (mAccessPoint.security == AccessPoint.SECURITY_EAP) {
+                mView.findViewById(R.id.l_method).setVisibility(View.GONE);
+                setPhase2Invisible();
+                setCaCertInvisible();
+                setAnonymousIdentInvisible();
+                setUserCertInvisible();
+            }
         }
     }
 
