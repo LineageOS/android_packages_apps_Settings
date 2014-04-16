4e08d0e50eb4bd1b4afaf69b8d19eaee6eb7821b
Roman Birg <roman@cyngn.com>
2014-04-16 13:31:10 -0700

    Settings: Remove Wallpaper selection from Display
    Let the launcher handle wallpaper selection.
    
    Change-Id: I7f0eb20a5bc01d82fdb98905524b9743fee447c6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1144,9 +1144,7 @@ public class Settings extends PreferenceActivity
     public boolean onPreferenceStartFragment(PreferenceFragment caller, Preference pref) {
         // Override the fragment title for Wallpaper settings
         int titleRes = pref.getTitleRes();
-        if (pref.getFragment().equals(WallpaperTypeSettings.class.getName())) {
-            titleRes = R.string.wallpaper_settings_fragment_title;
-        } else if (pref.getFragment().equals(OwnerInfoSettings.class.getName())
+        if (pref.getFragment().equals(OwnerInfoSettings.class.getName())
                 && UserHandle.myUserId() != UserHandle.USER_OWNER) {
             if (UserManager.get(this).isLinkedUser()) {
                 titleRes = R.string.profile_info_settings_title;
