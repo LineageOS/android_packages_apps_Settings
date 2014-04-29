35a3d7bbcd7be5b3954786938c582a5bd3c6bb43
Koushik Dutta <koushd@gmail.com>
2014-04-29 01:09:45 -0700

    Major refactor: switch to native fragments.
    Change-Id: I00013d14fed16e0d67aac8452eecd76df454be14

--- src/com/android/settings/cyanogenmod/superuser/PolicyNativeFragment.java
+++ src/com/android/settings/cyanogenmod/superuser/PolicyNativeFragment.java
@@ -1,55 +1,85 @@
 package com.android.settings.cyanogenmod.superuser;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.android.settings.Utils;
-import com.koushikdutta.superuser.LogNativeFragment;
-import com.koushikdutta.superuser.PolicyFragmentInternal;
-import com.koushikdutta.superuser.SettingsNativeFragment;
+import com.android.settings.R;
 
-public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyNativeFragment {
+import com.android.settings.Utils;
+import com.koushikdutta.superuser.LogFragment;
+import com.koushikdutta.superuser.PolicyFragment;
+import com.koushikdutta.superuser.SettingsFragment;
+
+public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
-        Utils.forcePrepareCustomPreferencesList(container, view, getInternal().getListView(), false);
+        Utils.forcePrepareCustomPreferencesList(container, view, getListView(), false);
         return view;
     }
 
+    ContextThemeWrapper mWrapper;
     @Override
-    public PolicyFragmentInternal createFragmentInterface() {
-        return new FragmentInternal(this) {
-            @Override
-            protected LogNativeFragment createLogNativeFragment() {
-                return new LogNativeFragment() {
-                    @Override
-                    public View onCreateView(LayoutInflater inflater,
-                            ViewGroup container, Bundle savedInstanceState) {
-                        View view = super.onCreateView(inflater, container, savedInstanceState);
-                        adjustListPadding(getInternal().getListView());
-                        return view;
-                    }
-                };
-            }
-
-            @Override
-            protected SettingsNativeFragment createSettingsNativeFragment() {
-                return new SettingsNativeFragment() {
-                    @Override
-                    public View onCreateView(LayoutInflater inflater,
-                            ViewGroup container, Bundle savedInstanceState) {
-                        View view = super.onCreateView(inflater, container, savedInstanceState);
-                        adjustListPadding(getInternal().getListView());
-                        return view;
-                    }
-                };
-            };
-        };
+    public Context getContext() {
+        if (mWrapper != null)
+            return mWrapper;
+        mWrapper = new ContextThemeWrapper(super.getContext(), R.style.SuperuserDark_LargeIcon);
+        return mWrapper;
     }
+
+    public static class EmbeddedSettingsFragment extends SettingsFragment {
+        ContextThemeWrapper mWrapper;
+        @Override
+        public Context getContext() {
+            if (mWrapper != null)
+                return mWrapper;
+            mWrapper = new ContextThemeWrapper(super.getContext(), R.style.SuperuserDark_LargeIcon);
+            return mWrapper;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                ViewGroup container, Bundle savedInstanceState) {
+            View view = super.onCreateView(inflater, container, savedInstanceState);
+            adjustListPadding(getListView());
+            return view;
+        }
+    }
+
+    public static class EmbeddedLogFragment extends LogFragment {
+        ContextThemeWrapper mWrapper;
+        @Override
+        public Context getContext() {
+            if (mWrapper != null)
+                return mWrapper;
+            mWrapper = new ContextThemeWrapper(super.getContext(), R.style.SuperuserDark_LargeIcon);
+            return mWrapper;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                ViewGroup container, Bundle savedInstanceState) {
+            View view = super.onCreateView(inflater, container, savedInstanceState);
+            adjustListPadding(getListView());
+            return view;
+        }
+    }
+
+    @Override
+    protected LogFragment createLogFragment() {
+        return new EmbeddedLogFragment();
+    }
+
+    @Override
+    protected SettingsFragment createSettingsFragment() {
+        return new EmbeddedSettingsFragment();
+    };
 
     private static void adjustListPadding(ListView list) {
         final Resources res = list.getResources();
