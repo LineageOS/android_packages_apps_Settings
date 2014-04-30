9a1803f16b93e6cb8c1b250bd035fad2c187fe47
Hamster Tian <th0928@126.com>
2014-04-30 16:11:31 +0200

    [3/3]Settings: add "Accept all files" option for incoming files via BT
     * Adapted for CM13.
     * PS2: empty line fix
     * PS3: variable name fix
    
      BT server in AOSP will check MIME type of incoming file. Only those with
    explicitly allowed types will be accepted. The MIME type whitelist is in
    packages/apps/Bluetooth/src/com/android/bluetooth/opp/Constants.java
      But this can be annoying when we want to transfer RAR, APK or conf files
    etc which do not exist in the list.
      This patch adds the option "Accept all files" in
              Settings -> Bluetooth -> menu.
      This option is disabled by default, which ensures security.
    
    Ref CYNGNOS-1825
    
    Change-Id: I76e008211429bf3bd28183713043bee5d326c21a
    (cherry picked from commit d7271e4de872e2a33a2102f4f283a4a35747d337)

--- src/com/android/settings/bluetooth/BluetoothSettings.java
+++ src/com/android/settings/bluetooth/BluetoothSettings.java
@@ -75,6 +75,7 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
     private static final int MENU_ID_SCAN = Menu.FIRST;
     private static final int MENU_ID_RENAME_DEVICE = Menu.FIRST + 1;
     private static final int MENU_ID_SHOW_RECEIVED = Menu.FIRST + 2;
+    private static final int MENU_ID_ACCEPT_ALL_FILES = Menu.FIRST + 3;
 
     /* Private intent to show the list of received files */
     private static final String BTOPP_ACTION_OPEN_RECEIVED_FILES =
@@ -270,6 +271,10 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
         boolean isDiscovering = mLocalAdapter.isDiscovering();
         int textId = isDiscovering ? R.string.bluetooth_searching_for_devices :
             R.string.bluetooth_search_for_devices;
+
+        boolean isAcceptAllFilesEnabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.BLUETOOTH_ACCEPT_ALL_FILES, 0) == 1;
+
         menu.add(Menu.NONE, MENU_ID_SCAN, 0, textId)
                 .setEnabled(bluetoothIsEnabled && !isDiscovering)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
@@ -277,6 +282,10 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
                 .setEnabled(bluetoothIsEnabled)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(Menu.NONE, MENU_ID_SHOW_RECEIVED, 0, R.string.bluetooth_show_received_files)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(Menu.NONE, MENU_ID_ACCEPT_ALL_FILES, 0, R.string.bluetooth_accept_all_files)
+                .setCheckable(true)
+                .setChecked(isAcceptAllFilesEnabled)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -301,6 +310,13 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
                 MetricsLogger.action(getActivity(), MetricsLogger.ACTION_BLUETOOTH_FILES);
                 Intent intent = new Intent(BTOPP_ACTION_OPEN_RECEIVED_FILES);
                 getActivity().sendBroadcast(intent);
+                return true;
+
+            case MENU_ID_ACCEPT_ALL_FILES:
+                item.setChecked(!item.isChecked());
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.BLUETOOTH_ACCEPT_ALL_FILES,
+                        item.isChecked() ? 1 : 0);
                 return true;
         }
         return super.onOptionsItemSelected(item);
