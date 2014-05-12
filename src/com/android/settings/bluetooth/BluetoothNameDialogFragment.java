4f6f1c84ae932bcc54c6aa59f9700ca479c1dcd3
Pramod Sivaraman <spramod@codeaurora.org>
2014-05-12 19:53:12 +0530

    Bluetooth:apps: Fix to avoid taking space as name upon orientation
    This fix is to avoid taking space character as a name in the
    BluetoothNameDialogFragment upon orientation change from
    PORTRAIT mode to LANDSCAPE mode and vice versa.
    
    Change-Id: Ia66f27bb9b06739e42d73b15745c12a42512a1fc
    CRs-Fixed: 650910

--- src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
+++ src/com/android/settings/bluetooth/BluetoothNameDialogFragment.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -190,6 +191,13 @@ public final class BluetoothNameDialogFragment extends DialogFragment implements
             if (mOkButton != null) {
                 mOkButton.setEnabled(s.length() != 0 && !(s.toString().trim().isEmpty()));
             }
+        }
+    }
+
+    public void onConfigurationChanged(Configuration newConfig, CharSequence s) {
+        super.onConfigurationChanged(newConfig);
+        if (mOkButton != null) {
+            mOkButton.setEnabled(s.length() != 0 && !(s.toString().trim().isEmpty()));
         }
     }
 
