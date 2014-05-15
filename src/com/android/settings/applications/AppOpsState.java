73ae73814f45480108256a3ef55472d684a9e5da
Kun Liang <kunliang@codeaurora.org>
2014-05-15 18:55:38 +0800

    AppOps: put Wifi/BT/Data into proper template
    Wifi/BT/Data should not be put into "media" template. Move them
    into "device" template.
    
    Change-Id: I9f96c1fdb3e20da211e863fd8e8c3851c222aca7

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -172,10 +172,7 @@ public class AppOpsState {
                     AppOpsManager.OP_AUDIO_MEDIA_VOLUME,
                     AppOpsManager.OP_AUDIO_ALARM_VOLUME,
                     AppOpsManager.OP_AUDIO_NOTIFICATION_VOLUME,
-                    AppOpsManager.OP_AUDIO_BLUETOOTH_VOLUME,
-                    AppOpsManager.OP_WIFI_CHANGE,
-                    AppOpsManager.OP_BLUETOOTH_CHANGE,
-                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
+                    AppOpsManager.OP_AUDIO_BLUETOOTH_VOLUME },
             new boolean[] { false,
                     true,
                     true,
@@ -188,10 +185,7 @@ public class AppOpsState {
                     false,
                     false,
                     false,
-                    false,
-                    true,
-                    true,
-                    true }
+                    false }
             );
 
     public static final OpsTemplate DEVICE_TEMPLATE = new OpsTemplate(
@@ -200,13 +194,19 @@ public class AppOpsState {
                     AppOpsManager.OP_CALL_PHONE,
                     AppOpsManager.OP_WRITE_SETTINGS,
                     AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
-                    AppOpsManager.OP_WAKE_LOCK },
+                    AppOpsManager.OP_WAKE_LOCK,
+                    AppOpsManager.OP_WIFI_CHANGE,
+                    AppOpsManager.OP_BLUETOOTH_CHANGE,
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
             new boolean[] { false,
                     true,
                     true,
                     true,
                     true,
-                    true,  }
+                    true,
+                    true,
+                    true,
+                    true }
             );
 
     public static final OpsTemplate BOOTUP_TEMPLATE = new OpsTemplate(
