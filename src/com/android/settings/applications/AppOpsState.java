19f00e4c2d2df1e6f0fe8751c30782ac614fb51e
Shashank Mittal <mittals@codeaurora.org>
2014-06-06 19:35:30 -0700

    AppOps: Clean up AppOps settings.
    Remove unused operations
    
    Change-Id: I20b3cd3eae5a8e669ced9767b9a9602848ab65a1

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -119,25 +119,17 @@ public class AppOpsState {
                     AppOpsManager.OP_READ_CALL_LOG,
                     AppOpsManager.OP_WRITE_CALL_LOG,
                     AppOpsManager.OP_READ_CALENDAR,
-                    AppOpsManager.OP_WRITE_CALENDAR,
-                    AppOpsManager.OP_READ_CLIPBOARD,
-                    AppOpsManager.OP_WRITE_CLIPBOARD },
+                    AppOpsManager.OP_WRITE_CALENDAR },
             new boolean[] { true,
                     true,
                     true,
                     true,
                     true,
-                    true,
-                    false,
-                    false }
+                    true }
             );
 
     public static final OpsTemplate MESSAGING_TEMPLATE = new OpsTemplate(
             new int[] { AppOpsManager.OP_READ_SMS,
-                    AppOpsManager.OP_RECEIVE_SMS,
-                    AppOpsManager.OP_RECEIVE_EMERGECY_SMS,
-                    AppOpsManager.OP_RECEIVE_MMS,
-                    AppOpsManager.OP_RECEIVE_WAP_PUSH,
                     AppOpsManager.OP_WRITE_SMS,
                     AppOpsManager.OP_SEND_SMS,
                     AppOpsManager.OP_READ_ICC_SMS,
@@ -146,61 +138,19 @@ public class AppOpsState {
                     true,
                     true,
                     true,
-                    true,
-                    true,
-                    true,
-                    true,
                     true }
             );
 
     public static final OpsTemplate MEDIA_TEMPLATE = new OpsTemplate(
-            new int[] { AppOpsManager.OP_VIBRATE,
-                    AppOpsManager.OP_CAMERA,
-                    AppOpsManager.OP_RECORD_AUDIO,
-                    AppOpsManager.OP_PLAY_AUDIO,
-                    AppOpsManager.OP_TAKE_MEDIA_BUTTONS,
-                    AppOpsManager.OP_TAKE_AUDIO_FOCUS,
-                    AppOpsManager.OP_AUDIO_MASTER_VOLUME,
-                    AppOpsManager.OP_AUDIO_VOICE_VOLUME,
-                    AppOpsManager.OP_AUDIO_RING_VOLUME,
-                    AppOpsManager.OP_AUDIO_MEDIA_VOLUME,
-                    AppOpsManager.OP_AUDIO_ALARM_VOLUME,
-                    AppOpsManager.OP_AUDIO_NOTIFICATION_VOLUME,
-                    AppOpsManager.OP_AUDIO_BLUETOOTH_VOLUME,
-                    AppOpsManager.OP_MUTE_MICROPHONE},
-            new boolean[] { false,
-                    true,
-                    true,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false }
+            new int[] { AppOpsManager.OP_CAMERA,
+                    AppOpsManager.OP_RECORD_AUDIO },
+            new boolean[] { true,
+                    true }
             );
 
     public static final OpsTemplate DEVICE_TEMPLATE = new OpsTemplate(
-            new int[] { AppOpsManager.OP_POST_NOTIFICATION,
-                    AppOpsManager.OP_ACCESS_NOTIFICATIONS,
-                    AppOpsManager.OP_CALL_PHONE,
-                    AppOpsManager.OP_WRITE_SETTINGS,
-                    AppOpsManager.OP_SYSTEM_ALERT_WINDOW,
-                    AppOpsManager.OP_WAKE_LOCK,
-                    AppOpsManager.OP_PROJECT_MEDIA,
-                    AppOpsManager.OP_ACTIVATE_VPN, },
-            new boolean[] { false,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    false,
-                    false, }
+            new int[] { AppOpsManager.OP_CALL_PHONE },
+            new boolean[] { true }
             );
 
     public static final OpsTemplate[] ALL_TEMPLATES = new OpsTemplate[] {
@@ -545,6 +495,14 @@ public class AppOpsState {
         }
         for (int i=0; i<apps.size(); i++) {
             PackageInfo appInfo = apps.get(i);
+            if (packageName == null && appInfo.packageName != null) {
+                try {
+                    appInfo = mPm.getPackageInfo(appInfo.packageName, PackageManager.GET_PERMISSIONS);
+                } catch (NameNotFoundException e) {
+                    if (DEBUG) Log.w(TAG, "Exception: " + e.toString());
+                    appInfo = apps.get(i);
+                }
+            }
             AppEntry appEntry = getAppEntry(context, appEntries, appInfo.packageName,
                     appInfo.applicationInfo);
             if (appEntry == null) {
