d6668d9bd25edefb962ad29e6b51f9e7cfa5d458
Raj Yengisetty <rajesh@cyngn.com>
2014-06-16 19:06:08 -0700

    Protected Apps:
     - App Info for protected Apps doesn't allow Uninstall or Clear data
     - App Info menu item for launching into Protected Apps when looking at protected components
    
    Change-Id: I64104d7ff3fbf9d8c393ebf262d4de0b28abbc5c

--- src/com/android/settings/cyanogenmod/ProtectedAppsReceiver.java
+++ src/com/android/settings/cyanogenmod/ProtectedAppsReceiver.java
@@ -13,12 +13,12 @@ import java.util.ArrayList;
 public class ProtectedAppsReceiver extends BroadcastReceiver {
     private static final String TAG = "ProtectedAppsReceiver";
 
-    private static final String PROTECTED_ACTION = "cyanogenmod.intent.action.PACKAGE_PROTECTED";
+    public static final String PROTECTED_ACTION = "cyanogenmod.intent.action.PACKAGE_PROTECTED";
     private static final String PROTECTED_CHANGED_ACTION =
             "cyanogenmod.intent.action.PROTECTED_COMPONENT_UPDATE";
-    private static final String PROTECTED_STATE =
+    public static final String PROTECTED_STATE =
             "cyanogenmod.intent.action.PACKAGE_PROTECTED_STATE";
-    private static final String PROTECTED_COMPONENT =
+    public static final String PROTECTED_COMPONENT =
             "cyanogenmod.intent.action.PACKAGE_PROTECTED_COMPONENT";
     private static final String PROTECTED_APP_PERMISSION = "cyanogenmod.permission.PROTECTED_APP";
 
