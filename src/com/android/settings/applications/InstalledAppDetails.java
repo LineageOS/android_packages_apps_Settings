732ffe302059da0d03cc54ec2dddd85575b4f9bc
Danny Baumann <dannybaumann@web.de>
2014-07-01 09:46:21 +0200

    Clean up protected apps code (1/2)
    - Work with actual ComponentNames instead of converting them between
      String and ComponentName all the time
    - Name protection state parameter in methods 'state' instead of
      'protect', as a value of true actually means it's not protected. Also
      consistently use the respective constants for its values.
    - Some misc. cleanup
    
    Change-Id: I2855978c8aef3cfa14249e3398039c7cdd145ede
    (cherry picked from commit 54108193c4c78cb085e5b07613b110d542116e83)

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -619,14 +619,13 @@ public class InstalledAppDetails extends Fragment
 
         @Override
         protected Void doInBackground(Void... params) {
-            String components = "";
+            ArrayList<ComponentName> components = new ArrayList<ComponentName>();
             for (ActivityInfo aInfo : mPackageInfo.activities) {
-                components += new ComponentName(aInfo.packageName, aInfo.name)
-                        .flattenToString() + "|";
+                components.add(new ComponentName(aInfo.packageName, aInfo.name));
             }
 
-            ProtectedAppsReceiver.protectedAppComponentsAndNotify
-                    (components, true, getActivity());
+            ProtectedAppsReceiver.updateProtectedAppComponentsAndNotify(getActivity(),
+                    components, PackageManager.COMPONENT_VISIBLE_STATUS);
             return null;
         }
     }
