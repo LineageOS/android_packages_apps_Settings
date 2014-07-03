bd887577587dc8875b3a03c2aa2728c0ceeb03af
Adnan <adnan@cyngn.com>
2014-07-02 18:43:15 -0700

    Settings: Don't match action bar parent for search on tablets.
    Change-Id: I9bc9851d39aa4d6dbed40df63dd4ea8df6f4c3f9

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -268,7 +268,10 @@ public class Settings extends PreferenceActivity
         new PopulateSearchSettingsTask().execute();
 
         ActionBar.LayoutParams layout = new ActionBar.LayoutParams(
-                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+                Utils.isPhone(this) ?
+                        ActionBar.LayoutParams.MATCH_PARENT : (int) Utils.convertDpToPixel(450, this),
+                ActionBar.LayoutParams.MATCH_PARENT);
+
         mSearchBar.setLayoutParams(layout);
         mSearchBar.setHint(R.string.settings_search_autocompleteview_hint);
         mSearchBar.setThreshold(1);
