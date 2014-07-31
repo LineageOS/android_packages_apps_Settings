fc3d9f617a4dbd89c8bf928d2866f2de5baf7f69
jianzhou <jianzhou@codeaurora.org>
2014-07-31 16:13:25 +0800

    Settings: Add the read only APN feature.
    Disable the edit screen for read only APNs.
    
    Some operators don't want their APNs to be edited by
    end user to avoid mistaken changes. End user should
    be able to only view the APNs, but not edit.
    
    Change-Id: I89f26ccc687d6989c562f164dbe53961a4d8a057

--- src/com/android/settings/ApnEditor.java
+++ src/com/android/settings/ApnEditor.java
@@ -90,6 +90,7 @@ public class ApnEditor extends PreferenceActivity
     private String mCurMnc;
     private String mCurMcc;
     private long mSubId;
+    private boolean mDisableEditor = false;
 
     private Uri mUri;
     private Cursor mCursor;
@@ -196,6 +197,12 @@ public class ApnEditor extends PreferenceActivity
         mSubId = intent.getLongExtra(SelectSubscription.SUBSCRIPTION_KEY,
                 SubscriptionManager.getDefaultSubId());
         Log.d(TAG,"ApnEditor onCreate received sub: " + mSubId);
+        mDisableEditor = intent.getBooleanExtra("DISABLE_EDITOR",false);
+        if (mDisableEditor) {
+            getPreferenceScreen().setEnabled(false);
+            Log.d(TAG, "ApnEditor form is disabled.");
+        }
+
         mFirstTime = icicle == null;
 
         if (action.equals(Intent.ACTION_EDIT)) {
@@ -452,6 +459,10 @@ public class ApnEditor extends PreferenceActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
+        if (mDisableEditor) {
+            Log.d(TAG, "Form is disabled. Do not create the options menu.");
+            return true;
+        }
         // If it's a new APN, then cancel will delete the new entry in onPause
         if (!mNewApn) {
             menu.add(0, MENU_DELETE, 0, R.string.menu_delete)
@@ -517,6 +528,12 @@ public class ApnEditor extends PreferenceActivity
         String apn = checkNotSet(mApn.getText());
         String mcc = checkNotSet(mMcc.getText());
         String mnc = checkNotSet(mMnc.getText());
+
+        // If the form is not editable, do nothing and return.
+        if(mDisableEditor){
+            Log.d(TAG, "Form is disabled. Nothing to save.");
+            return true;
+        }
 
         if (getErrorMsg() != null && !force) {
             showDialog(ERROR_DIALOG_ID);
