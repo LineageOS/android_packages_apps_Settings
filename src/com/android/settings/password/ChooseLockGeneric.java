f9fcaf2e613f1563e2887ef7e4b756ca76c3da3e
Adnan <adnan@cyngn.com>
2014-08-03 15:52:05 -0700

    Settings: Add option to scramble pin layout when unlocking (1/2).
    Change-Id: I3e2c200a0a31d3c765831bc30280029a50c88051

--- src/com/android/settings/password/ChooseLockGeneric.java
+++ src/com/android/settings/password/ChooseLockGeneric.java
@@ -97,6 +97,7 @@ import com.android.settings.security.screenlock.AutoPinConfirmPreferenceControll
 import com.android.settings.security.screenlock.LockAfterTimeoutPreferenceController;
 import com.android.settings.security.screenlock.PatternVisiblePreferenceController;
 import com.android.settings.security.screenlock.PinPrivacyPreferenceController;
+import com.android.settings.security.screenlock.PinScramblePreferenceController;
 import com.android.settings.security.screenlock.PowerButtonInstantLockPreferenceController;
 import com.android.settingslib.RestrictedPreference;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -705,6 +706,8 @@ public class ChooseLockGeneric extends SettingsActivity {
             mUnlockSettingsControllers.add(new PatternVisiblePreferenceController(
                     getContext(), mUserId, mLockPatternUtils));
             mUnlockSettingsControllers.add(new PinPrivacyPreferenceController(
+                    getContext(), mUserId, mLockPatternUtils));
+            mUnlockSettingsControllers.add(new PinScramblePreferenceController(
                     getContext(), mUserId, mLockPatternUtils));
             mUnlockSettingsControllers.add(new PowerButtonInstantLockPreferenceController(
                     getContext(), mUserId, mLockPatternUtils));
