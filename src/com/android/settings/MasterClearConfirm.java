67295975a2a1182ba4b336d470340143626f4cc7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-08-07 00:55:40 +0100

    Factory reset: Always show the "delete media" checkbox
    CM is usually installed with recoveries that preserve datamedia,
    including our own, so the assumption that deleting data will also
    clear personal media is wrong. Always show the checkbox that was
    originally limited to removable media to let the user do a full
    reset
    
    Change-Id: I2764c9509cc9ec50a1be979d748341037660a5da

--- src/com/android/settings/MasterClearConfirm.java
+++ src/com/android/settings/MasterClearConfirm.java
@@ -43,6 +43,7 @@ import android.widget.CheckBox;
 public class MasterClearConfirm extends Fragment {
 
     private View mContentView;
+    private boolean mWipeMedia;
     private boolean mEraseSdCard;
     private Button mFinalButton;
 
@@ -63,7 +64,9 @@ public class MasterClearConfirm extends Fragment {
                 intent.setComponent(ExternalStorageFormatter.COMPONENT_NAME);
                 getActivity().startService(intent);
             } else {
-                getActivity().sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
+                Intent intent = new Intent("android.intent.action.MASTER_CLEAR");
+                intent.putExtra("wipe_media", mWipeMedia);
+                getActivity().sendBroadcast(intent);
                 // Intent handling is asynchronous -- assume it will happen soon.
             }
         }
@@ -90,6 +93,7 @@ public class MasterClearConfirm extends Fragment {
         super.onCreate(savedInstanceState);
 
         Bundle args = getArguments();
+        mWipeMedia = args != null ? args.getBoolean(MasterClear.WIPE_MEDIA_EXTRA) : false;
         mEraseSdCard = args != null ? args.getBoolean(MasterClear.ERASE_EXTERNAL_EXTRA) : false;
     }
 }
