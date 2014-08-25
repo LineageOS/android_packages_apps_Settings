e5224cde2796a88b2bbfc2fb08d811be18484136
Roman Birg <roman@cyngn.com>
2014-08-25 15:36:51 -0700

    Settings: add last app action to be assigned to hardware keys
    Change-Id: I9004412528a7c32fecd7edea6167c1b80c51730d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -85,6 +85,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final int ACTION_VOICE_SEARCH = 4;
     private static final int ACTION_IN_APP_SEARCH = 5;
     private static final int ACTION_LAUNCH_CAMERA = 6;
+    private static final int ACTION_LAST_APP = 7;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
