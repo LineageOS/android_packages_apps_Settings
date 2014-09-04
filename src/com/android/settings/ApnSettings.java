5906f5d52b1430dd2d7020e48f93e61a5c3fde30
jianzhou <jianzhou@codeaurora.org>
2014-09-04 17:04:38 +0800

    Settings: Change APN update logic
    PhoneFeature change APN update logic, no need to send broadcast for this.
    
    Use provider method to update APN.
    
    Change-Id: Ia161819366b0f4044a0a8df24649e1c632f365c4

--- src/com/android/settings/ApnSettings.java
+++ src/com/android/settings/ApnSettings.java
@@ -117,7 +117,8 @@ public class ApnSettings extends SettingsPreferenceFragment implements
 
     private boolean mUnavailable;
 
-    private static final String ACTION_APN_RESRORE_COMPLETE = "com.android.apnsettings.RESRORE_COMPLETE";
+    private static final Uri URI_PHONE_FEATURE = Uri
+            .parse("content://com.qualcomm.qti.phonefeature.FEATURE_PROVIDER");
 
     private final BroadcastReceiver mMobileStateReceiver = new BroadcastReceiver() {
         @Override
@@ -480,7 +481,11 @@ public class ApnSettings extends SettingsPreferenceFragment implements
                         mRestoreDefaultApnMode = false;
                         return;
                     }
-                    getActivity().sendBroadcast(new Intent(ACTION_APN_RESRORE_COMPLETE));
+                    Bundle result = call("update_apn_by_area", null);
+                    if (result == null || result.getBoolean("result", false)) {
+                        Log.d(TAG, "failed to update APN");
+                    }
+
                     fillList();
                     getPreferenceScreen().setEnabled(true);
                     mRestoreDefaultApnMode = false;
@@ -551,5 +556,12 @@ public class ApnSettings extends SettingsPreferenceFragment implements
             result.add(iccOperatorNumeric);
         }
         return result.toArray(new String[2]);
+    }
+
+    private Bundle call(String method, Bundle extras) {
+        if (getContentResolver().acquireProvider(URI_PHONE_FEATURE) == null) {
+            return null;
+        }
+        return getContentResolver().call(URI_PHONE_FEATURE, method, null, extras);
     }
 }
