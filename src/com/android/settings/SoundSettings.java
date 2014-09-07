2fe58e13c49e259d42d76a3fd48baf18a19c2914
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-07 08:07:09 -0400

    Settings: "Vibrate during call" requires VOICE_CAPABLE
    No need to have this option available if you are on a wifi-only
    device.
    
    Change-Id: I30e91be73b5dd058c35ca4f5316563fa63d30d1c

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -103,7 +103,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String[] NEED_VOICE_CAPABILITY = {
             KEY_RINGTONE, KEY_DTMF_TONE, KEY_CATEGORY_CALLS,
             KEY_EMERGENCY_TONE, KEY_INCREASING_RING, KEY_VIBRATE,
-            KEY_VOLUME_ADJUST_SOUNDS
+            KEY_VOLUME_ADJUST_SOUNDS, KEY_VIBRATE_DURING_CALLS
     };
 
     private static final int MSG_UPDATE_RINGTONE_SUMMARY = 1;
