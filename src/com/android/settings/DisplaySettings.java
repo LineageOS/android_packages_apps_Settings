ba381936fba32b43b2434dda7f1e215fa5bd719f
Roman Birg <roman@cyngn.com>
2014-09-22 11:37:50 -0700

    Settings: set proper default value for tap to wake
    The first time the display_settings.xml file gets inflated, the
    PreferenceManager takes the defaultValue and persists it in the default
    shared preferences file.
    
    When we read the double tap to wake preference,
    we assume it has a default value of true, which it uses until the user
    enters Display Settings for the first time. At that point, the default
    value is persisted, and every boot after will use the default value in
    display_settings.xml.
    
    Remove the default defined in the XML file.
    
    Change-Id: I0989dd73b6b3c42ff8649bc17eebf4e191293ee4
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -477,9 +477,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
 
         if (mTapToWake != null) {
-            final SharedPreferences prefs =
-                    PreferenceManager.getDefaultSharedPreferences(getActivity());
-            mTapToWake.setChecked(prefs.getBoolean(KEY_TAP_TO_WAKE, true));
+            mTapToWake.setChecked(TapToWake.isEnabled());
         }
 
         // Default value for wake-on-plug behavior from config.xml
@@ -597,9 +595,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     mWakeWhenPluggedOrUnplugged.isChecked() ? 1 : 0);
             return true;
         } else if (preference == mTapToWake) {
-            final SharedPreferences prefs =
-                    PreferenceManager.getDefaultSharedPreferences(getActivity());
-            prefs.edit().putBoolean(KEY_TAP_TO_WAKE, mTapToWake.isChecked()).commit();
             return TapToWake.setEnabled(mTapToWake.isChecked());
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -710,6 +705,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
         if (isTapToWakeSupported()) {
             final boolean enabled = prefs.getBoolean(KEY_TAP_TO_WAKE, true);
+            Log.d(TAG, "tap to wake: " + enabled);
             if (!TapToWake.setEnabled(enabled)) {
                 Log.e(TAG, "Failed to restore tap-to-wake settings.");
             } else {
