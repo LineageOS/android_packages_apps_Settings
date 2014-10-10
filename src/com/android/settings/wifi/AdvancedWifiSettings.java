28d7633dab8c9e11e09631e47878a9ec64497c02
PrimeDirective <activethrasher00@gmail.com>
2014-10-10 12:15:25 -0400

    Wifi notifications: notify when wifi connects [2/2]
    Options:
      off (default)
      a dry white toast
      a notification
      a notification with a sound
    
    For those of us that survive on wifi islands in small college towns this is
    pretty nice to have
    
    Any notification within 30 seconds of broadcast intent ACTION_BOOT_COMPLETED
    will be canceled to prevent startup notification spamming. 8 (default) of the
    previous connections are remembered and it will update the notification's content
    accordingly if you've restored a connection.
    
    PS2: clean up strings
    PS3: fix build & fix list preference not being updated
    
    Change-Id: I18448773c64a6a0d65e7c39d008de2b371f4d556

--- src/com/android/settings/wifi/AdvancedWifiSettings.java
+++ src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -46,6 +46,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_FREQUENCY_BAND = "frequency_band";
     private static final String KEY_COUNTRY_CODE = "wifi_countrycode";
     private static final String KEY_NOTIFY_OPEN_NETWORKS = "notify_open_networks";
+    private static final String KEY_NOTIFY_CHANGED_NETWORKS = "notify_changed_networks";
     private static final String KEY_SLEEP_POLICY = "sleep_policy";
     private static final String KEY_POOR_NETWORK_DETECTION = "wifi_poor_network_detection";
     private static final String KEY_SCAN_ALWAYS_AVAILABLE = "wifi_scan_always_available";
@@ -54,6 +55,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_WIFI_PRIORITY = "wifi_priority";
 
     private WifiManager mWifiManager;
+    private ListPreference mNotifyChangedNetwork;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,6 +82,14 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
         notifyOpenNetworks.setChecked(Settings.Global.getInt(getContentResolver(),
                 Settings.Global.WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON, 0) == 1);
         notifyOpenNetworks.setEnabled(mWifiManager.isWifiEnabled());
+
+        mNotifyChangedNetwork = (ListPreference) findPreference(KEY_NOTIFY_CHANGED_NETWORKS);
+        int notifyValue = Settings.System.getInt(getContentResolver(),
+                    Settings.System.WIFI_NETWORK_NOTIFICATIONS, 0);
+        mNotifyChangedNetwork.setValueIndex(notifyValue);
+        mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[notifyValue]);
+        mNotifyChangedNetwork.setOnPreferenceChangeListener(this);
+        mNotifyChangedNetwork.setEnabled(mWifiManager.isWifiEnabled());
 
         CheckBoxPreference poorNetworkDetection =
             (CheckBoxPreference) findPreference(KEY_POOR_NETWORK_DETECTION);
@@ -223,6 +233,15 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
                         Toast.LENGTH_SHORT).show();
                 return false;
             }
+        }
+
+        if (KEY_NOTIFY_CHANGED_NETWORKS.equals(key)) {
+            int notifyValue = Integer.valueOf((String) newValue);
+            int index = mNotifyChangedNetwork.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.WIFI_NETWORK_NOTIFICATIONS,
+                    notifyValue);
+            mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[index]);
+            getActivity().sendBroadcast(new Intent("cm.UPDATE_WIFI_NOTIFICATION_PREFERENCE"));
         }
 
         if (KEY_COUNTRY_CODE.equals(key)) {
