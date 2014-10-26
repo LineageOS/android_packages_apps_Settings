VanirAOSP/packages_apps_Settings

Wifi notifications: notify when wifi connects [2/2]
Options:
  off (default)
  a dry white toast
  a notification
  a notification with a sound

For those of us that survive on wifi islands in small college towns this is
a nice addition

The first notification will be canceled if it happens within 30 seconds of
ACTION_BOOT_COMPLETED to help prevent startup notification spamming. 5 of the
previous connections are stored and it will update the content description
accordingly if you've reconnected to one

Change-Id: I18448773c64a6a0d65e7c39d008de2b371f4d556

src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -46,6 +46,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_FREQUENCY_BAND = "frequency_band";
     private static final String KEY_COUNTRY_CODE = "wifi_countrycode";
     private static final String KEY_NOTIFY_OPEN_NETWORKS = "notify_open_networks";
+    private static final String KEY_NOTIFY_CHANGED_NETWORKS = "notify_changed_networks";
     private static final String KEY_SLEEP_POLICY = "sleep_policy";
     private static final String KEY_POOR_NETWORK_DETECTION = "wifi_poor_network_detection";
     private static final String KEY_SCAN_ALWAYS_AVAILABLE = "wifi_scan_always_available";
@@ -54,6 +55,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_WIFI_PRIORITY = "wifi_priority";
 
     private WifiManager mWifiManager;
+    private ListPreference mNotifyChangedNetwork;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,6 +82,14 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
         notifyOpenNetworks.setChecked(Settings.Global.getInt(getContentResolver(),
                 Settings.Global.WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON, 0) == 1);
         notifyOpenNetworks.setEnabled(mWifiManager.isWifiEnabled());
+
+        mNotifyChangedNetwork = (ListPreference) findPreference(KEY_NOTIFY_CHANGED_NETWORKS);
+        int notifyValue = Settings.System.getInt(getContentResolver(),
+                    Settings.System.NETWORK_NOTIFICATIONS, 0);
+        mNotifyChangedNetwork.setValueIndex(notifyValue);
+        mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[notifyValue]);
+        mNotifyChangedNetwork.setOnPreferenceChangeListener(this);
+        mNotifyChangedNetwork.setEnabled(mWifiManager.isWifiEnabled());
 
         CheckBoxPreference poorNetworkDetection =
             (CheckBoxPreference) findPreference(KEY_POOR_NETWORK_DETECTION);
@@ -223,6 +233,16 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
                         Toast.LENGTH_SHORT).show();
                 return false;
             }
+        }
+
+        if (preference == mNotifyChangedNetwork) {
+            int notifyValue = Integer.valueOf((String) newValue);
+            int index = mNotifyChangedNetwork.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.NETWORK_NOTIFICATIONS,
+                    notifyValue);
+            mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[index]);
+            mContext.sendBroadcast(new Intent("com.vanir.UPDATE_NETWORK_PREFERENCES"));
+            return false;
         }
 
         if (KEY_COUNTRY_CODE.equals(key)) {

LineageOS/android_packages_apps_Settings

src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -46,6 +46,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_FREQUENCY_BAND = "frequency_band";
     private static final String KEY_COUNTRY_CODE = "wifi_countrycode";
     private static final String KEY_NOTIFY_OPEN_NETWORKS = "notify_open_networks";
+    private static final String KEY_NOTIFY_CHANGED_NETWORKS = "notify_changed_networks";
     private static final String KEY_SLEEP_POLICY = "sleep_policy";
     private static final String KEY_POOR_NETWORK_DETECTION = "wifi_poor_network_detection";
     private static final String KEY_SCAN_ALWAYS_AVAILABLE = "wifi_scan_always_available";
@@ -54,6 +55,7 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
     private static final String KEY_WIFI_PRIORITY = "wifi_priority";
 
     private WifiManager mWifiManager;
+    private ListPreference mNotifyChangedNetwork;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,6 +82,14 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
         notifyOpenNetworks.setChecked(Settings.Global.getInt(getContentResolver(),
                 Settings.Global.WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON, 0) == 1);
         notifyOpenNetworks.setEnabled(mWifiManager.isWifiEnabled());
+
+        mNotifyChangedNetwork = (ListPreference) findPreference(KEY_NOTIFY_CHANGED_NETWORKS);
+        int notifyValue = Settings.System.getInt(getContentResolver(),
+                    Settings.System.WIFI_NETWORK_NOTIFICATIONS, 0);
+        mNotifyChangedNetwork.setValueIndex(notifyValue);
+        mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[notifyValue]);
+        mNotifyChangedNetwork.setOnPreferenceChangeListener(this);
+        mNotifyChangedNetwork.setEnabled(mWifiManager.isWifiEnabled());
 
         CheckBoxPreference poorNetworkDetection =
             (CheckBoxPreference) findPreference(KEY_POOR_NETWORK_DETECTION);
@@ -223,6 +233,16 @@ public class AdvancedWifiSettings extends SettingsPreferenceFragment
                         Toast.LENGTH_SHORT).show();
                 return false;
             }
+        }
+
+        if (preference == mNotifyChangedNetwork) {
+            int notifyValue = Integer.valueOf((String) newValue);
+            int index = mNotifyChangedNetwork.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.WIFI_NETWORK_NOTIFICATIONS,
+                    notifyValue);
+            mNotifyChangedNetwork.setSummary(mNotifyChangedNetwork.getEntries()[index]);
+            mContext.sendBroadcast(new Intent("com.vanir.UPDATE_NETWORK_PREFERENCES"));
+            return false;
         }
 
         if (KEY_COUNTRY_CODE.equals(key)) {