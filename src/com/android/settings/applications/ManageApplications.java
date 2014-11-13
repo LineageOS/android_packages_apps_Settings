f547393d8f988cfe220deb154c833c8c3fc46a0c
Raj Yengisetty <rajesh@cyngn.com>
2014-11-12 17:14:27 -0800

    Protected App [2/3] Protected Apps Settings -> Apps: - Added Receiver which can send in a call to PackageManager to toggle a components protected status. - Add Protected Apps activity (available from Apps fragment) - Reads from ApplicationInfo state - Requires Pattern Lock to view/modify protected apps - Updates Settings Secure DB with protected components - Support resetting protected apps pattern lock
    Change-Id: If07a7b69ac963ffae855621881e1944fc8754782

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -181,10 +181,13 @@ public class ManageApplications extends Fragment implements
     public static final int SHOW_RUNNING_SERVICES = MENU_OPTIONS_BASE + 6;
     public static final int SHOW_BACKGROUND_PROCESSES = MENU_OPTIONS_BASE + 7;
     public static final int RESET_APP_PREFERENCES = MENU_OPTIONS_BASE + 8;
+
     // add for new feature for search applications
     public static final int SHOW_SEARCH_APPLICATIONS = MENU_OPTIONS_BASE + 9;
 
     private boolean mSearchappEnabled;
+
+    public static final int SHOW_PROTECTED_APPS = MENU_OPTIONS_BASE + 10;
 
     // sort order
     private int mSortOrder = SORT_ORDER_ALPHA;
@@ -1157,6 +1160,8 @@ public class ManageApplications extends Fragment implements
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
         menu.add(0, RESET_APP_PREFERENCES, 4, R.string.reset_app_preferences)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, SHOW_PROTECTED_APPS, 5, R.string.protected_apps)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         updateOptionsMenu();
     }
     
@@ -1194,6 +1199,7 @@ public class ManageApplications extends Fragment implements
             mOptionsMenu.findItem(SHOW_RUNNING_SERVICES).setVisible(showingBackground);
             mOptionsMenu.findItem(SHOW_BACKGROUND_PROCESSES).setVisible(!showingBackground);
             mOptionsMenu.findItem(RESET_APP_PREFERENCES).setVisible(false);
+            mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
             mShowBackground = showingBackground;
             if(mSearchappEnabled) {
                 //add for new feature for search applications
@@ -1205,10 +1211,12 @@ public class ManageApplications extends Fragment implements
             mOptionsMenu.findItem(SHOW_RUNNING_SERVICES).setVisible(false);
             mOptionsMenu.findItem(SHOW_BACKGROUND_PROCESSES).setVisible(false);
             mOptionsMenu.findItem(RESET_APP_PREFERENCES).setVisible(true);
+
             if(mSearchappEnabled) {
                 //add for new feature for search applications
                 mOptionsMenu.findItem(SHOW_SEARCH_APPLICATIONS).setVisible(true);
             }
+            mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
         }
     }
 
@@ -1327,6 +1335,10 @@ public class ManageApplications extends Fragment implements
             //add for new feature for search applications
             if(menuId == SHOW_SEARCH_APPLICATIONS){
                 showInputmethod();
+        } else if (menuId == SHOW_PROTECTED_APPS) {
+            //Launch Protected Apps Fragment
+            Intent intent = new Intent(getActivity(), ProtectedAppsActivity.class);
+            startActivity(intent);
         } else {
             // Handle the home button
             return false;
