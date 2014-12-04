c7d2d2aa5f2a3c77852b472cb2c6465b25e322e2
Diogo Ferreira <defer@cyngn.com>
2014-12-04 15:25:24 +0000

    blacklist: Make the UI more consistent
    This changes the blacklist UI to use a FAB for addition and
    also refactors the FAB code so that it can be reused by other
    activities.
    
    Change-Id: I71bdd615dfdc7b62f8dd321317a1978511a2d67e

--- src/com/android/settings/profiles/ProfileEnabler.java
+++ src/com/android/settings/profiles/ProfileEnabler.java
@@ -21,104 +21,30 @@ import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.database.ContentObserver;
-import android.net.NetworkInfo;
 import android.net.Uri;
-import android.net.wifi.SupplicantState;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Handler;
-import android.os.Message;
 import android.provider.Settings;
 import android.widget.Switch;
-import android.widget.Toast;
-import com.android.settings.R;
-import com.android.settings.WirelessSettings;
-import com.android.settings.search.Index;
 import com.android.settings.widget.SwitchBar;
-import com.android.settings.wifi.WifiSettings;
+import com.android.settings.cyanogenmod.BaseSystemSettingEnabler;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
-public class ProfileEnabler implements SwitchBar.OnSwitchChangeListener  {
+public class ProfileEnabler extends BaseSystemSettingEnabler {
     private Context mContext;
-    private SwitchBar mSwitchBar;
-    private SettingsObserver mSettingsObserver;
-    private boolean mListeningToOnSwitchChange = false;
-
-    private boolean mStateMachineEvent;
 
     public ProfileEnabler(Context context, SwitchBar switchBar) {
+        super(context, switchBar);
         mContext = context;
-        mSwitchBar = switchBar;
-        mSettingsObserver = new SettingsObserver(new Handler());
-        setupSwitchBar();
     }
 
-    public void setupSwitchBar() {
-        setSwitchState();
-        if (!mListeningToOnSwitchChange) {
-            mSwitchBar.addOnSwitchChangeListener(this);
-            mListeningToOnSwitchChange = true;
-        }
-        mSwitchBar.show();
-    }
-
-    public void teardownSwitchBar() {
-        if (mListeningToOnSwitchChange) {
-            mSwitchBar.removeOnSwitchChangeListener(this);
-            mListeningToOnSwitchChange = false;
-        }
-        mSwitchBar.hide();
-    }
-
-    public void resume(Context context) {
-        mContext = context;
-        if (!mListeningToOnSwitchChange) {
-            mSwitchBar.addOnSwitchChangeListener(this);
-            mSettingsObserver.observe();
-
-            mListeningToOnSwitchChange = true;
-        }
-    }
-
-    public void pause() {
-        if (mListeningToOnSwitchChange) {
-            mSwitchBar.removeOnSwitchChangeListener(this);
-            mSettingsObserver.unobserve();
-
-            mListeningToOnSwitchChange = false;
-        }
-    }
-
-    private void setSwitchBarChecked(boolean checked) {
-        mStateMachineEvent = true;
-        mSwitchBar.setChecked(checked);
-        mStateMachineEvent = false;
-    }
-
-    private void setSwitchState() {
-        boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1;
-        mStateMachineEvent = true;
-        setSwitchBarChecked(enabled);
-        mStateMachineEvent = false;
+    @Override
+    public String getSettingName() {
+        return Settings.System.SYSTEM_PROFILES_ENABLED;
     }
 
     @Override
     public void onSwitchChanged(Switch switchView, boolean isChecked) {
-        //Do nothing if called as a result of a state machine event
-        if (mStateMachineEvent) {
-            return;
-        }
-
-        // Handle a switch change
-        Settings.System.putInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_PROFILES_ENABLED, isChecked ? 1 : 0);
-
-        // Send a broadcast intent to the world
-        // TODO Enabling or disabling profiles should be at ProfileManager, not here
+        super.onSwitchChanged(switchView, isChecked);
         Intent intent=new Intent(ProfileManager.PROFILES_STATE_CHANGED_ACTION);
         intent.putExtra(
                 ProfileManager.EXTRA_PROFILES_STATE,
@@ -126,37 +52,5 @@ public class ProfileEnabler implements SwitchBar.OnSwitchChangeListener  {
                         ProfileManager.PROFILES_STATE_ENABLED :
                         ProfileManager.PROFILES_STATE_DISABLED);
         mContext.sendBroadcast(intent);
-    }
-
-    class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SYSTEM_PROFILES_ENABLED), false, this);
-            update();
-        }
-
-        void unobserve() {
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.unregisterContentObserver(this);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            update();
-        }
-
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            update();
-        }
-
-        public void update() {
-            setSwitchState();
-        }
     }
 }
