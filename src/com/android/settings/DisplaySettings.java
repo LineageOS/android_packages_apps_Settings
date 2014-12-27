7a59268b17afabf7acf0a8d720c55c47fe3b6f4c
repo Shareef Ali <shareefalis@gmail.com>
2014-12-27 00:40:59 -0600

    Setting: Display: Adaptive Brighrness:  fix checkbox not showing correct value
    problem:  checkbox is not showing correct value  because the kernel driver
     does not allow getting CABC values.
    
    flaw: on boot, the database is restoring the value anyways thus causing
    an inconsistant value everytime you try to go to setting
    
    fix: use database to restore that setting instead of the inconsistancy
    of kernel driver.
    
    Change-Id: I07959047f927878fcd99d36d5b7c3da53ba661be

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -369,13 +369,16 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     public void onResume() {
         super.onResume();
         updateDisplayRotationPreferenceDescription();
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mAdaptiveBacklight.getContext());
+        final boolean enabled = prefs.getBoolean(KEY_ADAPTIVE_BACKLIGHT,
+            AdaptiveBacklight.isEnabled());
         if (mAdaptiveBacklight != null) {
-            mAdaptiveBacklight.setChecked(AdaptiveBacklight.isEnabled());
+            mAdaptiveBacklight.setChecked(enabled);
         }
 
         if (mSunlightEnhancement != null) {
             if (SunlightEnhancement.isAdaptiveBacklightRequired() &&
-                    !AdaptiveBacklight.isEnabled()) {
+                    !enabled) {
                 mSunlightEnhancement.setEnabled(false);
             } else {
                 mSunlightEnhancement.setChecked(SunlightEnhancement.isEnabled());
@@ -399,10 +402,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.ACCELEROMETER_ROTATION), true,
                 mAccelerometerRotationObserver);
-
-        if (mAdaptiveBacklight != null) {
-            mAdaptiveBacklight.setChecked(AdaptiveBacklight.isEnabled());
-        }
 
         // Default value for wake-on-plug behavior from config.xml
         boolean wakeUpWhenPluggedOrUnpluggedConfig = getResources().getBoolean(
@@ -604,8 +603,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (isSunlightEnhancementSupported()) {
             final boolean enabled = prefs.getBoolean(KEY_SUNLIGHT_ENHANCEMENT,
                     SunlightEnhancement.isEnabled());
+            final boolean enabledA = prefs.getBoolean(KEY_ADAPTIVE_BACKLIGHT,
+                    AdaptiveBacklight.isEnabled());
             if (SunlightEnhancement.isAdaptiveBacklightRequired() &&
-                    !AdaptiveBacklight.isEnabled()) {
+                    !enabledA) {
                 SunlightEnhancement.setEnabled(false);
                 Log.d(TAG, "SRE requires CABC, disabled");
             } else {
