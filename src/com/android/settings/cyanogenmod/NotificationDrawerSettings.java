65c43fce37a55720190021cea7b37e98d14f5528
Jorge Martín <angel.arasthel@gmail.com>
2015-01-02 12:14:14 +0100

    Add customization for Quick Settings [2/2]
    Add a new section to customize Quick Settings.
    
    Modified DraggableGridView to arrange the items differently.
    
    Gave everything a "Material-ish" style.
    
    Inversion and Hotspot tiles can be rearranged now.
    
    Creadted a "Notification Drawer" category.
    
    frameworks/base: http://review.cyanogenmod.org/83417/
    
    Change-Id: I88f9f067e60e66d51583219c497aab39cb187fb4

---
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.cyanogenmod;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.cyanogenmod.qs.QSTiles;
+
+public class NotificationDrawerSettings extends SettingsPreferenceFragment {
+    private Preference mQSTiles;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+
+        mQSTiles = findPreference("qs_order");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int qsTileCount = QSTiles.determineTileCount(getActivity());
+        mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
+                    qsTileCount, qsTileCount));
+    }
+}
---
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.cyanogenmod.qs;
+
+import android.content.Context;
+
+import com.android.settings.R;
+
+/**
+ * This class holds the icon, the name - or the string the user sees,
+ * and the value which will be stored
+ */
+public class QSTileHolder {
+    public final int drawableId;
+    public final String value;
+    public final String name;
+
+    public QSTileHolder(int drawableId, String value, String name) {
+        this.drawableId = drawableId;
+        this.value = value;
+        this.name = name;
+    }
+
+    public static QSTileHolder from(Context context, String tileType) {
+        int resId = -1;
+        int stringId = -1;
+
+        if (tileType.equals("wifi")) {
+            resId = R.drawable.ic_settings_wireless;
+            stringId = R.string.wifi_quick_toggle_title;
+        } else if (tileType.equals("bt")) {
+            resId = R.drawable.ic_settings_bluetooth2;
+            stringId = R.string.bluetooth_settings_title;
+        } else if (tileType.equals("inversion")) {
+            resId = R.drawable.ic_settings_accessibility;
+            stringId = R.string.accessibility_display_inversion_preference_title;
+        } else if (tileType.equals("cell")) {
+            resId = R.drawable.ic_qs_signal;
+            stringId = R.string.cellular_data_title;
+        } else if (tileType.equals("airplane")) {
+            resId = R.drawable.ic_qs_airplane;
+            stringId = R.string.airplane_mode;
+        } else if (tileType.equals("rotation")) {
+            resId = R.drawable.ic_qs_rotation;
+            stringId = R.string.display_rotation_title;
+        } else if (tileType.equals("flashlight")) {
+            resId = R.drawable.ic_qs_flashlight;
+            stringId = R.string.power_flashlight;
+        } else if (tileType.equals("location")) {
+            resId = R.drawable.ic_settings_location;
+            stringId = R.string.location_title;
+        } else if (tileType.equals("cast")) {
+            resId = R.drawable.ic_qs_cast;
+            stringId = R.string.cast_screen;
+        } else if (tileType.equals("hotspot")) {
+            resId = R.drawable.ic_qs_hotspot;
+            stringId = R.string.hotspot;
+        }
+
+        String name = stringId != -1 ? context.getString(stringId) : null;
+        return new QSTileHolder(resId, tileType, name);
+    }
+}
