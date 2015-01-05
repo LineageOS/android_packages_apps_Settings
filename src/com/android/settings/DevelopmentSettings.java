6c580cf7264185b84f4a048f3c8a2e231053dace
Diogo Ferreira <defer@cyngn.com>
2015-01-05 18:19:12 +0000

    privacyguard: Add Superuser summary
    Adds superuser summary to the privacy guard "Advanced" menu, this
    is useful for auditing purposes. The tab is only visible when root
    for apps is enabled.
    
    Change-Id: I0b71c51d10e44e69102c311e526cdb1fde9bda26

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -755,7 +755,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
                 .getStringArray(R.array.root_access_entries)[Integer.valueOf(value)]);
     }
 
-    /* package */ static boolean isRootForAppsEnabled() {
+    public static boolean isRootForAppsEnabled() {
         int value = SystemProperties.getInt(ROOT_ACCESS_PROPERTY, 0);
         boolean daemonState =
                 SystemProperties.get("init.svc.su_daemon", "absent").equals("running");
