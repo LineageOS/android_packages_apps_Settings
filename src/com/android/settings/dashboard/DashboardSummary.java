96bd32d320000e81308092a8d8dd9f6d001ef9fa
Roman Birg <roman@cyngn.com>
2015-01-05 13:32:20 -0800

    Settings: add switches for dashboard items
    Change-Id: Ibff81510270745807a4b133457d60d47dd629df6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/dashboard/DashboardSummary.java
+++ src/com/android/settings/dashboard/DashboardSummary.java
@@ -33,9 +33,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
+import com.android.settings.widget.SwitchBar;
 
 import java.util.Arrays;
 import java.util.List;
@@ -161,7 +163,8 @@ public class DashboardSummary extends Fragment {
 
                 DashboardTileView tileView = new DashboardTileView(context);
                 updateTileView(context, res, tile, tileView.getImageView(),
-                        tileView.getTitleTextView(), tileView.getStatusTextView());
+                        tileView.getTitleTextView(), tileView.getStatusTextView(),
+                        tileView.getSwitchView());
 
                 tileView.setTile(tile);
 
@@ -176,7 +179,7 @@ public class DashboardSummary extends Fragment {
     }
 
     private void updateTileView(Context context, Resources res, DashboardTile tile,
-            ImageView tileIcon, TextView tileTextView, TextView statusTextView) {
+            ImageView tileIcon, TextView tileTextView, TextView statusTextView, Switch switchBar) {
 
         if (tile.iconRes > 0) {
             tileIcon.setImageResource(tile.iconRes);
@@ -193,6 +196,12 @@ public class DashboardSummary extends Fragment {
             statusTextView.setText(summary);
         } else {
             statusTextView.setVisibility(View.GONE);
+        }
+
+        if (tile.switchControl != null) {
+            switchBar.setVisibility(View.VISIBLE);
+        } else {
+            switchBar.setVisibility(View.GONE);
         }
     }
 
