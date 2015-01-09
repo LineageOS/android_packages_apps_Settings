0ae39f164e3b9acf25d0b66addc45d1937903ab0
Vladislav Koldobskiy <admin@nevergone.ru>
2015-01-10 00:21:17 +0300

    Settings: cleanup resources
    Cleanup unused strings & improve current ones
    
    Including:
    
    * replace SUB with SIM
    * we don't have App groups in Profiles anymore
    * tweaked debugging-related strings
    * changed Selector notification to Selector icon - it's located in
      Navigation bar now
    * Changed from Wi-Fi to Wi\u2011Fi
    * application -> app
    * fixed camel case
    * moved adb notify below adb over network
    
    Change-Id: If39e21cea60645e7502a3664c88745964d862eb5

--- src/com/android/settings/DataUsageSummary.java
+++ src/com/android/settings/DataUsageSummary.java
@@ -2581,7 +2581,7 @@ public class DataUsageSummary extends HighlightingFragment implements Indexable 
                    return mSubInfo.displayName;
                 }
             }
-            return getText(R.string.data_usage_tab_slot).toString() + " " + i;
+            return getString(R.string.data_usage_tab_slot, i);
         }
     }
 
