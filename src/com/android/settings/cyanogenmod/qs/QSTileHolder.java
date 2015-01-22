b56db660f330c11d879af0068df1b4880af338ec
Adnan Begovic <adnan@cyngn.com>
2015-01-22 12:23:59 -0800

    Settings: Add LteTile.
    Change-Id: I696624dd2637cf89cc574b727e1e04f24270ab27

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -90,6 +90,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_hotspot_on";
                 stringId = R.string.hotspot;
                 break;
+            case QSConstants.TILE_LTE:
+                resourceName = "ic_qs_lte_on";
+                stringId = R.string.lte;
+                break;
             default:
                 return null;
         }
