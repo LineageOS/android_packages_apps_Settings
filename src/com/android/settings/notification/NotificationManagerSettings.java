7bb43a3c123a800e565d994da19568b517cad982
Roman Birg <roman@cyngn.com>
2015-01-30 17:19:03 -0800

    Settings: reorganization
    Reorganize settings categories to be more user friendly. In addition,
    there is now an "Advanced mode". This mode is on by default for
    userdebug and eng builds, and can be overlayed to be turned on by
    default for user builds. Currently this toggle lives in About phone for
    easy discoverability.
    
    Change-Id: I18fa8c3e3ee7efc9fcaa7384d5b1cfb2495843f1
    Signed-off-by: Roman Birg <roman@cyngn.com>

---
+++ src/com/android/settings/notification/NotificationManagerSettings.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationManagerSettings extends SettingsPreferenceFragment
+        implements Indexable {
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.notification_manager_settings);
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.notification_manager_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+            };
+}
