8aa54efeb82b25402d255f72de1f138c5033eac2
Roman Birg <roman@cyngn.com>
2015-02-16 17:22:18 -0800

    Settings: move advanced mode to Settings provider
    Change-Id: I8636df8a6034d576a923bb1f756be18d380f978d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -44,6 +44,7 @@ import android.text.TextUtils;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
+import com.android.settings.cyanogenmod.SecureSettingSwitchPreference;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Index;
 import com.android.settings.search.Indexable;
@@ -88,10 +89,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
-    public static final String PREFS_FILE = "device";
     public static final String KEY_ADVANCED_MODE = "advanced_mode";
 
-    SwitchPreference mAdvancedSettings;
+    SecureSettingSwitchPreference mAdvancedSettings;
 
     long[] mHits = new long[3];
     int mDevHitCountdown;
@@ -195,7 +195,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 getPreferenceScreen().removePreference(pref);
             }
         }
-        mAdvancedSettings = (SwitchPreference) findPreference(KEY_ADVANCED_MODE);
+        mAdvancedSettings = (SecureSettingSwitchPreference) findPreference(KEY_ADVANCED_MODE);
     }
 
     @Override
@@ -281,12 +281,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                     Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
                 }
             }
-        } else if (preference.getKey().equals(KEY_ADVANCED_MODE)) {
-            final boolean isEnabled = mAdvancedSettings.isChecked();
-            getActivity().getSharedPreferences(PREFS_FILE, 0)
-                    .edit()
-                    .putBoolean(KEY_ADVANCED_MODE, isEnabled)
-                    .apply();
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
