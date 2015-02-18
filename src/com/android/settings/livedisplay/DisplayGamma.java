ec648714c10d421cb3f62742de0cdd16b3ef3ff7
Steve Kondik <steve@cyngn.com>
2015-02-18 03:44:31 -0800

    livedisplay: Add LiveDisplay settings
     * Add preference for LiveDisplay mode, and additional supporting
       options for auto mode.
     * CABC, SRE, CE are all under this umbrella now.
     * Manual RGB tweaking is now handled here and the requirement for
       CMHW has been removed as it will go thru RenderEngine.
     * Moved all related code into the livedisplay package.
    
    Change-Id: I0b00f2bfd8f8227d64ad271bbe0f004f0b4bcf9d

--- src/com/android/settings/livedisplay/DisplayGamma.java
+++ src/com/android/settings/livedisplay/DisplayGamma.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.hardware;
+package com.android.settings.livedisplay;
 
 import android.app.AlertDialog;
 import android.content.Context;
