32734c4cc14f6f4c05a1ba363f37b4b2310b8c3b
Roman Birg <roman@cyngn.com>
2015-02-19 15:20:23 -0800

    Settings: fix Vibrate on touch preference not working
    It was moved out of Other sounds, and never got hooked back up.
    
    Change-Id: Ibdc27be82d1fdad27e28d3dc20b245c6e81a125f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -35,6 +35,7 @@ import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.preference.SeekBarVolumizer;
 import android.preference.SwitchPreference;
 import android.preference.TwoStatePreference;
@@ -76,7 +77,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
     private static final String KEY_INCREASING_RING_VOLUME = "increasing_ring_volume";
     private static final String KEY_VIBRATION_INTENSITY = "vibration_intensity";
-
+    private static final String KEY_VIBRATE_ON_TOUCH = "vibrate_on_touch";
 
     private static final int SAMPLE_CUTOFF = 2000;  // manually cap sample playback at 2 seconds
 
@@ -138,7 +139,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         }
 
         if (!VibratorIntensity.isSupported()) {
-            removePreference(KEY_VIBRATION_INTENSITY);
+            vibrate.removePreference(vibrate.findPreference(KEY_VIBRATION_INTENSITY));
         }
 
         initRingtones(sounds);
@@ -163,6 +164,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         super.onPause();
         mVolumeCallback.stopSample();
         mSettingsObserver.register(false);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     // === Volumes ===
@@ -461,6 +467,13 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
+        private boolean mHasVibratorIntensity;
+
+        @Override
+        public void prepare() {
+            super.prepare();
+            mHasVibratorIntensity = VibratorIntensity.isSupported();
+        }
 
         public List<SearchIndexableResource> getXmlResourcesToIndex(
                 Context context, boolean enabled) {
@@ -478,6 +491,14 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
                 rt.add(KEY_PHONE_RINGTONE);
                 rt.add(KEY_VIBRATE_WHEN_RINGING);
             }
+            Vibrator vib = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (vib == null || !vib.hasVibrator()) {
+                rt.add(KEY_VIBRATE);
+            }
+            if (!mHasVibratorIntensity) {
+                rt.add(KEY_VIBRATION_INTENSITY);
+            }
+
             return rt;
         }
     };
