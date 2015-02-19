79e3308b583df08bfbb19b27b15ab4aac47add4f
Raj Yengisetty <rajesh@cyngn.com>
2015-02-19 14:25:47 -0800

    Add support for Multi-User
    Change-Id: Id4e4b11b8c1ee8150c30d7b027cb0491bd626203

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -229,6 +229,7 @@ public class SettingsActivity extends Activity
             R.id.device_section,
             R.id.notification_settings,
             R.id.display_settings,
+            R.id.button_settings,
             R.id.storage_settings,
             R.id.application_settings,
             R.id.battery_settings,
@@ -245,6 +246,7 @@ public class SettingsActivity extends Activity
             R.id.print_settings,
             R.id.nfc_payment_settings,
             R.id.home_settings,
+            R.id.status_bar_settings,
             R.id.dashboard
     };
 
