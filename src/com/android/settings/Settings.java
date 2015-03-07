f53865a6d1649f0d7e3f2e0366c6d6b41c321f99
Michael Bestas <mikeioannina@gmail.com>
2015-03-07 05:22:23 +0200

    Allow launching display rotation settings externally
    Change-Id: I69168fea2a43cfa7f2b8474042c35720f4431040

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -106,5 +106,6 @@ public class Settings extends SettingsActivity {
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LiveDisplayActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
 }
 
