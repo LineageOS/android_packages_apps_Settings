69d3f587d4eae4afd38c4fe02b0717d2b5c0a0fd
Matt Garnes <matt@cyngn.com>
2015-03-25 16:25:26 -0700

    Fix incorrect ManageApplications options menu order value.
    Change-Id: I5c9b7d1ce9e03fa0efe593efd76b4ad2899a126a

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -1118,7 +1118,7 @@ public class ManageApplications extends Fragment implements
             menu.add(0, SHOW_PROTECTED_APPS, 5, R.string.protected_apps)
                     .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         }
-        menu.add(0, APP_INSTALL_LOCATION, 4, R.string.app_install_location_title)
+        menu.add(0, APP_INSTALL_LOCATION, 6, R.string.app_install_location_title)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         updateOptionsMenu();
     }
