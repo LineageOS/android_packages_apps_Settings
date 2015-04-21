9490e1c70a5ce0d00ece394026040946b5ce0dd5
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 01:38:22 +0300

    Allow launching notification manager settings externally
    Change-Id: Ic9acab571fc78d139d4924f9a95194423bf1d267

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -125,4 +125,5 @@ public class Settings extends SettingsActivity {
     public static class ContributorsCloudActivity extends SettingsActivity { /* empty */ }
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class NotificationManagerActivity extends SettingsActivity { /* empty */ }
 }
