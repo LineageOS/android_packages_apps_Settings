tilaksidduram/packages_apps_Settings

QS: add Sync tile (2/2)
Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d

@alviteri: also move QS tiles per row summary to cr_strings

Conflicts:
	res/values/custom_strings.xml

Conflicts:
	res/values/cr_strings.xml

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -118,6 +118,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_visualizer_static";
                 stringId = R.string.qs_tile_visualizer;
                 break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_sync_tile;
+                break;
             case QSConstants.TILE_SCREEN_TIMEOUT:
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;

LineageOS/android_packages_apps_Settings

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -153,6 +153,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_heads_up_on";
                 stringId = R.string.qs_tile_heads_up;
                 break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_tile_sync;
+                break;
             default:
                 return null;
         }