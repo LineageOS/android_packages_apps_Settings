25a89627087edc8f20b9d48c31f8c2332af428a1
Roman Birg <roman@cyngn.com>
2015-04-30 10:39:09 -0700

    Settings: disable mobile network switch when SIM isn't ready
    Change-Id: I4676390760f0d6107e706fe75cd26f7bf603cad6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/dashboard/MobileNetworksEnabler.java
+++ src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,8 +83,10 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
-        setChecked(mTelephonyManager.getDataEnabled());
+        setEnabled(mTelephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY &&
+                mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+        setChecked(mTelephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY &&
+                mTelephonyManager.getDataEnabled());
     }
 
     @Override
