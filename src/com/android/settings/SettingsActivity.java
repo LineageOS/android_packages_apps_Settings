1d81754ce7af4011ba449587291b6480422a4b23
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-30 17:33:50 -0500

    Settings: remove mobile networks menu if ro.radio.noril is set
    Change-Id: I1015876a01c515ceda5ec98d972b6be9875df6a0

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -45,6 +45,7 @@ import android.os.Message;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.Preference;
@@ -1216,6 +1217,8 @@ public class SettingsActivity extends Activity
                     if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
                         removeTile = true;
                     } else if (TelephonyManager.getDefault().getPhoneCount() > 1) {
+                        removeTile = true;
+                    } else if (SystemProperties.getBoolean("ro.radio.noril", false)) {
                         removeTile = true;
                     }
                 } else if (id == R.id.data_usage_settings) {
