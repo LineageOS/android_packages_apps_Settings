4b2ac13e7f9f86ed73790fd8157b3ff77bbb726d
cody ferber <codyf86@gmail.com>
2015-05-11 21:24:47 -0400

    Fix mobile network switch for CDMA phones
    commit f1b6d3066e239b123375ecd5052b15db733e3e6f
    inadvertently removed the ability to enable/disable mobile
    network on CDMA phones.
    
    It currently is always enabled.
    
    Change-Id: I70a1b5fd63c49842f281823e6cd59a391d05934b

--- src/com/android/settings/dashboard/MobileNetworksEnabler.java
+++ src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,10 +83,15 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        final boolean simReady = mTelephonyManager.getSimState()
-                == TelephonyManager.SIM_STATE_READY;
-        setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
-        setChecked(simReady && mTelephonyManager.getDataEnabled());
+        if (mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+            final boolean simReady = mTelephonyManager.getSimState()
+                    == TelephonyManager.SIM_STATE_READY;
+            setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+            setChecked(simReady && mTelephonyManager.getDataEnabled());
+        } else {
+            setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+            setChecked(mTelephonyManager.getDataEnabled());
+        }
     }
 
     @Override

LineageOS/android_packages_apps_Settings

commit f1b6d3066e239b123375ecd5052b15db733e3e6f

Settings: disable mobile network switch when SIM isn't ready
Change-Id: I4676390760f0d6107e706fe75cd26f7bf603cad6
Signed-off-by: Roman Birg <roman@cyngn.com>

src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,8 +83,10 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
-        setChecked(mTelephonyManager.getDataEnabled());
+        final boolean simReady = mTelephonyManager.getSimState()
+                == TelephonyManager.SIM_STATE_READY;
+        setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+        setChecked(simReady && mTelephonyManager.getDataEnabled());
     }
 
     @Override

commit 4b2ac13e7f9f86ed73790fd8157b3ff77bbb726d

Fix mobile network switch for CDMA phones
commit f1b6d3066e239b123375ecd5052b15db733e3e6f
inadvertently removed the ability to enable/disable mobile
network on CDMA phones.

It currently is always enabled.

Change-Id: I70a1b5fd63c49842f281823e6cd59a391d05934b

src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,10 +83,15 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        final boolean simReady = mTelephonyManager.getSimState()
-                == TelephonyManager.SIM_STATE_READY;
-        setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
-        setChecked(simReady && mTelephonyManager.getDataEnabled());
+        if (mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+            final boolean simReady = mTelephonyManager.getSimState()
+                    == TelephonyManager.SIM_STATE_READY;
+            setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+            setChecked(simReady && mTelephonyManager.getDataEnabled());
+        } else {
+            setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+            setChecked(mTelephonyManager.getDataEnabled());
+        }
     }
 
     @Override