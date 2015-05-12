1f1dc71b79ca1db00f6b8d810f5da223562bfb46
Danny Baumann <dannybaumann@web.de>
2015-05-12 08:52:00 +0200

    Integrate mobile network settings into SIM settings for MSIM.
    Change-Id: Ie921fcac512d8e4197fef46d94e6f3d592bcd02f

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1211,10 +1211,6 @@ public class SettingsActivity extends Activity
                     } else if (TelephonyManager.getDefault().getPhoneCount() > 1) {
                         removeTile = true;
                     }
-                } else if (id == R.id.msim_mobile_networks) {
-                    if (TelephonyManager.getDefault().getPhoneCount() <= 1) {
-                        removeTile = true;
-                    }
                 } else if (id == R.id.data_usage_settings) {
                     // Remove data usage when kernel module not enabled
                     final INetworkManagementService netManager = INetworkManagementService.Stub
