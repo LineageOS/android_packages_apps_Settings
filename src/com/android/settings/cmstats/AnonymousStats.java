0f5f5055f38889b3bd2b61f0ceb21303466fe9fa
Roman Birg <roman@cyngn.com>
2015-06-15 14:42:37 -0700

    Settings: update community metrics
    - Send community metrics to CM.org as well as Cyanogen server
    
    - Kill off Google Analytics
    
    - Use JobScheduler to intelligently schedule uploads when network
      connection is available
    
    Change-Id: Ibd7d84a961dda4af20db9226191380e9f977b5de
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/cmstats/AnonymousStats.java
+++ src/com/android/settings/cmstats/AnonymousStats.java
@@ -20,13 +20,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.util.ArraySet;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.Set;
 
 public class AnonymousStats extends SettingsPreferenceFragment {
     private static final String PREF_FILE_NAME = "CMStats";
     /* package */ static final String ANONYMOUS_OPT_IN = "pref_anonymous_opt_in";
     /* package */ static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
+
+    /* package */ static final String KEY_JOB_QUEUE = "pref_job_queue";
 
     public static SharedPreferences getPreferences(Context context) {
         return context.getSharedPreferences(PREF_FILE_NAME, 0);
@@ -36,5 +41,47 @@ public class AnonymousStats extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.anonymous_stats);
+    }
+
+    public static Set<String> getJobQueue(Context context) {
+        return getPreferences(context).getStringSet(KEY_JOB_QUEUE, new ArraySet<String>());
+    }
+
+    public static void addJob(Context context, int jobId) {
+        Set<String> jobQueue = getJobQueue(context);
+        jobQueue.add(String.valueOf(jobId));
+
+        getPreferences(context)
+                .edit()
+                .putStringSet(KEY_JOB_QUEUE, jobQueue)
+                .commit();
+    }
+
+    public static void removeJob(Context context, int jobId) {
+        Set<String> jobQueue = getJobQueue(context);
+        jobQueue.remove(String.valueOf(jobId));
+        getPreferences(context)
+                .edit()
+                .putStringSet(KEY_JOB_QUEUE, jobQueue)
+                .commit();
+    }
+
+    public static int getNextJobId(Context context) {
+        Set<String> currentQueue = getJobQueue(context);
+
+        if (currentQueue == null) {
+            return 1;
+        } else {
+            int currentMax = -1;
+
+            for (String val : currentQueue) {
+                Integer i = Integer.parseInt(val);
+                if (i > currentMax) {
+                    currentMax = i;
+                }
+            }
+            return (currentMax + 1);
+
+        }
     }
 }
