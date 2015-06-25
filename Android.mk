f6131d1acc16cfff314db59e1dd3f5f519186bcd
Adnan Begovic <adnan@cyngn.com>
2015-06-25 12:53:50 -0700

    Settings: Refactor to point at Profiles in CMSDK.
    Change-Id: I589dd372a58d8aca0f804e419992c28188d5380c

--- Android.mk
+++ Android.mk
@@ -7,7 +7,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 	android-support-v7-cardview \
 	android-support-v13 \
 	jsr305 \
-        libGoogleAnalyticsV3
+	libGoogleAnalyticsV3 \
+	org.cyanogenmod.platform.sdk
 
 LOCAL_MODULE_TAGS := optional
 
