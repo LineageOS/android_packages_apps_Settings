272d3cc0a9512ebc9b6df680c1faca98809beef9
Jorge Ruesga <jorge@ruesga.com>
2015-08-12 01:05:08 +0200

    settings: contributors cloud
    Change-Id: If5b89e0d278b7a0c85c966e09264b60927889fc9
    JIRA: CML-133
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -327,7 +328,8 @@ public class SettingsActivity extends Activity
             NotificationManagerSettings.class.getName(),
             LockScreenSettings.class.getName(),
             LiveDisplay.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            ContributorsCloudFragment.class.getName()
     };
 
 
