74fb0de24cad5c4a4fd1a1e65a820db181c13fc0
Jorge Ruesga <jorge@ruesga.com>
2015-08-12 01:05:08 +0200

    settings: contributors cloud
    Change-Id: If5b89e0d278b7a0c85c966e09264b60927889fc9
    JIRA: CML-133
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -20,8 +20,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import com.android.settings.ButtonSettings;
-import com.android.settings.R;
-import com.android.settings.Utils;
+import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.livedisplay.DisplayGamma;
@@ -39,5 +38,8 @@ public class BootReceiver extends BroadcastReceiver {
         VibratorIntensity.restore(ctx);
         InputMethodAndLanguageSettings.restore(ctx);
         LocationSettings.restore(ctx);
+
+        // Extract the contributors database
+        ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
     }
 }
