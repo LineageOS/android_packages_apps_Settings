c2a7c19f08aeb814c7b6d301150c08f22082f943
Roman Birg <roman@cyngn.com>
2015-08-27 15:43:16 -0700

    Settings: fix up advanced mode logic
    The overlay config value should be a 'always-on' switch, otherwise
    we should read the user-set setting.
    
    Advanced mode should always be off by default.
    
    Ref: CYNGNOS-815
    
    Change-Id: I7f66eaf7fc84e9d3842e4ae874ffba2050b89ea6
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 055e70f6ba971fb1b30596065c2ccebb6a703296)

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1477,9 +1477,12 @@ public class SettingsActivity extends Activity
      * by default in an overlay.
      */
     public static boolean showAdvancedPreferences(Context context) {
-        return (android.provider.Settings.Secure.getInt(context.getContentResolver(),
-                android.provider.Settings.Secure.ADVANCED_MODE, 1) == 1)
-                && context.getResources().getBoolean(
+        final boolean forceAdvancedMode = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_advancedSettingsMode);
+        if (forceAdvancedMode) {
+            return true;
+        }
+        return (android.provider.Settings.Secure.getInt(context.getContentResolver(),
+                android.provider.Settings.Secure.ADVANCED_MODE, 0) == 1);
     }
 }
