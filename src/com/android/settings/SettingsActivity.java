84738aa4209d3dfce3dfcd4b65532a18f4035a1b
Giovanni <gogio@live.nl>
2015-10-28 08:58:07 +0100

    settings: Added contributors cloud
    Updated to latest commit: d124efa53b6c8335e459d02e053f0b19c44ad7b3
    
    Change-Id: I7388d67f278c3b219c09461de52065b035a4cb5c

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -80,6 +80,7 @@ import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -361,6 +362,7 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
+            ContributorsCloudFragment.class.getName()
     };
 
 
