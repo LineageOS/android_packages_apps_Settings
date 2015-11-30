8d5194b50359218f8f7d5f69b8f1f402b99e6e32
Danesh M <daneshm90@gmail.com>
2015-11-30 15:55:14 -0800

    Revert lockscreen shortcuts if app uninstalled (2/2)
    CYNGNOS-1374
    
    Change-Id: I12bfa1d0b73fa301116f0fe268d90a01401c642e

---
+++ src/com/android/settings/cyanogenmod/ShortcutCleanupReceiver.java
@@ -0,0 +1,32 @@
+package com.android.settings.cyanogenmod;
+
+import com.android.internal.util.cm.LockscreenShortcutsHelper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+
+public class ShortcutCleanupReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Uri uri = intent.getData();
+        if (uri == null) {
+            // they sent us a bad intent
+            return;
+        }
+
+        final String packageName = uri.getSchemeSpecificPart();
+        if (TextUtils.isEmpty(packageName)) {
+            // they sent us a bad intent
+            return;
+        }
+
+        cleanupLockscreenShortcuts(context, packageName);
+    }
+
+    private void cleanupLockscreenShortcuts(Context context, String packageName) {
+        LockscreenShortcutsHelper helper = new LockscreenShortcutsHelper(context, null);
+        helper.removeTargetsForPackage(packageName);
+    }
+}
