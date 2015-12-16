e9b6cd9c0982b4d81283ae4693ffd430a6c69044
Adnan Begovic <adnan@cyngn.com>
2015-12-16 13:10:19 -0800

    Settings: Utilize frameworks sms prompt interfaces as fallback.
    Ticket: CYNGNOS-1440
    Change-Id: I0890d5cc25124dc94434216763cb8965986ca4b3

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -38,6 +38,7 @@ import android.preference.PreferenceCategory;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.telephony.PhoneStateListener;
+import android.telephony.SmsManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
@@ -225,6 +226,9 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
             loge("RemoteException @isSMSPromptEnabled" + ex);
         } catch (NullPointerException ex) {
             loge("NullPointerException @isSMSPromptEnabled" + ex);
+        }
+        if (mExtTelephony == null) {
+            isSMSPrompt = SmsManager.getDefault().isSMSPromptEnabled();
         }
         log("[updateSmsValues] isSMSPrompt: " + isSMSPrompt);
         if (isSMSPrompt || sir == null) {
