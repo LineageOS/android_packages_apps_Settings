842c6e3f97f68eeb6fc8653b0664c9743243b00b
Danesh M <daneshm90@gmail.com>
2015-12-17 16:59:45 -0800

    Settings : Cleanup control media volume toggle
    Change-Id: I1e887e6a109aa75fcb1d1e74216fceb1cc91ab59

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -133,7 +133,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mVolumeKeyCursorControl;
     private SwitchPreference mVolumeWakeScreen;
     private SwitchPreference mVolumeMusicControls;
-    private SwitchPreference mVolumeControlRingStream;
     private SwitchPreference mSwapVolumeButtons;
     private SwitchPreference mDisableNavigationKeys;
     private SwitchPreference mNavigationBarLeftPref;
@@ -442,14 +441,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         mVolumeWakeScreen = (SwitchPreference) findPreference(CMSettings.System.VOLUME_WAKE_SCREEN);
         mVolumeMusicControls = (SwitchPreference) findPreference(KEY_VOLUME_MUSIC_CONTROLS);
 
-        mVolumeControlRingStream = (SwitchPreference)
-                findPreference(KEY_VOLUME_CONTROL_RING_STREAM);
-        int volumeControlRingtone = CMSettings.System.getInt(getContentResolver(),
-                CMSettings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1);
-        if (mVolumeControlRingStream != null) {
-            mVolumeControlRingStream.setChecked(volumeControlRingtone > 0);
-        }
-
         if (mVolumeWakeScreen != null) {
             if (mVolumeMusicControls != null) {
                 mVolumeMusicControls.setDependency(CMSettings.System.VOLUME_WAKE_SCREEN);
@@ -726,10 +717,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                     ? (ScreenType.isTablet(getActivity()) ? 2 : 1) : 0;
             CMSettings.System.putInt(getActivity().getContentResolver(),
                     CMSettings.System.SWAP_VOLUME_KEYS_ON_ROTATION, value);
-        } else if (preference == mVolumeControlRingStream) {
-            int value = mVolumeControlRingStream.isChecked() ? 1 : 0;
-            CMSettings.System.putInt(getActivity().getContentResolver(),
-                    CMSettings.System.VOLUME_KEYS_CONTROL_RING_STREAM, value);
         } else if (preference == mDisableNavigationKeys) {
             mDisableNavigationKeys.setEnabled(false);
             mNavigationPreferencesCat.setEnabled(false);
