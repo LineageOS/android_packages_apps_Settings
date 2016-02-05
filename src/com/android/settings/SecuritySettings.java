a9db9f0b219db63421a29f6c67aca3ef8c7f23ce
Diogo Ferreira <diogo@underdev.org>
2016-02-05 15:45:05 +0000

    Settings: Add CM-specific settings
    Change-Id: Iae0f60a62a51e96893766c4fa50a60ac4a214102
    Ticket: CYNGNOS-1577

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -81,8 +81,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
             new Intent(TrustAgentService.SERVICE_INTERFACE);
 
     // Fitler types for this panel
-    private static final String FILTER_TYPE_EXTRA = "filter_type";
-    private static final int TYPE_LOCKSCREEN_EXTRA = 0;
+    protected static final String FILTER_TYPE_EXTRA = "filter_type";
+    protected static final int TYPE_LOCKSCREEN_EXTRA = 0;
     private static final int TYPE_SECURITY_EXTRA = 1;
 
     // Lock Settings
@@ -128,9 +128,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
     // Only allow one trust agent on the platform.
     private static final boolean ONLY_ONE_TRUST_AGENT = true;
 
-    private static final int MY_USER_ID = UserHandle.myUserId();
+    protected static final int MY_USER_ID = UserHandle.myUserId();
 
-    private static final String LIVE_LOCK_SCREEN_FEATURE = "org.cyanogenmod.livelockscreen";
+    protected static final String LIVE_LOCK_SCREEN_FEATURE = "org.cyanogenmod.livelockscreen";
 
     private PackageManager mPM;
     private DevicePolicyManager mDPM;
@@ -200,7 +200,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
     }
 
-    private static int getResIdForLockUnlockScreen(Context context,
+    protected static int getResIdForLockUnlockScreen(Context context,
             LockPatternUtils lockPatternUtils) {
         int resid = 0;
         if (!lockPatternUtils.isSecure(MY_USER_ID)) {
@@ -532,7 +532,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
     }
 
-    private static ArrayList<TrustAgentComponentInfo> getActiveTrustAgents(
+    protected static ArrayList<TrustAgentComponentInfo> getActiveTrustAgents(
             PackageManager pm, LockPatternUtils utils, DevicePolicyManager dpm) {
         ArrayList<TrustAgentComponentInfo> result = new ArrayList<TrustAgentComponentInfo>();
         List<ResolveInfo> resolveInfos = pm.queryIntentServices(TRUST_AGENT_INTENT,
@@ -859,14 +859,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
             List<SearchIndexableResource> result = new ArrayList<SearchIndexableResource>();
 
-            LockPatternUtils lockPatternUtils = new LockPatternUtils(context);
-            // Add options for lock/unlock screen
-            int resId = getResIdForLockUnlockScreen(context, lockPatternUtils);
-
-            SearchIndexableResource sir = new SearchIndexableResource(context);
-            sir.xmlResId = resId;
-            result.add(sir);
-
+            int resId = 0;
+            SearchIndexableResource sir;
             if (mIsPrimary) {
                 DevicePolicyManager dpm = (DevicePolicyManager)
                         context.getSystemService(Context.DEVICE_POLICY_SERVICE);
@@ -913,22 +907,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
                 data = new SearchIndexableRaw(context);
                 data.title = res.getString(resId);
-                data.screenTitle = screenTitle;
-                result.add(data);
-            }
-
-            // Fingerprint
-            FingerprintManager fpm =
-                    (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
-            if (fpm.isHardwareDetected()) {
-                // This catches the title which can be overloaded in an overlay
-                data = new SearchIndexableRaw(context);
-                data.title = res.getString(R.string.security_settings_fingerprint_preference_title);
-                data.screenTitle = screenTitle;
-                result.add(data);
-                // Fallback for when the above doesn't contain "fingerprint"
-                data = new SearchIndexableRaw(context);
-                data.title = res.getString(R.string.fingerprint_manage_category_title);
                 data.screenTitle = screenTitle;
                 result.add(data);
             }
