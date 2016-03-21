fc996dbb27bb959fb0d054d26adb27c2e2cf5422
Luis Vidal <lvidal@cyngn.com>
2016-03-21 10:08:14 -0700

    Add Weather Content Provider [2/5]
    Introduce a new entry in the settings menu to select the available
    weather provider services.
    
    This entry will be parked in the System category between Date&Time
    and Accessibility
    
    Change-Id: Ida230f3be72eb575a2b09504c99eaa16851c99f8

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -84,6 +84,7 @@ import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.cyanogenmod.LiveLockScreenSettings;
+import com.android.settings.cyanogenmod.WeatherServiceSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -137,6 +138,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import cyanogenmod.app.CMContextConstants;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -377,7 +379,8 @@ public class SettingsActivity extends Activity
             ProfilesSettings.class.getName(),
             ContributorsCloudFragment.class.getName(),
             NotificationManagerSettings.class.getName(),
-            LiveLockScreenSettings.class.getName()
+            LiveLockScreenSettings.class.getName(),
+            WeatherServiceSettings.class.getName()
     };
 
 
@@ -1326,6 +1329,11 @@ public class SettingsActivity extends Activity
                     boolean hasDeviceKeys = getResources().getInteger(
                             com.android.internal.R.integer.config_deviceHardwareKeys) != 0;
                     if (!hasDeviceKeys) {
+                        removeTile = true;
+                    }
+                } else if (id == R.id.weather_settings) {
+                    if (!getPackageManager().hasSystemFeature(
+                            CMContextConstants.Features.WEATHER_SERVICES)) {
                         removeTile = true;
                     }
                 }
