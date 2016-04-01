b9442d42611ca021df8954f8fb9017664b14a732
Ethan Chen <intervigil@gmail.com>
2016-04-01 11:05:02 -0700

    DevelopmentSettings: Synchronize USB connection data state
    * UsbModeChooserActivity now takes into account whether the user has
      unlocked USB data for the current composition mode, but the development
      settings USB mode chooser dialog does not. This patchset makes it
      aware.
    * Assume if USB data hasn't been unlocked by the user, that USB charging
      mode is the current composition mode. This patch makes assumptions
      that the charging composition mode occurs at position 0 of the
      resources however. Any devices which have custom USB composition modes
      set in the overlay should be aware of this change.
    
    REF: CYNGNOS-2322
    Change-Id: I8789f23b1c58bfb6e23f4fcde2b2c10d5fd4adf2

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1576,14 +1576,15 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateLogdSizeValues();
     }
 
-    private void updateUsbConfigurationValues() {
+    private void updateUsbConfigurationValues(boolean isUnlocked) {
         if (mUsbConfiguration != null) {
             UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
             String[] values = getResources().getStringArray(R.array.usb_configuration_values);
             String[] titles = getResources().getStringArray(R.array.usb_configuration_titles);
             int index = 0;
-            for (int i = 0; i < titles.length; i++) {
+            // Assume if !isUnlocked -> charging, which should be at index 0
+            for (int i = 0; i < titles.length && isUnlocked; i++) {
                 if (manager.isFunctionEnabled(values[i])) {
                     index = i;
                     break;
@@ -1598,10 +1599,11 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private void writeUsbConfigurationOption(Object newValue) {
         UsbManager manager = (UsbManager)getActivity().getSystemService(Context.USB_SERVICE);
         String function = newValue.toString();
-        manager.setCurrentFunction(function);
         if (function.equals("none")) {
+            manager.setCurrentFunction(null);
             manager.setUsbDataUnlocked(false);
         } else {
+            manager.setCurrentFunction(function);
             manager.setUsbDataUnlocked(true);
         }
     }
@@ -2263,7 +2265,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            updateUsbConfigurationValues();
+            boolean isUnlocked = intent.getBooleanExtra(UsbManager.USB_DATA_UNLOCKED, false);
+            updateUsbConfigurationValues(isUnlocked);
         }
     };
 
