d09a84a01928764ba28a65d2091b5160c091a1e0
Steve Kondik <steve@cyngn.com>
2016-04-04 22:32:59 -0700

    settings: Unbreak the build
     * Someone better have donuts for me in the morning.
    
    DONUTS-1234
    
    Change-Id: I4c5756cfed77ffc1a7b74f4415565fdc519a7af5

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -820,7 +820,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     }
                     if (!isCameraGestureAvailable(context.getResources())) {
                         result.add(KEY_CAMERA_GESTURE);
-
+                    }
                     if (!context.getResources().getBoolean(
                                  com.android.internal.R.bool.config_enableWifiDisplay) &&
                             (SystemProperties.getInt(FORCE_WIFI_DISPLAY_ENABLE, -1) != 1)) {
