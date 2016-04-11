bc14236d94760474240a40b29662bc418b66108a
Steve Kondik <steve@cyngn.com>
2016-04-10 18:20:46 -0700

    livedisplay: Update Settings for new LiveDisplay API
     * LiveDisplay is getting a proper API. This patch refactors
       the settings to use it.
     * Also kill off the deprecated "gamma calibration" tunable
       which nobody should be using at this point.
    
    Change-Id: I948b8845cd06c8c634a201cb5d5b93b5766cc51d

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -28,7 +28,6 @@ import com.android.settings.Utils;
 import com.android.settings.contributors.ContributorsCloudFragment;
 import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
-import com.android.settings.livedisplay.DisplayGamma;
 import com.android.settings.location.LocationSettings;
 import com.android.settings.DevelopmentSettings;
 
@@ -42,7 +41,6 @@ public class BootReceiver extends BroadcastReceiver {
         if (!hasRestoredTunable(ctx)) {
             /* Restore the hardware tunable values */
             ButtonSettings.restoreKeyDisabler(ctx);
-            DisplayGamma.restore(ctx);
             VibratorIntensity.restore(ctx);
             InputMethodAndLanguageSettings.restore(ctx);
             setRestoredTunable(ctx);
