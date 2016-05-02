770a24b0b9801db033bb2e67ab6969d26b6fccd7
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-02 09:34:44 -0500

    Settings: fix toggling lockscreen rotation toggle [2/2]
    Change-Id: I4339afcf7befb70f459b088be9e1fc62a0465b4f

--- src/com/android/settings/cyanogenmod/DisplayRotation.java
+++ src/com/android/settings/cyanogenmod/DisplayRotation.java
@@ -30,6 +30,7 @@ import com.android.internal.view.RotationPolicy;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import cyanogenmod.providers.CMSettings;
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 public class DisplayRotation extends SettingsPreferenceFragment {
@@ -43,6 +44,7 @@ public class DisplayRotation extends SettingsPreferenceFragment {
     private static final String ROTATION_270_PREF = "display_rotation_270";
 
     private SwitchPreference mAccelerometer;
+    private SwitchPreference mLockScreenRotation;
     private CheckBoxPreference mRotation0Pref;
     private CheckBoxPreference mRotation90Pref;
     private CheckBoxPreference mRotation180Pref;
@@ -98,13 +100,11 @@ public class DisplayRotation extends SettingsPreferenceFragment {
             mRotation270Pref.setDependency(null);
         }
 
-        final SwitchPreference lockScreenRotation =
-                (SwitchPreference) findPreference(KEY_LOCKSCREEN_ROTATION);
-        boolean canRotateLockscreen = getResources().getBoolean(
-                com.android.internal.R.bool.config_enableLockScreenRotation);
+        mLockScreenRotation = (SwitchPreference) findPreference(KEY_LOCKSCREEN_ROTATION);
 
-        if (lockScreenRotation != null && !canRotateLockscreen) {
-            getPreferenceScreen().removePreference(lockScreenRotation);
+        if (!getResources().getBoolean(
+                com.android.internal.R.bool.config_enableLockScreenRotation)) {
+            getPreferenceScreen().removePreference(mLockScreenRotation);
         }
     }
 
@@ -173,6 +173,10 @@ public class DisplayRotation extends SettingsPreferenceFragment {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES, mode);
             return true;
+        } else if (preference == mLockScreenRotation) {
+            CMSettings.System.putInt(getActivity().getContentResolver(),
+                    CMSettings.System.LOCKSCREEN_ROTATION,
+                    (mLockScreenRotation.isChecked() ? 1 : 0));
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
