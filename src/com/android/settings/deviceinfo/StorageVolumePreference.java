7039c8663a39828850a2d9477ff978a7e2813154
maxwen <max.weninger@gmail.com>
2016-05-07 18:32:00 +0300

    Settings: display fstype for mounted volumes
    Change-Id: Ia3cc496fa851b9575fb3a43fe91f2f5544fe0393
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- src/com/android/settings/deviceinfo/StorageVolumePreference.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -26,6 +26,7 @@ import android.os.storage.VolumeInfo;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceViewHolder;
 import android.text.format.Formatter;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.util.TypedValue;
@@ -64,7 +65,8 @@ public class StorageVolumePreference extends Preference {
         setLayoutResource(R.layout.storage_volume);
 
         setKey(volume.getId());
-        setTitle(mStorageManager.getBestVolumeDescription(volume));
+        final String fsType = volume.fsType;
+        setTitle(mStorageManager.getBestVolumeDescription(volume) + (TextUtils.isEmpty(fsType) ? "" : " / " + fsType));
 
         Drawable icon;
         if (VolumeInfo.ID_PRIVATE_INTERNAL.equals(volume.getId())) {
