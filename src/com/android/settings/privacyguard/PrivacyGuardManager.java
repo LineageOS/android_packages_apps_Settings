3f65df74bfa367b9e7deb56ec3fdd30cdedcd4a7
Adnan Begovic <adnan@cyngn.com>
2016-05-20 12:28:25 -0700

    Settings: Dissallow privacy guarding system apps on user builds.
      Bandaid fix for google play services inundating the permission
      dialog request queue within AppOps service and causing watchdog
      to consitently kill the system server's activity manager service
      which is deadlocked retrieving dialogs from the queue.
    
    Change-Id: I10c432cebe6a4199517839598046eee7d19dcf72
    TICKET: CYNGNOS-2869

--- src/com/android/settings/privacyguard/PrivacyGuardManager.java
+++ src/com/android/settings/privacyguard/PrivacyGuardManager.java
@@ -17,6 +17,7 @@
 package com.android.settings.privacyguard;
 
 import android.app.FragmentTransaction;
+import android.os.Build;
 import android.view.animation.AnimationUtils;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -297,9 +298,8 @@ public class PrivacyGuardManager extends Fragment
         return true;
     }
 
-
     private boolean shouldShowSystemApps() {
-        return mPreferences.getBoolean("show_system_apps", false);
+        return mPreferences.getBoolean("show_system_apps", false) && !("user".equals(Build.TYPE));
     }
 
     public static class HelpDialogFragment extends DialogFragment {
