2f883aa3dd615fae030a2136377f6a6a2f7d2f23
tnmy-cyngn <tlnu@cyngn.com>
2016-05-27 02:53:41 +0530

    Hardware version info missing
    Vendor request to display hardware version in Settings -> About phone.
    Adding preference to display the info.
    
    YAM-394
    
    Change-Id: I820c6974b542a0fb8920258482b94f011826b603

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -98,6 +98,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_CM_UPDATES = "cm_updates";
     private static final String KEY_CM_LICENSE = "cmlicense";
+    private static final String KEY_HW_VERSION = "hardware_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -231,6 +232,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         // If enabled by default, just remove the setting, because it's confusing.
         removePreferenceIfBoolFalse(KEY_ADVANCED_MODE, !getResources().getBoolean(
                 com.android.internal.R.bool.config_advancedSettingsMode));
+
+        removePreferenceIfBoolFalse(KEY_HW_VERSION,
+                !TextUtils.isEmpty(getResources().getString(R.string.hardware_version_value)));
     }
 
     @Override
