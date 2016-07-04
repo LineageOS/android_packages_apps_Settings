d3c7004baabb896f16d46fb0e447f7cf0eafcc1e
George G <kreach3r@users.noreply.github.com>
2016-07-04 12:07:11 +0300

    Settings: Add Substratum to dashboard
    Launcher code taken from the SuperSU commit updated by Mat Pls
    
    Also remove the substratum tile if it's not installed.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -70,6 +70,7 @@ public class Settings extends SettingsActivity {
     public static class BackgroundCheckSummaryActivity extends SettingsActivity { /* empty */ }
     public static class StorageUseActivity extends SettingsActivity { /* empty */ }
     public static class DevelopmentSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SubstratumActivity extends SettingsActivity { /* empty */ }
     public static class AccessibilitySettingsActivity extends SettingsActivity { /* empty */ }
     public static class CaptioningSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AccessibilityInversionSettingsActivity extends SettingsActivity { /* empty */ }
