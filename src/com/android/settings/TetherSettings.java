4391fc6a1e10f6f0b8a84a6018c3e3673304d67a
FangYunong <yunong@codeaurora.org>
2016-09-20 16:55:21 +0800

    Settings: Remove "Bluetooth tethering"
    Remove "Bluetooth tethering" in Tethering & Mobile
    HotSpot and Help at MPCS and TMO mode.
    
    Change-Id: Idff8a187057c3a2eb11b5aca02678e552cf491dc
    CRs-fixed: 1064452

--- src/com/android/settings/TetherSettings.java
+++ src/com/android/settings/TetherSettings.java
@@ -261,7 +261,9 @@ public class TetherSettings extends RestrictedSettingsFragment
             getPreferenceScreen().removePreference(mCreateNetwork);
         }
 
-        if (!bluetoothAvailable) {
+        final boolean configHideBluetoothAndHelpMenu = getResources().getBoolean(
+                R.bool.config_hide_bluetooth_menu);
+        if (!bluetoothAvailable || configHideBluetoothAndHelpMenu) {
             getPreferenceScreen().removePreference(mBluetoothTether);
         } else {
             BluetoothPan pan = mBluetoothPan.get();
