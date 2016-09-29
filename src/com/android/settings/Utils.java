e433647b7457488eab77d5d77d6cfe294c9b3daa
Fang Yunong <yunong@codeaurora.org>
2016-09-29 16:01:26 +0800

    Settings:Change the way of email account displayed
    Show only the default mailbox account with the system in list,hide others.
    
    Change-Id: I5a86ed19c06c55032a6d4a7ddac30466885b57bd
    CRs-fixed: 1071041

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -794,13 +794,13 @@ public final class Utils extends com.android.settingslib.Utils {
      * Returns if need show the account with the given account type.
      */
     public static boolean showAccount(Context context, String accountType) {
-        String[] hideAccounts = context.getResources().getStringArray(R.array.hide_account_list);
-        if (hideAccounts == null || hideAccounts.length == 0) return true;
+        String[] showAccounts = context.getResources().getStringArray(R.array.show_account_list);
+        if (showAccounts == null || showAccounts.length == 0) return false;
 
-        for (String account : hideAccounts) {
-            if (account.equals(accountType)) return false;
+        for (String account : showAccounts) {
+            if (account.equals(accountType)) return true;
         }
-        return true;
+        return false;
     }
 
     /**
