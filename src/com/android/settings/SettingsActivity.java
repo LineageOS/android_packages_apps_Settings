9ec054969601913e86c5e987db8383f2dc9646c4
Steve Kondik <steve@cyngn.com>
2016-10-26 16:29:11 -0700

    Revert "Settings: Add enable/disable 4G settings."
     * More pointless crap
    
    This reverts commit 0fa4364e120f5d4085de3707d5170105e601e544.
    
    Change-Id: If83914bd538a7d9bdafe03314b7209cd34d12c00

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -232,7 +232,6 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private static final String ACTION_TIMER_SWITCH = "qualcomm.intent.action.TIMER_SWITCH";
 
-    private static final String LTE_4G_FRAGMENT = "com.android.settings.Lte4GEnableSetting";
     private String mFragmentClass;
     private String mActivityAction;
 
@@ -247,7 +246,6 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.DataUsageSummaryActivity.class.getName(),
             Settings.RoamingSettingsActivity.class.getName(),
             Settings.SimSettingsActivity.class.getName(),
-            Settings.Lte4GEnableActivity.class.getName(),
             Settings.WirelessSettingsActivity.class.getName(),
             //device_section
             Settings.HomeSettingsActivity.class.getName(),
@@ -1035,13 +1033,6 @@ public class SettingsActivity extends SettingsDrawerActivity
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
-        if (LTE_4G_FRAGMENT.equals(fragmentName)) {
-            Intent newIntent = new Intent("android.settings.SETTINGS");
-            newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(newIntent);
-            finish();
-            return null;
-        }
 
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
@@ -1090,10 +1081,6 @@ public class SettingsActivity extends SettingsDrawerActivity
         setTileEnabled(new ComponentName(packageName,
                 Settings.BluetoothSettingsActivity.class.getName()),
                 pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH), isAdmin, pm);
-
-        setTileEnabled(new ComponentName(packageName,
-                Settings.Lte4GEnableActivity.class.getName()),
-                getResources().getBoolean(R.bool.config_4gsettings_enabled), isAdmin, pm);
 
         setTileEnabled(new ComponentName(packageName,
                 Settings.DataUsageSummaryActivity.class.getName()),
