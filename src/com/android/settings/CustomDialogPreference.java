fa947a458a6c3f34b9528520f816608845331e90
LuK1337 <priv.luk@gmail.com>
2016-11-27 12:46:39 +0100

    Settings: Add vibrator intensity dialog
    Port the related changes from the following cm-13.0 commits:
    a284b34f11b3ad22084232950d81fb1bc68f21ce settings: Squashed commit of CMHW features
    d4ebf3a59930db25be8296c88b40c3ee4330768f Settings: Cleanup display settings code
    ba4c43f77699721dff7c2536ee61c0f8c18ea446 Settings: Change to CmHardwareService
    d2d9f45236308fc9f722bf897030d1aacfbc8be8 settings: Move CMHW to CMSDK
    bd3fe676619443b9060ffaa85abfe8e632e4340c Add isSupported() method to VibratorIntensity class
    9c35338443d9d9d84bbfe4c899d82d2da1f85b60 Settings : Add multiuser support for CMHardware
    bac4604893d4e24682e7bc016c362a2793bf1069 Settings: Fix one-time vibration intensity restore
    a3a64ce79d1f7f2bb848cfa7af2260be6360ae0e Settings: Rework vibration intensity slider
    76997a7856c5185c143088268d76bd760cf67a89 Settings: Save vibration intensity on close
    a44bfb5a396056e22825f4989eee47b90b7bbc14 Settings: Move vibrator value restore to onActivityStop
    9176a64ebcf2f59b01f0159ae129c56230b28cb6 Settings: Improve vibrator intensity dialog layout
    
    Change-Id: Iece36085f8f07a171affefd9a1027fb11fed5d93

--- src/com/android/settings/CustomDialogPreference.java
+++ src/com/android/settings/CustomDialogPreference.java
@@ -71,6 +71,10 @@ public class CustomDialogPreference extends DialogPreference {
         mFragment = fragment;
     }
 
+    protected boolean onDismissDialog(final DialogInterface dialog, final int which) {
+        return true;
+    }
+
     public static class CustomPreferenceDialogFragment extends PreferenceDialogFragment {
 
         public static CustomPreferenceDialogFragment newInstance(String key) {
@@ -83,6 +87,44 @@ public class CustomDialogPreference extends DialogPreference {
 
         private CustomDialogPreference getCustomizablePreference() {
             return (CustomDialogPreference) getPreference();
+        }
+
+        private class OnDismissListener implements View.OnClickListener {
+            private final int mWhich;
+            private final DialogInterface mDialog;
+
+            public OnDismissListener(final DialogInterface dialog, final int which) {
+                mWhich = which;
+                mDialog = dialog;
+            }
+
+            @Override
+            public void onClick(final View view) {
+                CustomPreferenceDialogFragment.this.onClick(mDialog, mWhich);
+                if (getCustomizablePreference().onDismissDialog(mDialog, mWhich)) {
+                    mDialog.dismiss();
+                }
+            }
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            if (getDialog() instanceof AlertDialog) {
+                AlertDialog a = (AlertDialog)getDialog();
+                if (a.getButton(Dialog.BUTTON_NEUTRAL) != null) {
+                    a.getButton(Dialog.BUTTON_NEUTRAL).setOnClickListener(
+                            new OnDismissListener(a, Dialog.BUTTON_NEUTRAL));
+                }
+                if (a.getButton(Dialog.BUTTON_POSITIVE) != null) {
+                    a.getButton(Dialog.BUTTON_POSITIVE).setOnClickListener(
+                            new OnDismissListener(a, Dialog.BUTTON_POSITIVE));
+                }
+                if (a.getButton(Dialog.BUTTON_NEGATIVE) != null) {
+                    a.getButton(Dialog.BUTTON_NEGATIVE).setOnClickListener(
+                            new OnDismissListener(a, Dialog.BUTTON_NEGATIVE));
+                }
+            }
         }
 
         @Override
