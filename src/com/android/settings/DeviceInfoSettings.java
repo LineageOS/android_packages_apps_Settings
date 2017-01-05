4d18ba216a21449f85bc8a7b9c95a58b17650b23
Michael Bestas <mikeioannina@gmail.com>
2017-01-06 00:41:41 +0200

    Change intent extras for LineageOS platlogo
    Change-Id: I8a96f51dbef707c0971a787e56efd0e4aefa8e23

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -322,7 +322,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.putExtra("is_cm", true);
+                intent.putExtra("is_lineage", true);
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
                 try {
