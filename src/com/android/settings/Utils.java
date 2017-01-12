5b152577bdb1b7f9dcb51f06d81f3a8519d69767
Arne Coucheron <arco68@gmail.com>
2017-01-12 01:38:42 +0100

    Revert "DataUsageSummary: Invoke vendor part of the MobileNetwiorkSettings"
    This reverts commit ac60addc6e3f8f13246b2df18e6614034a1c3fee.
    
    Change-Id: I2934b3f7002b4a351508f9ceb22923f80596554a

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1203,26 +1203,6 @@ public final class Utils extends com.android.settingslib.Utils {
         return false;
     }
 
-    /**
-     * check whether NetworkSetting apk exist in system, if yes, return true, else
-     * return false.
-     */
-    public static boolean isNetworkSettingsApkAvailable(Context context) {
-        // check whether the target handler exist in system
-        Intent intent = new Intent("org.codeaurora.settings.NETWORK_OPERATOR_SETTINGS_ASYNC");
-        PackageManager pm = context.getPackageManager();
-        List<ResolveInfo> list = pm.queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : list){
-            // check is it installed in system.img, exclude the application
-            // installed by user
-            if ((resolveInfo.activityInfo.applicationInfo.flags &
-                    ApplicationInfo.FLAG_SYSTEM) != 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static boolean isPackageDirectBootAware(Context context, String packageName) {
         try {
             final ApplicationInfo ai = context.getPackageManager().getApplicationInfo(
