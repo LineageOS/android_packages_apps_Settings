c1a2f1eb0d0bfc911f81d4189dc03742ddeae850
Arne Coucheron <arco68@gmail.com>
2017-01-12 01:38:35 +0100

    Revert "TMO: Settings Support for Self Service/MyAccount requirements from TMO"
    This reverts commit 353e52ee784a231cdb52b4b7976ee5e4bac827e6.
    
    Change-Id: Ibb597d5e41f6f8f5e5a29ef2d372347541283304

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -105,9 +105,6 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
 
     private PreferenceScreen mButtonWfc;
 
-    private static final String VOICE_OVER_LTE = "voice_over_lte";
-    private SwitchPreference mVoLtePreference;
-    private boolean mLteEnabled = false;
     /**
      * Invoked on each preference click in this hierarchy, overrides
      * PreferenceFragment's implementation.  Used to make sure we track the
@@ -139,8 +136,6 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
             }
             startActivity(intent);
             return true;
-        } else if (mLteEnabled && preference == mVoLtePreference) {
-            ImsManager.setEnhanced4gLteModeSetting(getActivity(), mVoLtePreference.isChecked());
         } else if (preference == findPreference(KEY_MOBILE_NETWORK_SETTINGS)
                 && mIsNetworkSettingsAvailable) {
             onMobileNetworkSettingsClick();
@@ -391,14 +386,6 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
             // Grey out if provisioning is not available.
             p.setEnabled(!TetherSettings
                     .isProvisioningNeededButUnavailable(getActivity()));
-        mLteEnabled = getActivity().getResources().getBoolean(R.bool.config_voice_over_lte_enabled);
-        mVoLtePreference = (SwitchPreference) findPreference(VOICE_OVER_LTE);
-        if (mLteEnabled) {
-            mVoLtePreference.setChecked(
-                    ImsManager.isEnhanced4gLteModeSettingEnabledByUser(getActivity()));
-        } else {
-            getPreferenceScreen().removePreference(mVoLtePreference);
-        }
         }
 
         // Remove network reset if not allowed
