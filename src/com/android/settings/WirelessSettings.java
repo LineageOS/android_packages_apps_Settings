a9d1a3488c524e66ad3f060d1cd11289dbfa3d0c
Arne Coucheron <arco68@gmail.com>
2017-01-12 01:38:55 +0100

    Revert "Invoke vendor part of the MobileNetworkSettings"
    This reverts commit a1768aecaee76afa30e7f5d7960db201bb6d7b31.
    
    Change-Id: I08b145d60f49601d3c73806fe4b72956d2b7d42a

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -26,8 +26,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
@@ -98,8 +96,6 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
     private PackageManager mPm;
     private UserManager mUm;
 
-    boolean mIsNetworkSettingsAvailable = false;
-
     private static final int MANAGE_MOBILE_PLAN_DIALOG_ID = 1;
     private static final String SAVED_MANAGE_MOBILE_PLAN_MSG = "mManageMobilePlanMessage";
 
@@ -136,45 +132,11 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
             }
             startActivity(intent);
             return true;
-        } else if (preference == findPreference(KEY_MOBILE_NETWORK_SETTINGS)
-                && mIsNetworkSettingsAvailable) {
-            onMobileNetworkSettingsClick();
-            return true;
         }
         // Let the intents be launched by the Preference manager
         return super.onPreferenceTreeClick(preference);
     }
 
-    /**
-     * check whether NetworkSetting apk exist in system, if yes, return true, else
-     * return false.
-     */
-    private boolean isNetworkSettingsApkAvailable() {
-
-            // check whether the target handler exist in system
-            Intent intent = new Intent("org.codeaurora.settings.NETWORK_OPERATOR_SETTINGS_ASYNC");
-            List<ResolveInfo> list = mPm.queryIntentActivities(intent, 0);
-            for (ResolveInfo resolveInfo : list){
-                // check is it installed in system.img, exclude the application
-                // installed by user
-                if ((resolveInfo.activityInfo.applicationInfo.flags &
-                        ApplicationInfo.FLAG_SYSTEM) != 0) {
-                    return true;
-                }
-            }
-        return false;
-    }
-
-    public void onMobileNetworkSettingsClick() {
-        log("onMobileNetworkSettingsClick:");
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        log("Qcom Proprietary MobileNetworkSettings Enabled");
-        // prepare intent to start MobileNetworkSettings activity from
-        // Qcom Proprietary
-        intent.setComponent(new ComponentName("com.qualcomm.qti.networksetting",
-               "com.qualcomm.qti.networksetting.MobileNetworkSettings"));
-        startActivity(intent);
-    }
     private String mManageMobilePlanMessage;
     public void onManageMobilePlanClick() {
         log("onManageMobilePlanClick:");
@@ -363,8 +325,6 @@ public class WirelessSettings extends SettingsPreferenceFragment implements Inde
                         UserManager.DISALLOW_CONFIG_MOBILE_NETWORKS, UserHandle.myUserId())) {
             removePreference(KEY_MOBILE_NETWORK_SETTINGS);
             removePreference(KEY_MANAGE_MOBILE_PLAN);
-        } else {
-            mIsNetworkSettingsAvailable = isNetworkSettingsApkAvailable();
         }
         // Remove Mobile Network Settings and Manage Mobile Plan
         // if config_show_mobile_plan sets false.
