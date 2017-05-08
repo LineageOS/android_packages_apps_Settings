a22ed5ac14531a19abeba2e997b49d285521d728
klabit87 <keith.labit@gmail.com>
2017-05-07 22:08:22 -0500

    Fix Settings FC caused by changelog viewer

--- src/com/android/settings/aim/fragments/Changelog.java
+++ src/com/android/settings/aim/fragments/Changelog.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016 Nitrogen Project
+    Copyright (C) 2016
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
     limitations under the License.
  */
 
-package com.android.settings.fragments.Changelog;
+package com.android.settings.aim.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
