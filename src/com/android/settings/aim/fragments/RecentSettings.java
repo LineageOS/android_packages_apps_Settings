37e09f0f85bcf7821c0e8ba60a004128c8bca55a
Anand Ramachandran <anand.nv1998@gmail.com>
2017-06-02 15:59:09 +0530

    FreedomHub: Add Recents tab for recents tweaks
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

---
+++ src/com/android/settings/aim/fragments/RecentSettings.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017 AIM ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aim.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class RecentSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.aim_recents_tab);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
+}
