ResurrectionRemix/Resurrection_packages_apps_Settings

Settings:Incall Vibration options [3/3]
Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/android/settings/rr/InCallSettings.java
@@ -1,0 +1,47 @@
+/*Copyright (C) 2015 The ResurrectionRemix Project
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+package com.android.settings.rr;
+
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class InCallSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.RESURRECTED;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.rr_incall);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) 		{
+        return true;
+    }
+}

LineageOS/android_packages_apps_Settings

Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
Conflicts:
	res/values/aim_strings.xml
	res/xml/aim_misc.xml
	src/com/android/settings/aim/MiscSettings.java

src/com/android/settings/aim/InCallSettings.java
@@ -1,0 +1,47 @@
+/*Copyright (C) 2017 AIM ROM
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+package com.android.settings.aim;
+
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class InCallSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aim_incall);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) 		{
+        return true;
+    }
+}
5b2286c88fd6f623073b0b86ffa74b7b1f9d14ec
beanstown106 <nbenis106@gmail.com>
2017-05-12 15:27:47 +0530

    Settings:Incall Vibration options [3/3]
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
    
    Conflicts:
    	res/values/aim_strings.xml
    	res/xml/aim_misc.xml
    	src/com/android/settings/aim/MiscSettings.java

--- src/com/android/settings/aim/tabs/Misc.java
+++ src/com/android/settings/aim/tabs/Misc.java
@@ -5,15 +5,27 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import android.os.Bundle;
 import com.android.settings.R;
 
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.android.settings.Utils;
+
+import android.telephony.TelephonyManager;
 public class Misc extends SettingsPreferenceFragment {
+
+private static final String AIM_INCALL = "aim_incall";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.aim_misc_tab);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(AIM_INCALL);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
     }
 
     @Override
