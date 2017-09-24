4a007abc99364b0d2b12ba38d31768d7f83c86b2
Harry Youd <harry@harryyoud.co.uk>
2017-09-24 18:36:04 +0100

    Settings: Add LineageOS version to "About phone"
    Change-Id: Ia5f125d711654638e87f7cfea5216adf8454d6da

---
+++ src/com/android/settings/deviceinfo/LineageOSVersionPreferenceController.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.PreferenceController;
+
+public class LineageOSVersionPreferenceController extends PreferenceController {
+
+    private static final String KEY_LINEAGE_VERSION = "lineage_version";
+    private static final String KEY_LINEAGE_VERSION_PROP = "ro.lineage.version";
+
+    public LineageOSVersionPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_LINEAGE_VERSION;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        preference.setSummary(SystemProperties.get(KEY_LINEAGE_VERSION_PROP,
+                mContext.getResources().getString(R.string.lineage_version_default)));
+    }
+}
