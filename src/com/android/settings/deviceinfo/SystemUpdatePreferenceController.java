c5f1365c5a632242afadf9d515db79662eccac10
Harry Youd <harry@harryyoud.co.uk>
2017-09-27 17:11:51 +0100

    Settings: Disable stock system updates provided by GMS
    Change-Id: I25d2a3c37492a2111fa746133a0705b63a90ea42

--- src/com/android/settings/deviceinfo/SystemUpdatePreferenceController.java
+++ src/com/android/settings/deviceinfo/SystemUpdatePreferenceController.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +48,7 @@ public class SystemUpdatePreferenceController extends PreferenceController {
 
     @Override
     public boolean isAvailable() {
-        return mUm.isAdminUser();
+        return false;
     }
 
     @Override
