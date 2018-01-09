e28237cda2e9de165a4b65a5ba0c9e87d938f76d
Bruno Martins <bgcngm@gmail.com>
2018-01-09 15:41:02 +0000

    DevelopmentSettings: Hide OEM unlock by default
     * The "OEM unlocking" toogle should not be visible on any
       device running LineageOS, for obvious reasons. Allow it only
       to show up for Google devices, such as Pixel, that set
       ro.oem_unlock_supported=1.
    
    Change-Id: I2b09974fc24e783b985de213b504e964771cbcb7

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -418,7 +418,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mKeepScreenOn = (RestrictedSwitchPreference) findAndInitSwitchPref(KEEP_SCREEN_ON);
         mBtHciSnoopLog = findAndInitSwitchPref(BT_HCI_SNOOP_LOG);
         mEnableOemUnlock = (RestrictedSwitchPreference) findAndInitSwitchPref(ENABLE_OEM_UNLOCK);
-        if (!showEnableOemUnlockPreference()) {
+        if (!showEnableOemUnlockPreference()) ||
+                !SystemProperties.getBoolean("ro.oem_unlock_supported", false)) {
             removePreference(mEnableOemUnlock);
             mEnableOemUnlock = null;
         }
