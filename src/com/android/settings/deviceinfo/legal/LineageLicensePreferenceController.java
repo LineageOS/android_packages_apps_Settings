8b444840f1b0e076d9efff2cd7edd03f2eb2d81b
Roman Birg <roman@cyngn.com>
2018-02-22 15:27:46 +0100

    Settings: Add LineageOS legal info
    Open up the LineageOS legal info in the browser.
    
    Change-Id: I263ccc0509e275d17512528deb606341d58e7a0d
    Ticket-Id: CYNGNOS-1895
    Signed-off-by: Roman Birg <roman@cyngn.com>

---
+++ src/com/android/settings/deviceinfo/legal/LineageLicensePreferenceController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.legal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.SystemProperties;
+
+public class LineageLicensePreferenceController extends LegalPreferenceController {
+
+    private static final String PROPERTY_LINEAGELICENSE_URL = "ro.lineagelegal.url";
+
+    public LineageLicensePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    protected Intent getIntent() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse(SystemProperties.get(PROPERTY_LINEAGELICENSE_URL)));
+        return intent;
+    }
+}
