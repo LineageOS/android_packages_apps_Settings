469a9e1348c3300430ae733f8abb4a0e8c6be2ff
juwei <juwei@codeaurora.org>
2018-05-07 13:31:50 +0800

    Settings: Add switch preference for AGPS
    Allow toggling AGPS (which in turn disables SUPL) using the existing
    AOSP mechanism that was simply never exposed to the user.
    Based on the following commits from CodeAurora, modified to
    use SwitchPreference and improved strings:
    
    Author: juwei <juwei@codeaurora.org>
    Date:   Mon May 7 13:31:50 2018 +0800
        Settings: Add AGPS setting menu.
    
        Add AGPS setting switch to allow user turn on/off
        AGPS function.
    
        Change-Id: If8a20a8293b9f4b12933bc3820b4cd135a938b72
        CRs-Fixed: 2169854
    
    Author: hoffc <hoffc@codeaurora.org>
    Date:   Wed Dec 19 15:56:07 2018 +0800
        Settings: Refactor Agps preference controller code
    
        (1) Extract inner class AgpsPreferenceController as one standalone file.
        (2) Delete the useless code of reading the SUPL server and port from etc/gps.conf file.
        (3) Remove the config item "config_agps_enabled".
    
        Change-Id: I20465aa4375c7fe64bc84f579a39178506b8e211
        CRs-Fixed: 2370304
    
    Author: hoffc <hoffc@codeaurora.org>
    Date:   Tue Jun 1 08:52:31 2021 +0800
        Settings: fix NPE caused by missing agps preference definition
    
        No agps preference is defined in location settings, results in NPE when agps controller init
    
        add agps preference definition in location settings
    
        Change-Id: I92e3293bccb90da7b6a1057e2dd83f8aa909d308
        CRs-Fixed: 2958241
    
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Sat Feb 25 03:03:23 2023 +0530
    
        AGPS: Mention that it's ignored during user-initiated emergency calls
    
        Issue: calyxos#1490
        Change-Id: Iab13be99ccd65e0fdabcb67a25d73944ef24556a
    
    Issue: calyxos#1490
    Change-Id: Idea7026f0c954c1f2a056e7b4595a879ef161f5c

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -120,6 +120,7 @@ public class LocationSettings extends DashboardFragment implements
         use(LocationForWorkPreferenceController.class).init(this);
         use(LocationSettingsFooterPreferenceController.class).init(this);
         use(LocationForPrivateProfilePreferenceController.class).init(this);
+        use(AgpsPreferenceController.class).init(this);
     }
 
     @Override
