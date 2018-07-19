efa41ca36940db7afa83c19be49544a32a4bce29
Pingzhi Wang <pingzhi.xa.wang@sony.com>
2018-07-19 11:01:33 +0800

    Button text for app space management is overwritten
    Apps that have implemented a managed space activity
    gets the wrong label in Settings../App info/"Storage & cache"
    
    Test: manual
    Bug: 111674319
    Change-Id: I1f7cccf0558960f867a8bf2d0ee14dc5ec9fd7a9

--- src/com/android/settings/applications/AppStorageSettings.java
+++ src/com/android/settings/applications/AppStorageSettings.java
@@ -316,10 +316,7 @@ public class AppStorageSettings extends AppInfoWithHeader
                         .setButton1Text(R.string.clear_user_data_text)
                         .setButton1Icon(R.drawable.ic_settings_delete);
             }
-            mButtonsPref
-                    .setButton1Text(R.string.clear_user_data_text)
-                    .setButton1Icon(R.drawable.ic_settings_delete)
-                    .setButton1OnClickListener(v -> handleClearDataClick());
+            mButtonsPref.setButton1OnClickListener(v -> handleClearDataClick());
         }
 
         if (mAppsControlDisallowedBySystem || AppUtils.isMainlineModule(mPm, mPackageName)) {
