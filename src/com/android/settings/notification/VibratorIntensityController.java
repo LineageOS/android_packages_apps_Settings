2faa547efe11eeafc39dd7308d23580a8531f37d
Joey <joey@lineageos.org>
2018-07-30 11:53:43 +0200

    Settings: add HIDL vibration intensity preference
    * Adapt cmhw code to hidl implementation
    
    Port the related changes from the following cm-14.1 commits:
    35d2546596c13a4de9fcdc18f55ae17b958121b9 Settings: Add CMHW vibrator intensity preference
    
    Port the related changes from the following cm-13.0 commits:
    a284b34f11b3ad22084232950d81fb1bc68f21ce settings: Squashed commit of CMHW features
    d4ebf3a59930db25be8296c88b40c3ee4330768f Settings: Cleanup display settings code
    ba4c43f77699721dff7c2536ee61c0f8c18ea446 Settings: Change to CmHardwareService
    d2d9f45236308fc9f722bf897030d1aacfbc8be8 settings: Move CMHW to CMSDK
    bd3fe676619443b9060ffaa85abfe8e632e4340c Add isSupported() method to VibratorIntensity class
    9c35338443d9d9d84bbfe4c899d82d2da1f85b60 Settings : Add multiuser support for CMHardware
    bac4604893d4e24682e7bc016c362a2793bf1069 Settings: Fix one-time vibration intensity restore
    a3a64ce79d1f7f2bb848cfa7af2260be6360ae0e Settings: Rework vibration intensity slider
    76997a7856c5185c143088268d76bd760cf67a89 Settings: Save vibration intensity on close
    a44bfb5a396056e22825f4989eee47b90b7bbc14 Settings: Move vibrator value restore to onActivityStop
    9176a64ebcf2f59b01f0159ae129c56230b28cb6 Settings: Improve vibrator intensity dialog layout
    
    Change-Id: Iece36085f8f07a171affefd9a1027fb11fed5d93
    Signed-off-by: Joey <joey@lineageos.org>

---
+++ src/com/android/settings/notification/VibratorIntensityController.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+import android.os.RemoteException;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.util.NoSuchElementException;
+
+import vendor.lineage.vibrator.V1_0.IVibrator;
+
+public class VibratorIntensityController extends AbstractPreferenceController
+        implements PreferenceControllerMixin {
+
+    private static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
+
+    public VibratorIntensityController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        try {
+            IVibrator server = IVibrator.getService();
+            return server != null;
+        } catch (NoSuchElementException | RemoteException ignored) {
+        }
+        return false;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_VIBRATOR_INTENSITY;
+    }
+}
