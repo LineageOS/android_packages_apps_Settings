67ef016e5fc49f8dd1c5a5f85ec0f0f7955216c9
nx111 <NX111.AimH@gmail.com>
2019-06-09 22:08:52 +0800

    settings: appops: support setting for work profile apps.
    Change-Id: Ic66a4cb31d910509cd094f77618054c36bad27cf

--- src/com/android/settings/applications/appops/AppOpsState.java
+++ src/com/android/settings/applications/appops/AppOpsState.java
@@ -29,6 +29,7 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.UserHandle;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.util.SparseArray;
@@ -602,12 +603,12 @@ public class AppOpsState {
     }
 
     private AppEntry getAppEntry(final Context context, final HashMap<String, AppEntry> appEntries,
-            final String packageName, ApplicationInfo appInfo, boolean applyFilters) {
+            final String packageName, ApplicationInfo appInfo, int userId, boolean applyFilters) {
 
         if (appInfo == null) {
             try {
-                appInfo = mPm.getApplicationInfo(packageName,
-                        PackageManager.GET_DISABLED_COMPONENTS);
+                appInfo = mPm.getApplicationInfoAsUser(packageName,
+                        PackageManager.GET_DISABLED_COMPONENTS, userId);
             } catch (PackageManager.NameNotFoundException e) {
                 Log.w(TAG, "Unable to find info for package " + packageName);
                 return null;
@@ -627,11 +628,11 @@ public class AppOpsState {
             }
         }
 
-        AppEntry appEntry = appEntries.get(packageName);
+        AppEntry appEntry = appEntries.get(String.format("%d_%s", userId, packageName));
         if (appEntry == null) {
             appEntry = new AppEntry(this, appInfo);
             appEntry.loadLabel(context);
-            appEntries.put(packageName, appEntry);
+            appEntries.put(String.format("%d_%s",userId, packageName), appEntry);
         }
         return appEntry;
     }
@@ -715,7 +716,8 @@ public class AppOpsState {
         if (pkgs != null) {
             for (int i=0; i<pkgs.size(); i++) {
                 AppOpsManager.PackageOps pkgOps = pkgs.get(i);
-                AppEntry appEntry = getAppEntry(context, appEntries, pkgOps.getPackageName(), null,
+                int userId = UserHandle.getUserId(pkgOps.getUid());
+                AppEntry appEntry = getAppEntry(context, appEntries, pkgOps.getPackageName(), null, userId,
                         applyFilters);
                 if (appEntry == null) {
                     continue;
@@ -740,7 +742,7 @@ public class AppOpsState {
         if (packageName != null) {
             apps = new ArrayList<PackageInfo>();
             try {
-                PackageInfo pi = mPm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
+                PackageInfo pi = mPm.getPackageInfoAsUser(packageName, PackageManager.GET_PERMISSIONS, UserHandle.getUserId(uid));
                 apps.add(pi);
             } catch (NameNotFoundException e) {
             }
@@ -751,8 +753,12 @@ public class AppOpsState {
         }
         for (int i=0; i<apps.size(); i++) {
             PackageInfo appInfo = apps.get(i);
+            int userId = UserHandle.getUserId(uid);
+            if (appInfo.applicationInfo != null) {
+                userId = UserHandle.getUserId(appInfo.applicationInfo.uid);
+            }
             AppEntry appEntry = getAppEntry(context, appEntries, appInfo.packageName,
-                    appInfo.applicationInfo, applyFilters);
+                    appInfo.applicationInfo, userId, applyFilters);
             if (appEntry == null) {
                 continue;
             }
