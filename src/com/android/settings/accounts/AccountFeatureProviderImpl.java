cf7328621f344bf4329c2fd3d04ad3fe63760182
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 12:20:19 -0300

    Settings: Set the default account provider to Google
    Change-Id: I4cedd3ea99be911e874270bee86f9c5447cc4268

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,16 +1,17 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return null;
+        return "com.google";
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return new Account[0];
+        return AccountManager.get(context).getAccountsByType("com.google");
     }
 }
