ab2d29081a66f105b66518e941cb1897c7233940
althafvly <althafvly@gmail.com>
2019-10-25 07:16:45 +0000

    Settings: Protect sensitive data on the about phone
    - Hide some personal info like phone number and IMEI.
    - IMEI shows on a tap, so why not.
    - Avoid showing this in screenshot/screenrecord
    
    Co-authored-by: El Dainosor <eldainosor@gmail.com>
    Change-Id: I45b7898da440c36045aec64a0a86fb804116cd9d

--- src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
@@ -56,7 +56,23 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
 
     @Override
     public CharSequence getSummary() {
-        return getFirstPhoneNumber();
+        return mContext.getString(R.string.device_info_protected_single_press);
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        String prefKey = preference.getKey();
+        if (prefKey.startsWith(KEY_PHONE_NUMBER)) {
+            int simSlotNumber = 0;
+            if (!TextUtils.equals(prefKey, KEY_PHONE_NUMBER)) {
+                // Get multisim slot number from preference key.
+                // Multisim preference key is KEY_PHONE_NUMBER + simSlotNumber
+                simSlotNumber = Integer.parseInt(
+                        prefKey.replaceAll("[^0-9]", ""));
+            }
+            updatePhoneNumber(simSlotNumber, true);
+        }
+        return super.handlePreferenceTreeClick(preference);
     }
 
     @Override
@@ -73,7 +89,6 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
             final Preference multiSimPreference = createNewPreference(screen.getContext());
             multiSimPreference.setOrder(phonePreferenceOrder + simSlotNumber);
             multiSimPreference.setKey(KEY_PHONE_NUMBER + simSlotNumber);
-            multiSimPreference.setSelectable(false);
             category.addPreference(multiSimPreference);
             mPreferenceList.add(multiSimPreference);
         }
@@ -82,10 +97,15 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
     @Override
     public void updateState(Preference preference) {
         for (int simSlotNumber = 0; simSlotNumber < mPreferenceList.size(); simSlotNumber++) {
-            final Preference simStatusPreference = mPreferenceList.get(simSlotNumber);
-            simStatusPreference.setTitle(getPreferenceTitle(simSlotNumber));
-            simStatusPreference.setSummary(getPhoneNumber(simSlotNumber));
+            updatePhoneNumber(simSlotNumber, false);
         }
+    }
+
+    private void updatePhoneNumber(int simSlotNumber, boolean enable) {
+        final Preference simStatusPreference = mPreferenceList.get(simSlotNumber);
+        simStatusPreference.setTitle(getPreferenceTitle(simSlotNumber));
+        simStatusPreference.setSummary(
+                enable ? getPhoneNumber(simSlotNumber) : getSummary());
     }
 
     @Override
