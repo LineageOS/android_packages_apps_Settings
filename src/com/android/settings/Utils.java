b432122effe2b37e12c64f34b3454abb28aad28d
Danny Baumann <dannybaumann@web.de>
2019-10-30 13:52:55 +0100

    Don't change nouns in summaries to lower case for German
    In German, nouns are capitalized.
    
    Change-Id: Iff7770c2f1f761fbaccc241c929cf1cf4097cb57

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1170,4 +1170,11 @@ public final class Utils extends com.android.settingslib.Utils {
         }
         return isMediaOutputDisabled;
     }
+
+    public static String normalizeTitleCaseIfRequired(Context context, String input) {
+        if (!context.getResources().getBoolean(R.bool.language_capitalizes_nouns)) {
+            return input.toLowerCase();
+        }
+        return input;
+    }
 }
