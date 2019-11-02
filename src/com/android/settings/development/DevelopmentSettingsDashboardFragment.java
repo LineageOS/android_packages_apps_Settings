62093e5ce85bb191daaa2ae790e624a1638bb5ad
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-02 23:09:40 +0100

    Add toggle to enable ADB root
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: Ic80dbf79265c0fe7113f42299479873befb05004

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -531,6 +531,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new AdbPreferenceController(context, fragment));
         controllers.add(new ClearAdbKeysPreferenceController(context, fragment));
         controllers.add(new AdbWirelessDebuggingPreferenceController(context, lifecycle));
+        controllers.add(new AdbRootPreferenceController(context, fragment));
         controllers.add(new AdbAuthorizationTimeoutPreferenceController(context));
         controllers.add(new LocalTerminalPreferenceController(context));
         controllers.add(new LinuxTerminalPreferenceController(context));
