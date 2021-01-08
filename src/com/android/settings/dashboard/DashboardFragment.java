c98196ffe67f02848c8ed832b7e81bb1672bcc85
Michael W <baddaemon87@gmail.com>
2021-01-08 16:50:53 +0100

    Settings: Remove empty category from Security Settings
    * The first category only holds google play protect and find my device,
      which require a gapps installation. For non-gapps installs, this category
      simply shows the title
    * Remove it when it's actually empty
    
    Change-Id: I36eea8d289502b0f74fe719d717228cb371f13ca

--- src/com/android/settings/dashboard/DashboardFragment.java
+++ src/com/android/settings/dashboard/DashboardFragment.java
@@ -435,6 +435,7 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
         }
 
         updatePreferenceVisibility(mPreferenceControllers);
+        updateCategoryVisibility();
     }
 
     @VisibleForTesting
@@ -457,6 +458,10 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
             }
         }
     }
+
+    // declaring abstract would be better but requires too many classes to be modified, so just
+    // make it public and don't do anything in the default case
+    public void updateCategoryVisibility() { }
 
     /**
      * Refresh preference items backed by DashboardCategory.
