3e6c9e42ab08476b406c297007f50408d95c7a0f
flawedworld <38294951+flawedworld@users.noreply.github.com>
2021-04-06 01:13:36 +0100

    Add device info field with bootloader version
    Change-Id: I5133236306206986077344364750419203138139

---
+++ src/com/android/settings/deviceinfo/firmwareversion/BootloaderVersionPreferenceController.java
@@ -0,0 +1,24 @@
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.Build;
+
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+
+public class BootloaderVersionPreferenceController extends BasePreferenceController {
+
+    public BootloaderVersionPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return TextUtils.isEmpty(Build.BOOTLOADER) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return Build.BOOTLOADER;
+    }
+}
