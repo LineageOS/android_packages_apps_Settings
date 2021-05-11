040fb96e26ff888b3311410e4c55e48923d924a4
LuK1337 <priv.luk@gmail.com>
2021-05-11 11:32:38 +0200

    Settings: Make setup wizard go through ChooseLockPatternSize if needed
    Change-Id: Idfd3670d4b541b3618c4416b3b9dbf7d88ae3ed1

--- src/com/android/settings/password/SetupChooseLockPattern.java
+++ src/com/android/settings/password/SetupChooseLockPattern.java
@@ -42,7 +42,8 @@ import com.google.android.setupdesign.GlifLayout;
 public class SetupChooseLockPattern extends ChooseLockPattern {
 
     public static Intent modifyIntentForSetup(Context context, Intent chooseLockPatternIntent) {
-        chooseLockPatternIntent.setClass(context, SetupChooseLockPattern.class);
+        chooseLockPatternIntent.setClass(context, ChooseLockPatternSize.class);
+        chooseLockPatternIntent.putExtra("className", SetupChooseLockPattern.class.getName());
         return chooseLockPatternIntent;
     }
 
