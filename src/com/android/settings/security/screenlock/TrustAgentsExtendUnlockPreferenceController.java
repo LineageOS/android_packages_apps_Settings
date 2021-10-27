efe02a5c07de4418e2907f9893dc0df0541da551
Paul Keith <javelinanddart@gmail.com>
2021-10-27 00:47:49 -0500

    Settings: Add more options for trust agents
    * Bring back options that used to be in developer options:
     - Trust agents only extend unlock (default true)
     - Lock device when trust is lost (default false)
    
    Change-Id: I41ceae32f53d7358c1bf8ea9fb2e45a8d9db9bf4

---
+++ src/com/android/settings/security/screenlock/TrustAgentsExtendUnlockPreferenceController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.screenlock;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.TwoStatePreference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class TrustAgentsExtendUnlockPreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_TRUST_AGENTS_EXTEND_UNLOCK =
+        "security_setting_trust_agents_extend_unlock";
+
+    private final int mUserId;
+
+    public TrustAgentsExtendUnlockPreferenceController(Context context, int userId) {
+        super(context);
+        mUserId = userId;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_TRUST_AGENTS_EXTEND_UNLOCK;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Settings.System.putIntForUser(
+                mContext.getContentResolver(),
+                Settings.Secure.TRUST_AGENTS_EXTEND_UNLOCK,
+                (Boolean) newValue ? 1 : 0,
+                mUserId);
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        ((TwoStatePreference) preference).setChecked(Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.Secure.TRUST_AGENTS_EXTEND_UNLOCK,
+                1,
+                mUserId) == 1);
+    }
+}
