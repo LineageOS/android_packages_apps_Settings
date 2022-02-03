cc68943d6d21392cc1c039d316adc6bd6cf603e6
Bruno Martins <bgcngm@gmail.com>
2022-02-03 21:03:02 +0000

    fingerprint: Rework enrollment screen
    This is based on the following changes, adapted to Android 12
    considering the fact that AOSP now supports FOD as well as
    fingerprint sensors embedded in the power button:
    
    Author: d34d <clark@cyngn.com>
    Date:   2016-06-23 11:21:45 -0700
    
        fingerprint: Allow devices to configure sensor location
    
        The text for enrolling a fingerprint always assumes the sensor is
        located on the back.  This patch adds a config integer that defines
        where the sensor is located on the device.
          0 = back
          1 = front
          2 = left side
          3 = right side
    
        This patch also adds a new drawable that shows the front of the device
        when applicable.
    
        Change-Id: Ia8f5f8ff4ba4d13d35aed3052f60ff665dd4f294
        TICKET: CYNGNOS-3089
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   2018-01-26 22:23:03 +0100
    
        FingerprintEnrollFindSensor: Don't overlay front for side sensors
    
        * Both the base image and the base image + overlay are equally nonsense
          for devices with a side fingerprint scanner, but for those that overlay
          a custom fingerprint enrollment graphic, this image overlay wreaks havoc
        * Only set this overlay to be visible if the sensor is on the front, to
          allow devices with side sensors to only overlay the base image
    
        Change-Id: I7cbcea4830d4526cf9f7d139e7f84f21117fa9c4
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Thu Jan 17 21:44:28 2019 +0100
    
        Settings: Make find fingerprint look great again
    
        * The current logic places the animation based on the size of
          the whole overlay instead of the image, which is a fixed-size png
        * Get the size of the image instead and place the dot relative to it
        * Also use "wrap_content" and add a padding to the top so the
          distance to the text is a fixed thing, not depending on how
          much space relative to the wanted space it actually takes
    
        * Preview: https://imgur.com/a/XanJ2aP
    
        Change-Id: I2bd08cee1abd1c6bad78ca1efc2189e573ded3cc
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Dec 15 17:11:41 2018 +0100
    
        fingerprint: Remove unnecessary spacing in enroll layout
    
        * This makes it look ugly on 480 dpi devices.
    
        Change-Id: Ia1bbd443063a125878dddd12e5c6a816d289fb82
    
    Change-Id: If372ef50c4a719e89a4e80cc20e3eb445af61bc6

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
@@ -122,8 +122,19 @@ public class FingerprintEnrollFindSensor extends BiometricEnrollBase implements
                     break;
             }
         } else {
+            final boolean isFrontFacingFps = getResources().getBoolean(
+                    R.bool.config_is_front_facing_fps);
+            final String fpsLocation = getString(mCanAssumeSidefps
+                    ? R.string.fingerprint_enroll_find_sensor_message_side : isFrontFacingFps
+                            ? R.string.fingerprint_enroll_find_sensor_message_front
+                            : R.string.fingerprint_enroll_find_sensor_message_rear);
+
             setHeaderText(R.string.security_settings_fingerprint_enroll_find_sensor_title);
-            setDescriptionText(R.string.security_settings_fingerprint_enroll_find_sensor_message);
+            setDescriptionText(fpsLocation);
+            if (isFrontFacingFps) {
+                findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                        .setVisibility(View.VISIBLE);
+            }
         }
 
         // This is an entry point for SetNewPasswordController, e.g.
