b4c0a5c8e4dfe65719ef177be088196871ed5612
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-23 01:44:32 +0300

    Settings: gestures: Add page for Screen-Off UDFPS
    Change-Id: I313969384fe605d7b5ff7dfc3a3fcebd514dd7cf

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -28,6 +28,7 @@ import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
+import com.android.settings.gestures.ScreenOffUdfpsPreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.security.screenlock.LockScreenPreferenceController;
@@ -92,6 +93,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
+        use(ScreenOffUdfpsPreferenceController.class).setConfig(getConfig(context));
     }
 
     @Override
