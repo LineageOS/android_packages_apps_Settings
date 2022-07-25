6db691b57ed61b7b2ee130d861bab92c26ba3a03
Tư Mã Tần Quảng <baobaoxich@gmail.com>
2022-07-25 20:22:55 +0000

    Fix the problem of inconsistencies between restrict data types
    When implementing (un)restrict data through adb will also immediately update the options toggle in the settings
    Fix the problem of asynchronous settings when implementing the restrict data via adb
    When disabling "Allow network access" in settings will disable all other types of data at the same time
    Fix the "Unstricted data" option can still be triggered despite disabling "Allow network access" through "Special app access" → "Unstricted data" in Settings
    When restrict simultaneously Wi-Fi data, Mobile data and VPN data will also automatically disable "Allow network access" immediately
    
    Signed-off-by: Tư Mã Tần Quảng <baobaoxich@gmail.com>
    Change-Id: Ibd20d50db193b61eaf38bc0c8c45c08e6b309f9f

--- src/com/android/settings/datausage/AppDataUsage.java
+++ src/com/android/settings/datausage/AppDataUsage.java
@@ -263,28 +263,51 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mRestrictBackground) {
-            mDataSaverBackend.setIsBlacklisted(mAppItem.key, mPackageName, !(Boolean) newValue);
+        if (preference == mRestrictAll) {
+            setAppRestrictAll(!(Boolean) newValue);
+            // Disable "Allow network access" will restrict all other network type
+            if (!(Boolean) newValue) {
+                setAppRestrictWifi(!(Boolean) newValue);
+                mDataSaverBackend.setIsBlacklisted(mAppItem.key, mPackageName, !(Boolean) newValue);
+                setAppRestrictCellular(!(Boolean) newValue);
+                setAppRestrictVpn(!(Boolean) newValue);
+                refreshPrefs();
+            }
             updatePrefs();
             return true;
-        } else if (preference == mRestrictAll) {
-            setAppRestrictAll(!(Boolean) newValue);
+        } else if (preference == mRestrictBackground) {
+            mDataSaverBackend.setIsBlacklisted(mAppItem.key, mPackageName, !(Boolean) newValue);
+            if (!(Boolean) newValue) {
+                refreshPrefs();
+            }
             updatePrefs();
             return true;
         } else if (preference == mRestrictCellular) {
             setAppRestrictCellular(!(Boolean) newValue);
+            // Restrict "Background data" if restrict "Mobile data"
+            if (!(Boolean) newValue) {
+                mDataSaverBackend.setIsBlacklisted(mAppItem.key, mPackageName, !(Boolean) newValue);
+                refreshPrefs();
+            }
             updatePrefs();
             return true;
         } else if (preference == mRestrictVpn) {
             setAppRestrictVpn(!(Boolean) newValue);
+            if (!(Boolean) newValue) {
+                refreshPrefs();
+            }
             updatePrefs();
             return true;
         } else if (preference == mRestrictWifi) {
             setAppRestrictWifi(!(Boolean) newValue);
+            if (!(Boolean) newValue) {
+                refreshPrefs();
+            }
             updatePrefs();
             return true;
         } else if (preference == mUnrestrictedData) {
             mDataSaverBackend.setIsWhitelisted(mAppItem.key, mPackageName, (Boolean) newValue);
+            updatePrefs();
             return true;
         }
         return false;
@@ -309,6 +332,23 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    private void refreshPrefs() {
+        boolean isRestrictWifi = getAppRestrictWifi();
+        setAppRestrictWifi(isRestrictWifi);
+        mDataSaverBackend.refreshBlacklist();
+        boolean isRestrictCellular = getAppRestrictCellular();
+        setAppRestrictCellular(isRestrictCellular);
+        boolean isRestrictVpn = getAppRestrictVpn();
+        setAppRestrictVpn(isRestrictVpn);
+        boolean isRestrictAll = getAppRestrictAll();
+        // Set "Allow network access" to disabled if all other network type are restricted
+        if(isRestrictWifi && isRestrictCellular && isRestrictVpn && !isRestrictAll) {
+            isRestrictAll = true;
+        }
+        setAppRestrictAll(isRestrictAll);
+        updatePrefs();
     }
 
     @VisibleForTesting
