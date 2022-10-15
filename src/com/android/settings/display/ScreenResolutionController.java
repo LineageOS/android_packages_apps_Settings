8f9e7f63e03e18fd0efe36791c058f32deb64528
Luofan Chen <clfbbn@gmail.com>
2022-10-15 18:29:54 +0800

    resolution: Rework on screen resolution change setting
    Rewrite the screen resolution setting based on
    Iac1f65ab09717ea55a5b471e094385c77ba894ee.
    
    Change-Id: I3273f369477284c9b7040fe0982945ec6722a1c1

--- src/com/android/settings/display/ScreenResolutionController.java
+++ src/com/android/settings/display/ScreenResolutionController.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,72 +17,211 @@
 
 package com.android.settings.display;
 
+import android.annotation.Nullable;
+import android.util.Log;
 import android.content.Context;
 import android.hardware.display.DisplayManager;
 import android.view.Display;
 
-import androidx.annotation.VisibleForTesting;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
-import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.display.DisplayDensityUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /** Controller that switch the screen resolution. */
-public class ScreenResolutionController extends BasePreferenceController {
+public class ScreenResolutionController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
 
-    static final int FHD_WIDTH = 1080;
-    static final int QHD_WIDTH = 1440;
+    private static final String KEY_RESOLUTION_SWITCH = "screen_resolution";
 
-    private Display mDisplay;
+    private static final String TAG = "screenResolution";
 
-    public ScreenResolutionController(Context context, String key) {
-        super(context, key);
+    private ListPreference mListPreference;
 
-        mDisplay =
-                mContext.getSystemService(DisplayManager.class).getDisplay(Display.DEFAULT_DISPLAY);
-    }
+    private final List<String> mEntries = new ArrayList<>();
+    private final List<String> mValues = new ArrayList<>();
 
-    /** Check if the width is supported by the display. */
-    private boolean isSupportedMode(int width) {
-        for (Display.Mode mode : getSupportedModes()) {
-            if (mode.getPhysicalWidth() == width) return true;
+    private final Display mDisplay;
+
+    private final DisplayObserver mDisplayObserver;
+
+    public ScreenResolutionController(Context context) {
+        super(context, KEY_RESOLUTION_SWITCH);
+
+        mDisplay = Objects.requireNonNull(
+                context.getSystemService(DisplayManager.class)).getDisplay(
+                Display.DEFAULT_DISPLAY);
+
+        mDisplayObserver = new DisplayObserver(context);
+
+        // Find related display resolutions
+        Display.Mode mode = mDisplay.getMode();
+        Display.Mode[] avail_modes = mDisplay.getSupportedModes();
+
+        for (Display.Mode m : avail_modes) {
+            if (m.getRefreshRate() == mode.getRefreshRate()) {
+                mEntries.add(String.format("%d x %d", m.getPhysicalWidth(), m.getPhysicalHeight()));
+                mValues.add(String.format("%d %d", m.getPhysicalWidth(),
+                        m.getPhysicalHeight()));
+            }
         }
-        return false;
-    }
-
-    /** Return true if the device contains two (or more) resolutions. */
-    protected boolean checkSupportedResolutions() {
-        return isSupportedMode(FHD_WIDTH) && isSupportedMode(QHD_WIDTH);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return (checkSupportedResolutions()) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return mEntries.size() > 1 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
-    public CharSequence getSummary() {
-        String summary = null;
-        switch (getDisplayWidth()) {
-            case FHD_WIDTH:
-                summary = mContext.getString(R.string.screen_resolution_summary_high);
-                break;
-            case QHD_WIDTH:
-                summary = mContext.getString(R.string.screen_resolution_summary_highest);
-                break;
-            default:
-                summary = mContext.getString(R.string.screen_resolution_title);
+    public String getPreferenceKey() {
+        return KEY_RESOLUTION_SWITCH;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mListPreference = screen.findPreference(getPreferenceKey());
+        assert mListPreference != null;
+        mListPreference.setEntries(mEntries.toArray(new String[0]));
+        mListPreference.setEntryValues(mValues.toArray(new String[0]));
+
+        super.displayPreference(screen);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        Display.Mode mode = mDisplay.getMode();
+
+        int index = Math.max(0, mListPreference.findIndexOfValue(
+                String.format("%d %d", mode.getPhysicalWidth(), mode.getPhysicalHeight())));
+        mListPreference.setValueIndex(index);
+        mListPreference.setSummary(mListPreference.getEntries()[index]);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mDisplayObserver.startObserve();
+        String[] valueString = newValue.toString().split(" ");
+        assert valueString.length == 2;
+        int resolutionWidth = Integer.parseInt(valueString[0]);
+        int resolutionHeight = Integer.parseInt(valueString[1]);
+        Display.Mode switchMode = new Display.Mode(resolutionWidth, resolutionHeight,
+                mDisplay.getMode().getRefreshRate());
+        mDisplay.setUserPreferredDisplayMode(switchMode);
+
+        return true;
+    }
+
+    private static final class DisplayObserver implements DisplayManager.DisplayListener {
+        @Nullable
+        private final Context mContext;
+        private int mOldDensity = 0;
+        private final AtomicInteger mPreviousWidth = new AtomicInteger(-1);
+
+        DisplayObserver(Context context) {
+            mContext = context;
         }
 
-        return summary;
-    }
+        public void startObserve() {
+            if (mContext == null) {
+                return;
+            }
+            final DisplayDensityUtils density = new DisplayDensityUtils(mContext);
+            final int mOldIndex = density.getCurrentIndex();
+            if (density.getValues()[mOldIndex] == mOldDensity) {
+                return;
+            }
+            mOldDensity = density.getValues()[mOldIndex];
+            mPreviousWidth.set(getCurrentWidth(Display.DEFAULT_DISPLAY));
+            final DisplayManager dm = mContext.getSystemService(DisplayManager.class);
+            assert dm != null;
+            dm.registerDisplayListener(this, null);
+        }
 
-    @VisibleForTesting
-    public int getDisplayWidth() {
-        return mDisplay.getMode().getPhysicalWidth();
-    }
+        public void stopObserve() {
+            if (mContext == null) {
+                return;
+            }
+            final DisplayManager dm = mContext.getSystemService(DisplayManager.class);
+            assert dm != null;
+            dm.unregisterDisplayListener(this);
+        }
 
-    @VisibleForTesting
-    public Display.Mode[] getSupportedModes() {
-        return mDisplay.getSupportedModes();
+        @Override
+        public void onDisplayAdded(int displayId) {
+        }
+
+        @Override
+        public void onDisplayRemoved(int displayId) {
+        }
+
+        @Override
+        public void onDisplayChanged(int displayId) {
+            if (displayId != Display.DEFAULT_DISPLAY) {
+                Log.e(TAG, "not main display: " + displayId);
+                return;
+            }
+            if (!isDensityChanged() || !isResolutionChangeApplied(displayId)) {
+                Log.e(TAG, "Nothing changed");
+                return;
+            }
+            restoreDensity(displayId);
+            stopObserve();
+        }
+
+        private void restoreDensity(int displayId) {
+            final DisplayDensityUtils density = new DisplayDensityUtils(mContext);
+            int[] densityValues = density.getValues();
+            int newWidth = getCurrentWidth(displayId);
+
+            // Scale the DPI based on the following formula:
+            //   px = C1 * (dpi / C2) ==> px_new / px_old = dpi_new / dpi_old
+            // So we have:
+            //   dpi_new = dpi_old * px_new / px_old
+            int newDensity = (int) ((double) mOldDensity * newWidth / mPreviousWidth.get());
+
+            // Find the closest DPI setting
+            int minDistance = Math.abs(densityValues[0] - newDensity);
+            int idx = 0;
+            for (int i = 1; i < densityValues.length; i++) {
+                int dist = Math.abs(densityValues[i] - newDensity);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    idx = i;
+                }
+            }
+
+            Log.d(TAG, "Current width " + newWidth + ", old width " + mPreviousWidth.get());
+            Log.d(TAG,
+                    "Original dpi: " + mOldDensity + ", would like to change to " + newDensity
+                            + " actually set to " + densityValues[idx]);
+            DisplayDensityUtils.setForcedDisplayDensity(Display.DEFAULT_DISPLAY,
+                    densityValues[idx]);
+
+            // Update values
+            mPreviousWidth.set(newWidth);
+            mOldDensity = densityValues[idx];
+        }
+
+        private boolean isDensityChanged() {
+            final DisplayDensityUtils density = new DisplayDensityUtils(mContext);
+            return density.getDefaultDensity() != mOldDensity;
+        }
+
+        private int getCurrentWidth(int displayId) {
+            final DisplayManager dm = mContext.getSystemService(DisplayManager.class);
+            assert dm != null;
+            return dm.getDisplay(displayId).getMode().getPhysicalWidth();
+        }
+
+        private boolean isResolutionChangeApplied(int displayId) {
+            return mPreviousWidth.get() != getCurrentWidth(displayId);
+        }
     }
 }
