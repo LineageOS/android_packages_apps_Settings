65fe282428d34b6bf21964e6fa49d0f12bf1c7fd
Michael Bestas <mkbestas@lineageos.org>
2022-10-15 18:14:21 +0300

    [SQUASH] Merge tag 'android-13.0.0_r11' into staging/lineage-20.0_merge-android-13.0.0_r11
    Android 13.0.0 release 11
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCY0jiUgAKCRDorT+BmrEO
    # eBxuAJ42R7eZlf9A+yHCTWgE0s8/Q9IsVwCfV0UwsHhCVsTZBbkymg7qGrEl9iY=
    # =1lWL
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Fri Oct 14 07:15:14 2022 EEST
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [marginal]
    # gpg: initial-contribution@android.com: Verified 1359 signatures in the past
    #      11 months.  Encrypted 4 messages in the past 9 months.
    # gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    # gpg:          It is not certain that the signature belongs to the owner.
    # Primary key fingerprint: 4340 D135 70EF 945E 8381  0964 E8AD 3F81 9AB1 0E78
    
    # By Bill Yi (73) and others
    # Via Automerger Merge Worker (772) and others
    * tag 'android-13.0.0_r11': (104 commits)
      [Settings] Not allow APN delete when adding is not an option
      Wi-Fi panel doesn't need to check permission
      Import translations. DO NOT MERGE ANYWHERE
      Don't cancel passkey entry pairing autonomously
      Set an action name for wallpaer index
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      [MEP] Hide the preferred SIM dialog when the user has replaced the SIM
      [RRS] Activate investigation of screen_resolution in settingsstats
      Import translations. DO NOT MERGE ANYWHERE
      ...
    
     Conflicts:
    	res/values/config.xml
    	src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
    
    Change-Id: I258b4143f41c10c9a036d32ccf02d952775e24ed

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollFindSensor.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.accessibility.AccessibilityManager;
@@ -45,6 +46,9 @@ import java.util.List;
  */
 public class FingerprintEnrollFindSensor extends BiometricEnrollBase implements
         BiometricEnrollSidecar.Listener {
+
+
+    private static final String TAG = "FingerprintEnrollFindSensor";
 
     @Nullable
     private FingerprintFindSensorAnimation mAnimation;
@@ -268,6 +272,19 @@ public class FingerprintEnrollFindSensor extends BiometricEnrollBase implements
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG,
+                "onActivityResult(requestCode=" + requestCode + ", resultCode=" + resultCode + ")");
+        boolean enrolledFingerprint = false;
+        if (data != null) {
+            enrolledFingerprint = data.getBooleanExtra(EXTRA_FINISHED_ENROLL_FINGERPRINT, false);
+        }
+
+        if (resultCode == RESULT_CANCELED && enrolledFingerprint) {
+            setResult(resultCode, data);
+            finish();
+            return;
+        }
+
         if (requestCode == CONFIRM_REQUEST) {
             if (resultCode == RESULT_OK && data != null) {
                 throw new IllegalStateException("Pretty sure this is dead code");
