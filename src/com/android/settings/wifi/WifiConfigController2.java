5de807144a2eff5eb02afeaabaa7e458f6724354
Tommy Webb <tommy@calyxinstitute.org>
2023-04-21 15:33:10 -0400

    Properly set Wi-Fi Privacy spinner initial state
    In the Wi-Fi configuration dialog, properly set the initial value for
    the MAC address randomization privacy spinner.
    
    Bug: 279001779
    Change-Id: I14f444429a4f4534f361dd25d032c2bae5b662fb

--- src/com/android/settings/wifi/WifiConfigController2.java
+++ src/com/android/settings/wifi/WifiConfigController2.java
@@ -344,7 +344,7 @@ public class WifiConfigController2 implements TextWatcher,
 
                 if (mPrivacySettingsSpinner != null) {
                     final int prefMacValue = WifiPrivacyPreferenceController2
-                            .translateMacRandomizedValueToPrefValue(config.macRandomizationSetting);
+                            .translateMacRandomizedValueToPrefValue(mWifiEntry.getPrivacy());
                     mPrivacySettingsSpinner.setSelection(prefMacValue);
                 }
 
