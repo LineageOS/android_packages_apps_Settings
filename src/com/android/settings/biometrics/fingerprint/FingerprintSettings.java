6303038ad63bc0801982fc9b0086753b32802b56
Michael Bestas <mkbestas@gmail.com>
2023-12-09 15:34:22 +0200

    Hide fingerprint settings help if URL is empty
    Change-Id: I1631575bd6e48419f0649491a5ae167adf72c8c2

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -486,10 +486,17 @@ public class FingerprintSettings extends SubSettings {
         private void updateFooterColumns(@NonNull Activity activity) {
             final EnforcedAdmin admin = RestrictedLockUtilsInternal.checkIfKeyguardFeaturesDisabled(
                     activity, DevicePolicyManager.KEYGUARD_DISABLE_FINGERPRINT, mUserId);
-            final Intent helpIntent = HelpUtils.getHelpIntent(
-                    activity, getString(getHelpResource()), activity.getClass().getName());
-            final View.OnClickListener learnMoreClickListener = (v) ->
-                    activity.startActivityForResult(helpIntent, 0);
+            final Intent helpIntent;
+            final View.OnClickListener learnMoreClickListener;
+            if (getHelpResource() != 0) {
+                helpIntent = HelpUtils.getHelpIntent(
+                        activity, getString(getHelpResource()), activity.getClass().getName());
+                learnMoreClickListener = (v) ->
+                        activity.startActivityForResult(helpIntent, 0);
+            } else {
+                helpIntent = null;
+                learnMoreClickListener = null;
+            }
 
             mFooterColumns.clear();
             if (admin != null) {
@@ -511,11 +518,13 @@ public class FingerprintSettings extends SubSettings {
                 column2.mTitle = getText(
                         R.string.security_fingerprint_disclaimer_lockscreen_disabled_2
                 );
-                if (isSfps()) {
-                    column2.mLearnMoreOverrideText = getText(
-                            R.string.security_settings_fingerprint_settings_footer_learn_more);
+                if (helpIntent != null) {
+                    if (isSfps()) {
+                        column2.mLearnMoreOverrideText = getText(
+                                R.string.security_settings_fingerprint_settings_footer_learn_more);
+                    }
+                    column2.mLearnMoreClickListener = learnMoreClickListener;
                 }
-                column2.mLearnMoreClickListener = learnMoreClickListener;
                 mFooterColumns.add(column2);
             } else {
                 final FooterColumn column = new FooterColumn();
@@ -523,9 +532,11 @@ public class FingerprintSettings extends SubSettings {
                         ? R.string.private_space_fingerprint_enroll_introduction_message
                         : R.string.security_settings_fingerprint_enroll_introduction_v3_message,
                         DeviceHelper.getDeviceName(getActivity()));
-                column.mLearnMoreClickListener = learnMoreClickListener;
-                column.mLearnMoreOverrideText = getText(
-                        R.string.security_settings_fingerprint_settings_footer_learn_more);
+                if (helpIntent != null) {
+                    column.mLearnMoreClickListener = learnMoreClickListener;
+                    column.mLearnMoreOverrideText = getText(
+                            R.string.security_settings_fingerprint_settings_footer_learn_more);
+                }
                 mFooterColumns.add(column);
             }
         }
