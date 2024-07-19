ee772d5532a77b2fd5f67edfd29d77b68e98268d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-07-19 08:36:15 +0530

    Settings: Reset tether offload developer setting correctly
    Tethering offload is enabled by default, i.e tether_offload_disabled is 0.
    When developer settings is disabled, we want it set back to 0 i.e
    tethering offload enabled.
    
    Change-Id: I71b7fd04cf01b0cfa40b36568169e444f8e32a22

--- src/com/android/settings/development/TetheringHardwareAccelPreferenceController.java
+++ src/com/android/settings/development/TetheringHardwareAccelPreferenceController.java
@@ -60,7 +60,7 @@ public class TetheringHardwareAccelPreferenceController extends DeveloperOptions
     public void updateState(Preference preference) {
         final int tetheringMode = Settings.Global.getInt(
                 mContext.getContentResolver(),
-                Settings.Global.TETHER_OFFLOAD_DISABLED, 0 /* default */);
+                Settings.Global.TETHER_OFFLOAD_DISABLED, SETTING_VALUE_ON);
         ((TwoStatePreference) mPreference).setChecked(tetheringMode != SETTING_VALUE_OFF);
     }
 
@@ -68,7 +68,7 @@ public class TetheringHardwareAccelPreferenceController extends DeveloperOptions
     protected void onDeveloperOptionsSwitchDisabled() {
         super.onDeveloperOptionsSwitchDisabled();
         Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.TETHER_OFFLOAD_DISABLED, SETTING_VALUE_OFF);
+                Settings.Global.TETHER_OFFLOAD_DISABLED, SETTING_VALUE_ON);
         ((TwoStatePreference) mPreference).setChecked(false);
     }
 }
