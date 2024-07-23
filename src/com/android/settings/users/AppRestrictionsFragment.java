14939ba2f493f342de3a419843a31e662af722af
Adam Bookatz <bookatz@google.com>
2024-07-22 17:03:12 -0700

    startActivityForResult with new Intent
    Rather than use the raw Intent, we make a copy of it. See bug.
    
    Bug: 330722900
    Flag: EXEMPT bugfix
    Test: manual
    Test: atest com.android.settings.users.UserSettingsTest
                com.android.settings.users.UserDetailsSettingsTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1189e24e47571eae86634aeaa7dc60b8fe7f4820)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:fdb148b6efb16af018a39511001b48286f401512)
    Merged-In: Id74e4b7ae261f2916eedaef04a679f83409a4b67
    Change-Id: Id74e4b7ae261f2916eedaef04a679f83409a4b67

--- src/com/android/settings/users/AppRestrictionsFragment.java
+++ src/com/android/settings/users/AppRestrictionsFragment.java
@@ -648,7 +648,7 @@ public class AppRestrictionsFragment extends SettingsPreferenceFragment implemen
                     int requestCode = generateCustomActivityRequestCode(
                             RestrictionsResultReceiver.this.preference);
                     AppRestrictionsFragment.this.startActivityForResult(
-                            restrictionsIntent, requestCode);
+                            new Intent(restrictionsIntent), requestCode);
                 }
             }
         }
