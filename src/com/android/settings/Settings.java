f50d5eb9f4c2308c080d51ce565399f60c997c0d
tom hsu <tomhsu@google.com>
2025-07-31 09:08:00 +0000

    [Settings] Avoid start wrong fragment from MobileNetworkActivity
    Flag: EXEMPT bug fix
    Fix: b/409319199
    Test: Manual test
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:6787354cfb461920a1d5f90291878ae399e81048
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:b2505ecf4d46495fbc775ebfc81ea488b1707f3a
    Merged-In: I2068ceb7ef0fc78ae0948f9058eae2b1b7e57453
    Change-Id: I2068ceb7ef0fc78ae0948f9058eae2b1b7e57453

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -27,6 +27,9 @@ import android.text.TextUtils;
 import android.util.FeatureFlagUtils;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settings.biometrics.face.FaceSettings;
 import com.android.settings.core.FeatureFlags;
@@ -378,7 +381,8 @@ public class Settings extends SettingsActivity {
     public static class MobileNetworkListActivity extends SettingsActivity {}
     public static class PowerMenuSettingsActivity extends SettingsActivity {}
     public static class MobileNetworkActivity extends SettingsActivity {
-
+        private static final String MOBILE_NETWORK_FRAGMENT_NAME =
+                "com.android.settings.network.telephony.MobileNetworkSettings";
         public static final String TAG = "MobileNetworkActivity";
         public static final String EXTRA_MMS_MESSAGE = "mms_message";
         public static final String EXTRA_SHOW_CAPABILITY_DISCOVERY_OPT_IN =
@@ -393,7 +397,10 @@ public class Settings extends SettingsActivity {
         @Override
         protected void onNewIntent(Intent intent) {
             super.onNewIntent(intent);
-
+            if (!isTargetIsMobileNetwork(intent)) {
+                finish();
+                return;
+            }
             Log.d(TAG, "Starting onNewIntent");
 
             createUiFromIntent(null /* savedState */, convertIntent(intent));
@@ -416,6 +423,14 @@ public class Settings extends SettingsActivity {
             String intentAction = (intent != null ? intent.getAction() : null);
             return TextUtils.equals(intentAction,
                     ImsRcsManager.ACTION_SHOW_CAPABILITY_DISCOVERY_OPT_IN);
+        }
+
+        private static boolean isTargetIsMobileNetwork(@NonNull Intent intent) {
+            String fragmentName = intent.getStringExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT);
+            if (fragmentName == null && fragmentName.isEmpty()) {
+                return false;
+            }
+            return fragmentName.equals(MOBILE_NETWORK_FRAGMENT_NAME);
         }
     }
 
