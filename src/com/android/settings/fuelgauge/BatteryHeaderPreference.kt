196196d27d74130023e16bbd61ab5462f3933aed
armdebug <armdebug@protonmail.com>
2026-02-19 07:53:50 +0000

    Settings: Move charge counter readout to BatteryHeaderPreference.kt
    The charge counter (mAh) readout is a Lineage addition, not present
    upstream. AOSP migrated BatteryHeaderPreference to Kotlin without it.
    
    Move the logic to the new Kotlin class, and drop it from the old Java
    controller.
    
    Change-Id: Ib14e6ea8362103e765b63e359c2b3db19a5d3946

--- src/com/android/settings/fuelgauge/BatteryHeaderPreference.kt
+++ src/com/android/settings/fuelgauge/BatteryHeaderPreference.kt
@@ -145,8 +145,13 @@ class BatteryHeaderPreference :
         private fun quickUpdateHeaderPreference(preference: UsageProgressBarPreference) {
             val batteryIntent = BatteryUtils.getBatteryIntent(preference.context) ?: return
             val batteryLevel: Int = Utils.getBatteryLevel(batteryIntent)
+            val chargeCounterUah = batteryIntent.getIntExtra(android.os.BatteryManager.EXTRA_CHARGE_COUNTER, -1)
             preference.apply {
                 setUsageSummary(com.android.settings.Utils.formatPercentage(batteryLevel))
+                if (chargeCounterUah > 0) {
+                    val chargeCounterMah = chargeCounterUah / 1000
+                    setTotalSummary(context.getString(R.string.battery_charge_counter_summary, chargeCounterMah))
+                }
                 setPercent(batteryLevel.toLong(), BATTERY_MAX_LEVEL)
                 setBottomSummary("")
             }
