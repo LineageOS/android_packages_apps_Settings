2e9007ea7f38b47f4817ab7a3a487aecd09c4dda
Joe Bolinger <jbolinger@google.com>
2026-03-27 22:34:13 +0000

    Ensure remote device credential alias is used for action CONFIRM_REMOTE_DEVICE_CREDENTIAL.
    Bug: 487522353
    Flag: EXEMPT CVE_FIX
    Test: atest StsHostTestCases:android.security.sts.Bug_487522353#testPocBug_487522353
    DISABLE_TOPIC_PROTECTOR
    Change-Id: I0671e5945e42cdb809bafe0065e987e7c7fa6f1d
    (cherry picked from commit 4e2cf5e22953a3d3c9957d448cf69fd8d6ecaead)

--- tests/robotests/testutils/com/android/settings/testutils/shadow/ShadowLockPatternUtils.java
+++ tests/robotests/testutils/com/android/settings/testutils/shadow/ShadowLockPatternUtils.java
@@ -107,6 +107,15 @@ public class ShadowLockPatternUtils {
     }
 
     @Implementation
+    protected int getCredentialTypeForUser(int userId) {
+        final boolean isSecure = sUserToIsSecureMap.getOrDefault(userId, false);
+        if (isSecure) {
+            return LockPatternUtils.CREDENTIAL_TYPE_PIN;
+        }
+        return LockPatternUtils.CREDENTIAL_TYPE_NONE;
+    }
+
+    @Implementation
     protected static boolean isDeviceEncryptionEnabled() {
         return sDeviceEncryptionEnabled;
     }
