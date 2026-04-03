94353362af8b97054b638f03c40650673bccac8e
Diya Bera <diyab@google.com>
2026-04-03 19:51:55 +0000

    Remove EXTRA_DATA from ConfirmDeviceCredentialActivity
    Bug: 494585668
    Test: N/A
    Flag: EXEMPT CVE_FIX
    Change-Id: I29eb7981d202c878c51b9e68ab1564d662ec0fc1
    DISABLE_TOPIC_PROTECTOR: omitting STS test

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -309,6 +309,8 @@ public class FingerprintSettings extends SubSettings {
                 "biometrics_authentication_requested";
         private static final String KEY_BIOMETRICS_USE_FINGERPRINT_TO_CATEGORY =
                 "biometric_settings_use_fingerprint_to";
+        private static final String KEY_GK_PW_HANDLE =
+                "gk_pw_handle";
 
         private static final int MSG_REFRESH_FINGERPRINT_TEMPLATES = 1000;
         private static final int MSG_FINGER_AUTH_SUCCESS = 1001;
@@ -363,6 +365,7 @@ public class FingerprintSettings extends SubSettings {
         private boolean mHasRunChallengeInvoker = false;
 
         private long mChallenge;
+        private long mGkPwHandle;
 
         private static final String TAG_AUTHENTICATE_SIDECAR = "authenticate_sidecar";
         private static final String TAG_REMOVAL_SIDECAR = "removal_sidecar";
@@ -653,6 +656,7 @@ public class FingerprintSettings extends SubSettings {
                         mHasFirstEnrolled);
                 mBiometricsAuthenticationRequested = savedInstanceState.getBoolean(
                         KEY_BIOMETRICS_AUTHENTICATION_REQUESTED);
+                mGkPwHandle = savedInstanceState.getLong(KEY_GK_PW_HANDLE);
             }
 
             // (mLaunchedConfirm or mIsEnrolling) means that we are waiting an activity result.
@@ -1234,6 +1238,7 @@ public class FingerprintSettings extends SubSettings {
             outState.putBoolean(KEY_HAS_RUN_CHALLENGE_INVOKER, mHasRunChallengeInvoker);
             outState.putBoolean(KEY_BIOMETRICS_AUTHENTICATION_REQUESTED,
                     mBiometricsAuthenticationRequested);
+            outState.putLong(KEY_GK_PW_HANDLE, mGkPwHandle);
         }
 
         @Override
@@ -1397,6 +1402,7 @@ public class FingerprintSettings extends SubSettings {
                 if (resultCode == RESULT_FINISHED || resultCode == RESULT_OK) {
                     runChallengeGeneratedInvokers();
                     if (BiometricUtils.containsGatekeeperPasswordHandle(data)) {
+                        mGkPwHandle = BiometricUtils.getGatekeeperPasswordHandle(data);
                         final Utils.BiometricStatus biometricAuthStatus =
                                 Utils.requestBiometricAuthenticationForMandatoryBiometrics(
                                         getActivity(),
@@ -1406,21 +1412,21 @@ public class FingerprintSettings extends SubSettings {
                             if (biometricAuthStatus != Utils.BiometricStatus.NOT_ACTIVE) {
                                 Utils.launchBiometricPromptForMandatoryBiometrics(this,
                                         BIOMETRIC_AUTH_REQUEST,
-                                        mUserId, true /* hideBackground */, data);
+                                        mUserId, true /* hideBackground */);
                             } else {
-                                handleAuthenticationSuccessful(data);
+                                handleAuthenticationSuccessful(mGkPwHandle);
                             }
                         } else if (biometricAuthStatus == Utils.BiometricStatus.OK) {
                             Utils.launchBiometricPromptForMandatoryBiometrics(this,
                                     BIOMETRIC_AUTH_REQUEST,
-                                    mUserId, true /* hideBackground */, data);
+                                    mUserId, true /* hideBackground */);
                         } else if (biometricAuthStatus != Utils.BiometricStatus.NOT_ACTIVE) {
                             IdentityCheckBiometricErrorDialog
                                     .showBiometricErrorDialogAndFinishActivityOnDismiss(
                                             getActivity(),
                                             biometricAuthStatus);
                         } else {
-                            handleAuthenticationSuccessful(data);
+                            handleAuthenticationSuccessful(mGkPwHandle);
                         }
                     } else {
                         Log.d(TAG, "Data null or GK PW missing");
@@ -1475,7 +1481,11 @@ public class FingerprintSettings extends SubSettings {
             } else if (requestCode == BIOMETRIC_AUTH_REQUEST) {
                 mBiometricsAuthenticationRequested = false;
                 if (resultCode == RESULT_OK) {
-                    handleAuthenticationSuccessful(data);
+                    if (mGkPwHandle == 0L) {
+                        Log.e(TAG, "Gatekeeper password not set.");
+                    } else {
+                        handleAuthenticationSuccessful(mGkPwHandle);
+                    }
                 } else {
                     if (resultCode
                             == ConfirmDeviceCredentialActivity.BIOMETRIC_LOCKOUT_ERROR_RESULT) {
@@ -1531,7 +1541,7 @@ public class FingerprintSettings extends SubSettings {
             }
         }
 
-        private void handleAuthenticationSuccessful(Intent data) {
+        private void handleAuthenticationSuccessful(long gkPwHandle) {
             if (!mHasFirstEnrolled && !mIsEnrolling) {
                 final Activity activity = getActivity();
                 if (activity != null) {
@@ -1546,8 +1556,7 @@ public class FingerprintSettings extends SubSettings {
                 // Token and challenge will be updated later through the activity result
                 // of AUTO_ADD_FIRST_FINGERPRINT_REQUEST.
                 mIsEnrolling = true;
-                addFirstFingerprint(
-                        BiometricUtils.getGatekeeperPasswordHandle(data));
+                addFirstFingerprint(gkPwHandle);
             } else {
                 mFingerprintManager.generateChallenge(mUserId,
                         (sensorId, userId, challenge) -> {
@@ -1561,10 +1570,10 @@ public class FingerprintSettings extends SubSettings {
                             final GatekeeperPasswordProvider provider =
                                     new GatekeeperPasswordProvider(
                                             new LockPatternUtils(activity));
-                            mToken = provider.requestGatekeeperHat(data, challenge,
+                            mToken = provider.requestGatekeeperHat(gkPwHandle, challenge,
                                     mUserId);
                             mChallenge = challenge;
-                            provider.removeGatekeeperPasswordHandle(data, false);
+                            provider.removeGatekeeperPasswordHandle(gkPwHandle);
                             updateAddPreference();
                         });
             }
--- src/com/android/settings/password/ConfirmDeviceCredentialActivity.java
+++ src/com/android/settings/password/ConfirmDeviceCredentialActivity.java
@@ -87,7 +87,6 @@ public class ConfirmDeviceCredentialActivity extends FragmentActivity {
             "biometric_prompt_negative_button_text";
     public static final String BIOMETRIC_PROMPT_HIDE_BACKGROUND =
             "biometric_prompt_hide_background";
-    public static final String EXTRA_DATA = "extra_data";
     public static final int BIOMETRIC_LOCKOUT_ERROR_RESULT = 100;
 
     public static class InternalActivity extends ConfirmDeviceCredentialActivity {
@@ -112,7 +111,6 @@ public class ConfirmDeviceCredentialActivity extends FragmentActivity {
     private boolean mGoingToBackground;
     private boolean mWaitingForBiometricCallback;
     private int mBiometricsAuthenticators;
-    private Intent mIntentData;
 
     private Executor mExecutor = (runnable -> {
         mHandler.post(runnable);
@@ -163,7 +161,7 @@ public class ConfirmDeviceCredentialActivity extends FragmentActivity {
                         ConfirmDeviceCredentialActivity.this);
             }
 
-            setResult(Activity.RESULT_OK, mIntentData);
+            setResult(Activity.RESULT_OK);
             finish();
         }
 
@@ -212,7 +210,6 @@ public class ConfirmDeviceCredentialActivity extends FragmentActivity {
         mBiometricsAuthenticators = intent.getIntExtra(BIOMETRIC_PROMPT_AUTHENTICATORS,
                 BiometricManager.Authenticators.DEVICE_CREDENTIAL
                         | BiometricManager.Authenticators.BIOMETRIC_WEAK);
-        mIntentData = intent.getParcelableExtra(EXTRA_DATA, Intent.class);
         final String negativeButtonText = intent.getStringExtra(
                 BIOMETRIC_PROMPT_NEGATIVE_BUTTON_TEXT);
         final boolean frp =
