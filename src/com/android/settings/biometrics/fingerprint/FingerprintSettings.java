11ca620583f4671ef324af79a34f109215b9040e
Diya Bera <diyab@google.com>
2026-04-03 19:51:55 +0000

    Remove EXTRA_DATA from ConfirmDeviceCredentialActivity
    Bug: 494585668
    Test: N/A
    Flag: EXEMPT CVE_FIX
    Change-Id: I29eb7981d202c878c51b9e68ab1564d662ec0fc1
    (cherry picked from commit e99f4f51fe5823cd990f93984517ee2875809c7f)
    DISABLE_TOPIC_PROTECTOR: omitting STS test

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -304,6 +304,8 @@ public class FingerprintSettings extends SubSettings {
                 "biometrics_authentication_requested";
         private static final String KEY_BIOMETRICS_USE_FINGERPRINT_TO_CATEGORY =
                 "biometric_settings_use_fingerprint_to";
+        private static final String KEY_GK_PW_HANDLE =
+                "gk_pw_handle";
 
         private static final int MSG_REFRESH_FINGERPRINT_TEMPLATES = 1000;
         private static final int MSG_FINGER_AUTH_SUCCESS = 1001;
@@ -358,6 +360,7 @@ public class FingerprintSettings extends SubSettings {
         private boolean mHasRunChallengeInvoker = false;
 
         private long mChallenge;
+        private long mGkPwHandle;
 
         private static final String TAG_AUTHENTICATE_SIDECAR = "authenticate_sidecar";
         private static final String TAG_REMOVAL_SIDECAR = "removal_sidecar";
@@ -647,6 +650,7 @@ public class FingerprintSettings extends SubSettings {
                         mHasFirstEnrolled);
                 mBiometricsAuthenticationRequested = savedInstanceState.getBoolean(
                         KEY_BIOMETRICS_AUTHENTICATION_REQUESTED);
+                mGkPwHandle = savedInstanceState.getLong(KEY_GK_PW_HANDLE);
             }
 
             // (mLaunchedConfirm or mIsEnrolling) means that we are waiting an activity result.
@@ -1227,6 +1231,7 @@ public class FingerprintSettings extends SubSettings {
             outState.putBoolean(KEY_HAS_RUN_CHALLENGE_INVOKER, mHasRunChallengeInvoker);
             outState.putBoolean(KEY_BIOMETRICS_AUTHENTICATION_REQUESTED,
                     mBiometricsAuthenticationRequested);
+            outState.putLong(KEY_GK_PW_HANDLE, mGkPwHandle);
         }
 
         @Override
@@ -1390,6 +1395,7 @@ public class FingerprintSettings extends SubSettings {
                 if (resultCode == RESULT_FINISHED || resultCode == RESULT_OK) {
                     runChallengeGeneratedInvokers();
                     if (BiometricUtils.containsGatekeeperPasswordHandle(data)) {
+                        mGkPwHandle = BiometricUtils.getGatekeeperPasswordHandle(data);
                         final Utils.BiometricStatus biometricAuthStatus =
                                 Utils.requestBiometricAuthenticationForMandatoryBiometrics(
                                         getActivity(),
@@ -1398,9 +1404,9 @@ public class FingerprintSettings extends SubSettings {
                         if (biometricAuthStatus != Utils.BiometricStatus.NOT_ACTIVE) {
                             Utils.launchBiometricPromptForMandatoryBiometrics(this,
                                     BIOMETRIC_AUTH_REQUEST,
-                                    mUserId, true /* hideBackground */, data);
+                                    mUserId, true /* hideBackground */);
                         } else {
-                            handleAuthenticationSuccessful(data);
+                            handleAuthenticationSuccessful(mGkPwHandle);
                         }
                     } else {
                         Log.d(TAG, "Data null or GK PW missing");
@@ -1455,7 +1461,11 @@ public class FingerprintSettings extends SubSettings {
             } else if (requestCode == BIOMETRIC_AUTH_REQUEST) {
                 mBiometricsAuthenticationRequested = false;
                 if (resultCode == RESULT_OK) {
-                    handleAuthenticationSuccessful(data);
+                    if (mGkPwHandle == 0L) {
+                        Log.e(TAG, "Gatekeeper password not set.");
+                    } else {
+                        handleAuthenticationSuccessful(mGkPwHandle);
+                    }
                 } else {
                     if (resultCode
                             == ConfirmDeviceCredentialActivity.BIOMETRIC_LOCKOUT_ERROR_RESULT) {
@@ -1511,7 +1521,7 @@ public class FingerprintSettings extends SubSettings {
             }
         }
 
-        private void handleAuthenticationSuccessful(Intent data) {
+        private void handleAuthenticationSuccessful(long gkPwHandle) {
             if (!mHasFirstEnrolled && !mIsEnrolling) {
                 final Activity activity = getActivity();
                 if (activity != null) {
@@ -1526,8 +1536,7 @@ public class FingerprintSettings extends SubSettings {
                 // Token and challenge will be updated later through the activity result
                 // of AUTO_ADD_FIRST_FINGERPRINT_REQUEST.
                 mIsEnrolling = true;
-                addFirstFingerprint(
-                        BiometricUtils.getGatekeeperPasswordHandle(data));
+                addFirstFingerprint(gkPwHandle);
             } else {
                 mFingerprintManager.generateChallenge(mUserId,
                         (sensorId, userId, challenge) -> {
@@ -1541,10 +1550,10 @@ public class FingerprintSettings extends SubSettings {
                             final GatekeeperPasswordProvider provider =
                                     new GatekeeperPasswordProvider(
                                             new LockPatternUtils(activity));
-                            mToken = provider.requestGatekeeperHat(data, challenge,
+                            mToken = provider.requestGatekeeperHat(gkPwHandle, challenge,
                                     mUserId);
                             mChallenge = challenge;
-                            provider.removeGatekeeperPasswordHandle(data, false);
+                            provider.removeGatekeeperPasswordHandle(gkPwHandle);
                             updateAddPreference();
                         });
             }
