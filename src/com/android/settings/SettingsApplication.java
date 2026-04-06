db6aef927fca971cbd883858a14536a966e96878
Mihai Stancu <mihaistancu@google.com>
2026-04-06 16:43:15 +0000

    Sanitize package labels in SettingsApplication
    Forces all loadLabel() calls to sanitize package labels
    globally by invoking PackageItemInfo.forceSafeLabels()
    during onCreate().
    
    Bug: 488559440
    Test: manual
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:50d61685ab9b6e03996f6b93cc8b0fc337e8410c
    Merged-In: I8ce8a122a473df7282eef283b1b13ce33c2ad9a3
    Change-Id: I8ce8a122a473df7282eef283b1b13ce33c2ad9a3

--- src/com/android/settings/SettingsApplication.java
+++ src/com/android/settings/SettingsApplication.java
@@ -18,6 +18,7 @@ package com.android.settings;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageItemInfo;
 import android.content.pm.PackageManager;
 import android.database.ContentObserver;
 import android.hardware.fingerprint.FingerprintManager;
@@ -77,6 +78,9 @@ public class SettingsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Force all loadLabel() calls to sanitize package labels
+        PackageItemInfo.forceSafeLabels();
 
         if (Flags.catalyst()) {
             CatalystFlagProviderFactory.INSTANCE.setProvider(
