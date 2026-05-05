1f532d7848d0171e86886732c709af5cdccd1220
Duy Truong <duytruong@google.com>
2026-05-04 22:47:16 -0700

    Fix confused deputy in Bluetooth settings dashboard
    The Bluetooth dashboard incorrectly trusted any caller from the Settings
    package itself. On multi-pane devices, a deep link trampoline causes
    external requests to appear as if they came from Settings.
    
    This change uses getInitialCallingPackage() to correctly identify the
    original caller and only enable AlwaysDiscoverable mode for trusted
    system callers.
    
    Bug: 471797085
    Test: atest BluetoothDashboardFragmentTest BluetoothDashboardFragmentVulnerabilityTest
    Flag: EXEMPT BUGFIX
    
    Note: Autogenerated by MendIt (go/androidmendit)
    NO_IFTTT=The changes are internal security checks in the fragment and do not affect the Catalyst screen structure or API.
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:0ca0778b89215e64f8573cb0487efd0b702721cf
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:080c5c9ee637718287d37e75b745c062d3998ec1
    Merged-In: I58b0c0898408562c1c088f14e0f3fff6ceaff1eb
    Change-Id: I58b0c0898408562c1c088f14e0f3fff6ceaff1eb

--- src/com/android/settings/bluetooth/DevicePickerFragment.java
+++ src/com/android/settings/bluetooth/DevicePickerFragment.java
@@ -19,6 +19,7 @@ package com.android.settings.bluetooth;
 import static android.os.UserManager.DISALLOW_CONFIG_BLUETOOTH;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.settings.SettingsEnums;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -33,10 +34,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.settings.R;
 import com.android.settings.flags.Flags;
+import com.android.settings.SettingsActivity;
 import com.android.settings.password.PasswordUtils;
 import com.android.settingslib.bluetooth.CachedBluetoothDevice;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -98,14 +101,26 @@ public final class DevicePickerFragment extends DeviceListPreferenceFragment {
         getActivity().setTitle(getString(R.string.device_picker));
         UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
         mScanAllowed = !um.hasUserRestriction(DISALLOW_CONFIG_BLUETOOTH);
-        mCallingAppPackageName = PasswordUtils.getCallingAppPackageName(
-                getActivity().getActivityToken());
+        mCallingAppPackageName = resolveInitialCallingPackage();
         if (!TextUtils.equals(mCallingAppPackageName, mLaunchPackage)) {
             Log.w(TAG, "sendDevicePickedIntent() launch package name is not equivalent to"
                     + " calling package name!");
         }
         mContext = getContext();
         setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    private String resolveInitialCallingPackage() {
+        Activity activity = getActivity();
+        if (activity instanceof SettingsActivity) {
+            return ((SettingsActivity) activity).getInitialCallingPackage();
+        }
+
+        if (activity != null) {
+            return PasswordUtils.getCallingAppPackageName(activity.getActivityToken());
+        }
+        return null;
     }
 
     @Override
