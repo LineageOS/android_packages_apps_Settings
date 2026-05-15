06c6e89cd59518136c0929c58da29327646b6891
Milton <mingjuwu@google.com>
2026-05-15 08:25:23 +0000

    Sanitize sensitive BiometricsSettingsBase extra
    External callers launching CombinedBiometricSettingsActivity could
    previously inject arbitrary EXTRA_USER_ID and EXTRA_KEY_GK_PW_HANDLE
    intent extras. This allowed unprivileged local apps to bypass device
    credential confirmation and manipulate biometric settings (such as
    Watch Unlock) for arbitrary users.
    
    This change ensures that sensitive intent extras (target user ID and
    gatekeeper password handle) are only accepted when the activity is
    launched internally by the Settings package itself. For external
    callers, it defaults to the current user and ignores any supplied
    gatekeeper password handle, properly enforcing the device credential
    confirmation flow.
    
    Bug: 490286986
    Test: atest CombinedBiometricProfileSettingsTest
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:6b7ef885f660aff17008c2877b19a2d3f2092242
    Merged-In: Ife9b0bbf44d97fe1b7aef55a3cef053cdb9377af
    Change-Id: Ife9b0bbf44d97fe1b7aef55a3cef053cdb9377af

--- src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
+++ src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
@@ -42,6 +42,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
 import com.android.settings.R;
+import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
 import com.android.settings.biometrics.BiometricEnrollBase;
 import com.android.settings.biometrics.BiometricStatusPreferenceController;
@@ -80,6 +81,7 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
 
     protected int mUserId;
     protected long mGkPwHandle;
+    private boolean mAllowInternalExtras;
     private boolean mConfirmCredential;
     private boolean mBiometricsAuthenticationRequested;
     @Nullable private FaceManager mFaceManager;
@@ -105,8 +107,18 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        mUserId = getActivity().getIntent().getIntExtra(Intent.EXTRA_USER_ID,
-                UserHandle.myUserId());
+        mUserId = UserHandle.myUserId();
+        mAllowInternalExtras = false;
+
+        final Activity hostActivity = getActivity();
+        if (hostActivity instanceof SettingsActivity) {
+            final SettingsActivity activity = (SettingsActivity) hostActivity;
+            final String callingPackage = activity.getInitialCallingPackage();
+            if (TextUtils.equals(callingPackage, activity.getPackageName())) {
+                mAllowInternalExtras = true;
+                mUserId = activity.getIntent().getIntExtra(Intent.EXTRA_USER_ID, mUserId);
+            }
+        }
     }
 
     @Override
@@ -115,7 +127,8 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
         mFaceManager = Utils.getFaceManagerOrNull(getActivity());
         mFingerprintManager = Utils.getFingerprintManagerOrNull(getActivity());
 
-        if (BiometricUtils.containsGatekeeperPasswordHandle(getIntent())) {
+        if (mAllowInternalExtras
+                && BiometricUtils.containsGatekeeperPasswordHandle(getIntent())) {
             mGkPwHandle = BiometricUtils.getGatekeeperPasswordHandle(getIntent());
         }
 
