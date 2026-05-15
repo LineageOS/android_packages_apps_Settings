ff82ae7605fea833fe347424e82cd848f042fce7
Gabriel Roman <gabrielroman@google.com>
2026-05-15 04:18:43 -0700

    Replace DeviceAdminInfo.loadDescription() with loadDescriptionSafe()
    The loadDescriptionSafe() truncates the description string to a safe
    1024 characters length.
    
    Bug: 492093013
    Test: manual
    Flag: EXEMPT BUGFIX
    DISABLE_TOPIC_PROTECTOR
    #b/492093013
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:b546ebe716f7ad1d6c01769a2072eaff47987234
    Merged-In: I1d29452e150538bcc576ac2d856029729a2ba51d
    Change-Id: I1d29452e150538bcc576ac2d856029729a2ba51d

--- src/com/android/settings/applications/specialaccess/deviceadmin/DeviceAdminListItem.java
+++ src/com/android/settings/applications/specialaccess/deviceadmin/DeviceAdminListItem.java
@@ -46,7 +46,7 @@ class DeviceAdminListItem implements Comparable<DeviceAdminListItem> {
         final PackageManager pm = context.getPackageManager();
         mName = mInfo.loadLabel(pm);
         try {
-            mDescription = mInfo.loadDescription(pm);
+            mDescription = mInfo.loadDescriptionSafe(pm);
         } catch (Resources.NotFoundException exception) {
             Log.w(TAG, "Setting description to null because can't find resource: " + mKey);
         }
